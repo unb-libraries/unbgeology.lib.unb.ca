4969d34536c8d072c04ef27d145b64832a85735a
Patrick Schilf <pschilf@unb.ca>
2023-04-24 14:26:48 -0300

    ROCKS-24 Implement logout

---
+++ app/pages/logout.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: [
+    function () {
+      const event = useRequestEvent()
+      event.context.session = null
+      return navigateTo({ path: `/` })
+    },
+  ],
+})
+</script>
