660ba649124494c4b79171abaae238e61548e8e3
Patrick Schilf <pschilf@unb.ca>
2023-04-26 12:55:32 -0300

    ROCKS-24 Add login/logout button

--- app/components/Page/Header.vue
+++ app/components/Page/Header.vue
@@ -3,8 +3,8 @@
     <div class="p-4">
       <div class="container mx-auto flex flex-col justify-between text-lg md:flex-row">
         <div class="flex flex-row justify-between">
-          <NuxtLink to="/" @click="collapsed = true">
-            UNB Earth Sciences Collections
+          <NuxtLink class="self-center" to="/" @click="collapsed = true">
+            <span>UNB Earth Sciences Collections</span>
           </NuxtLink>
           <div class="cursor-pointer md:hidden" @click="collapsed = !collapsed">
             <svg
@@ -21,6 +21,10 @@
         </div>
         <nav class="flex flex-col md:flex md:flex-row md:items-center md:space-x-6" :class="collapsed ? 'hidden' : ''">
           <!-- Menu items here -->
+          <button class="bg-black p-3 text-white">
+            <a v-if="!authenticated" class="ml-2 py-2 md:ml-0 md:py-0" href="/login">Login</a>
+            <a v-else class="ml-2 py-2 md:ml-0 md:py-0" href="/logout">Logout</a>
+          </button>
         </nav>
       </div>
     </div>
@@ -29,4 +33,6 @@
 
 <script setup lang="ts">
 const collapsed = ref(true)
+const { session } = await useSession()
+const authenticated = computed(() => session.value?.uid !== undefined)
 </script>
