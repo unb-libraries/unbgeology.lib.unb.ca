3b9b5e5c2c8432a1133f5ba0dac7c66bafb656f3
Patrick Schilf <pschilf@unb.ca>
2023-05-09 15:53:09 -0300

    ROCKS-25 Validate user form fields

--- app/pages/users/register.vue
+++ app/pages/users/register.vue
@@ -5,26 +5,32 @@
     </h1>
   </section>
   <section class="container mx-auto my-4">
-    <div v-if="errorMessage" class="border-l-4 border-l-red-600 bg-red-200 p-3 text-red-800">
-      {{ errorMessage }}
-    </div>
     <form @submit.prevent="submit">
       <p class="my-4 flex flex-col space-y-2">
         <label for="username">Username</label>
-        <PvInputText v-model="username" placeholder="someone" class="max-w-md" />
+        <PvInputText v-model="username" placeholder="someone" class="max-w-md" :class="{ 'p-invalid': usernameError }" />
+        <small class="text-red-600">{{ usernameError || '&nbsp;' }}</small>
       </p>
       <p class="my-4 flex flex-col space-y-2">
         <label for="email">Email</label>
-        <PvInputText id="email" v-model="email" placeholder="someone@unb.ca" class="max-w-md" />
+        <PvInputText id="email" v-model="email" placeholder="someone@unb.ca" class="max-w-md" :class="{ 'p-invalid': emailError }" />
+        <small class="text-red-600">{{ emailError || '&nbsp;' }}</small>
       </p>
-      <button class="bg-black p-3 text-white" type="submit">
-        Submit
-      </button>
+      <p class="space-x-2">
+        <button class="border-2 border-black bg-black p-3 text-white disabled:cursor-not-allowed disabled:border-gray-400 disabled:bg-gray-400" type="submit" :disabled="!(!usernameError && !emailError)">
+          Submit
+        </button>
+        <button class="border-2 border-black p-3">
+          <a href="/users">Cancel</a>
+        </button>
+      </p>
     </form>
   </section>
 </template>
 
 <script setup lang="ts">
+import { useField } from 'vee-validate'
+
 definePageMeta({
   auth: {
     public: false,
@@ -32,23 +38,37 @@ definePageMeta({
   },
 })
 
-const username = ref(``)
-const email = ref(``)
-const errorMessage = ref(``)
+const { value: username, errorMessage: usernameError } = useField(`username`, async function (username: string) {
+  if (username) {
+    const { error } = await useFetch(`/api/users/${username}`)
+    if (!error.value) {
+      return `Username already exists`
+    }
+    return true
+  }
+  return true
+})
+
+const { value: email, errorMessage: emailError } = useField(`email`, function (email: string) {
+  if (email && !email.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i)) {
+    return `Invalid email address`
+  }
+  return true
+})
 
 const submit = async function () {
-  const { error } = await useFetch(`/api/users`, {
-    method: `POST`,
-    body: {
-      username: username.value,
-      email: email.value,
-    },
-  })
+  if (!usernameError.value && !emailError.value) {
+    const { error } = await useFetch(`/api/users`, {
+      method: `POST`,
+      body: {
+        username: username.value,
+        email: email.value,
+      },
+    })
 
-  if (!error.value) {
-    navigateTo(`/users`)
-  } else {
-    errorMessage.value = error.value.statusMessage as string
+    if (!error.value) {
+      navigateTo(`/users`)
+    }
   }
 }
 </script>
