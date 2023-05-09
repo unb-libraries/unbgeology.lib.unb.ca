40ae97aa755fce3003bd1ce269a70ab20e66bbcf
Patrick Schilf <pschilf@unb.ca>
2023-05-09 10:13:54 -0300

    ROCKS-27 Build basic table component

---
+++ app/components/Sy/Table.vue
@@ -0,0 +1,25 @@
+<template>
+  <PvTable :value="rows">
+    <PvColumn v-for="col of columns" :key="col.field" :header="col.header" :field="col.field" />
+  </PvTable>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  header: {
+    type: Object,
+    required: true,
+  },
+  rows: {
+    type: Array<Object>,
+    default: [],
+  },
+})
+
+const columns = computed(() => {
+  return Object.entries(props.header).map(([column, label]: [string, string]) => ({
+    header: label,
+    field: column,
+  }))
+})
+</script>
--- app/plugins/primevue.ts
+++ app/plugins/primevue.ts
@@ -1,5 +1,10 @@
 import PrimeVue from 'primevue/config'
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(PrimeVue)
+
+  nuxtApp.vueApp.component(`PvTable`, DataTable)
+  nuxtApp.vueApp.component(`PvColumn`, Column)
 })
