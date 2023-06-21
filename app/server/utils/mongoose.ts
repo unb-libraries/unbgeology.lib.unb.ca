a074f77809b3411ae0130e031f835c9270bfa5f4
Patrick Schilf <pschilf@unb.ca>
2023-06-21 14:24:49 -0300

    ROCKS-29 Fix created,updated timestamps

--- app/server/utils/mongoose.ts
+++ app/server/utils/mongoose.ts
@@ -2,12 +2,16 @@ import { type Entity } from "types/entity"
 import { model as defineModel, Schema, type SchemaDefinition } from "mongoose"
 
 export const defineEntityType = function<E extends Entity> (name: string, definition: SchemaDefinition<E>) {
-  const schema = new Schema<E>(definition, {
+  const schema = new Schema<E>({
+    ...definition,
+    created: Schema.Types.Number,
+    updated: Schema.Types.Number,
+  }, {
     id: false,
     timestamps: {
       createdAt: `created`,
       updatedAt: `updated`,
-      currentTime: () => Math.floor(Date.now() / 1000),
+      currentTime: () => Date.now(),
     },
   })
 
