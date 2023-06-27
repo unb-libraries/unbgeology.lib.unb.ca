3a8b8467ca85281d10b29e3eaa467539ebbef305
Patrick Schilf <pschilf@unb.ca>
2023-06-27 15:48:53 -0300

    ROCKS-31 Leaflet CSS

--- app/config/prod/nuxt.config.ts
+++ app/config/prod/nuxt.config.ts
@@ -2,6 +2,8 @@ export default defineNuxtConfig({
   buildDir: `/app/html/.build/.nuxt`,
   css: [
     `~/assets/css/main.css`,
+    // REFACTOR: only load this on pages with maps
+    `~/node_modules/leaflet/dist/leaflet.css`,
   ],
   postcss: {
     plugins: {
