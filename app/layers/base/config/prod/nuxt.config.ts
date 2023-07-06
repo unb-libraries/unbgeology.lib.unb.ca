6c9a45dbe6fa0a1c5fb779b467681cb89305e862
Patrick Schilf <pschilf@unb.ca>
2023-07-06 17:08:26 -0300

    ROCKS-24 Generate SAML metadata in 'nitro:init' hook

--- app/layers/base/config/prod/nuxt.config.ts
+++ app/layers/base/config/prod/nuxt.config.ts
@@ -1,9 +1,50 @@
+import { mkdirSync, writeFileSync } from "fs"
+import { SAML } from "@node-saml/node-saml"
 import { ValidateInResponseTo } from "@node-saml/node-saml/lib/types"
+import { createResolver } from "nuxt/kit"
 
-const { APP_ROOT } = process.env
+const {
+  APP_ROOT,
+  NUXT_PUBLIC_SAML_ENTRY_POINT,
+  NUXT_PUBLIC_SAML_CALLBACK_URL,
+  NUXT_PUBLIC_SAML_ISSUER,
+  NUXT_PUBLIC_SAML_CERT,
+} = process.env
 
 export default defineNuxtConfig({
+  hooks: {
+    'nitro:init': (nitro) => {
+      const { resolve } = createResolver(`/tmp`)
+
+      const samlDir = resolve(`saml`)
+      mkdirSync(samlDir, { recursive: true })
+
+      const saml = new SAML(nitro.options.runtimeConfig.public.saml)
+      writeFileSync(
+        resolve(`saml`, `metadata.xml`),
+        saml.generateServiceProviderMetadata(null),
+      )
+
+      nitro.options.publicAssets.push({
+        baseURL: `saml`,
+        dir: samlDir,
+        maxAge: 60 * 60 * 24 * 7, // 7 days
+      })
+    },
+  },
   nitro: {
+    runtimeConfig: {
+      public: {
+        saml: {
+          entryPoint: NUXT_PUBLIC_SAML_ENTRY_POINT || ``,
+          callbackUrl: NUXT_PUBLIC_SAML_CALLBACK_URL || ``,
+          issuer: NUXT_PUBLIC_SAML_ISSUER || ``,
+          cert: NUXT_PUBLIC_SAML_CERT || ``,
+          validateInResponseTo: ValidateInResponseTo.never,
+          disableRequestedAuthnContext: true,
+        },
+      },
+    },
     storage: {
       db: {
         driver: `fs`,
@@ -13,14 +54,6 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
     public: {
-      saml: {
-        entryPoint: ``,
-        callbackUrl: ``,
-        issuer: ``,
-        cert: ``,
-        validateInResponseTo: ValidateInResponseTo.never,
-        disableRequestedAuthnContext: true,
-      },
       session: {
         name: `sessionId`,
       },
--- app/layers/base/plugins/saml.server.ts
+++ app/layers/base/plugins/saml.server.ts
@@ -1,13 +1,7 @@
-import { writeFileSync } from "fs"
 import { SAML } from "@node-saml/node-saml"
-import { createResolver } from "nuxt/kit"
 
 export default defineNuxtPlugin(() => {
   const saml = new SAML(useRuntimeConfig().public.saml)
-
-  const { resolve } = createResolver(import.meta.url)
-  writeFileSync(resolve(`../public/saml/metadata.xml`), saml.generateServiceProviderMetadata(null))
-
   return {
     provide: {
       saml,
