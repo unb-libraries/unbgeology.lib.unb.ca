e7afc1aff1091472a34203eb84853d740066f897
Patrick Schilf <pschilf@unb.ca>
2023-08-28 15:02:49 -0300

    ROCKS-29 GET /api/buildings/:slug/rooms/:number

--- app/server/api/buildings/[slug]/rooms/index.get.ts
+++ app/server/api/buildings/[slug]/rooms/index.get.ts
@@ -1,10 +1,10 @@
 import Building from "~~/server/entityTypes/Building"
 import { type IRoom } from "~~/server/entityTypes/Room"
 
-export default defineEventHandler((async event => {
+export default defineEventHandler(async (event) => {
   const { slug } = getRouterParams(event)
   const { path } = event
-  
+
   const doc = await Building.findOne({ slug })
 
   if (!doc) {
@@ -15,4 +15,4 @@ export default defineEventHandler((async event => {
     self: `${path}/${room.number}`,
     ...room.toJSON(),
   }))
-}))
+})
