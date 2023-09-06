a5844aac3d51619bf32879d939ed893cb0e0a3b2
Patrick Schilf <pschilf@unb.ca>
2023-09-06 15:28:53 -0300

    ROCKS-37 Use discriminator model

--- app/server/api/taxonomies/[type]/index.get.ts
+++ app/server/api/taxonomies/[type]/index.get.ts
@@ -1,10 +1,19 @@
-import Taxonomy from "entity-types/Taxonomy"
+import { type ITaxonomy } from "entity-types/Taxonomy"
+import { type Model } from "mongoose"
 
 export default defineEventHandler(async (event) => {
   const { path } = event
   const { type } = getRouterParams(event)
 
-  const docs = await Taxonomy
+  const Discriminator: Model<ITaxonomy> | undefined = Object
+    .values(Taxonomy.discriminators || {})
+    .find(t => t.modelName.toLowerCase() === type)
+
+  if (!Discriminator) {
+    throw createError({ statusCode: 400, statusMessage: `Taxonomy ${type} does not exist.` })
+  }
+
+  const docs = await Discriminator
     .find()
     .select(`-_id -__v`)
 
--- app/server/api/taxonomies/[type]/index.post.ts
+++ app/server/api/taxonomies/[type]/index.post.ts
@@ -1,8 +1,18 @@
-import Taxonomy from "entity-types/Taxonomy"
+import { type ITaxonomy } from "entity-types/Taxonomy"
+import { type Model } from "mongoose"
 
 export default defineEventHandler(async (event) => {
   const { type } = getRouterParams(event)
   const { label, slug, ...other } = await readBody(event)
 
-  return await Taxonomy.create({ label, slug, __t: type, ...other })
+  const Discriminator: Model<ITaxonomy> | undefined = Object
+    .values(Taxonomy.discriminators || {})
+    .find(t => t.modelName.toLowerCase() === type)
+
+  if (!Discriminator) {
+    throw createError({ statusCode: 400, statusMessage: `Taxonomy ${type} does not exist.` })
+  }
+
+  return await Discriminator
+    .create({ label, slug, ...other })
 })
