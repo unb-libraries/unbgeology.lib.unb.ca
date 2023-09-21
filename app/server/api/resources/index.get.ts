f01564d4248a85118eb49ed6da1d51c45f4be11f
Patrick Schilf <pschilf@unb.ca>
2023-09-21 14:49:29 -0300

    ROCKS-37 Remove building, room entities as they are now taxonomies

--- app/server/api/resources/index.get.ts
+++
@@ -1,9 +0,0 @@
-export default defineEventHandler((event) => {
-  const { path } = event
-  return {
-    "self": path,
-    "storage-units": {
-      self: `${path}/storage-units`,
-    },
-  }
-})
