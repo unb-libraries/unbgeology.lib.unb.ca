9934bd1f93d4e9fade2c790c9d0fee6bdb5071ec
Patrick Schilf <pschilf@unb.ca>
2023-09-27 16:20:03 -0300

    ROCKS-34 Remove seed handlers

--- app/layers/local/server/api/seed/classifications/index.delete.ts
+++
@@ -1,7 +0,0 @@
-import Classification from "~/server/entityTypes/Classification"
-
-export default defineEventHandler(async (event) => {
-  await Classification.deleteMany()
-  setResponseStatus(event, 204, `Classification objects unseeded.`)
-  return null
-})
