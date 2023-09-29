721d712a7f43913b6939d4982b30b3afcea41b6d
Patrick Schilf <pschilf@unb.ca>
2023-09-29 13:38:04 -0300

    ROCKS-15 Update lightship

--- app/layers/base/server/plugins/lightship.ts
+++ app/layers/base/server/plugins/lightship.ts
@@ -1,13 +1,7 @@
+import { createLightship } from "lightship"
 import type { NitroApp } from "nitropack"
-import type { Lightship } from "lightship"
-
-// Lightship is CommonJS module
-// https://nodejs.org/api/esm.html#esm_interoperability_with_commonjs
-// eslint-disable-next-line import/default
-import lightship from "lightship"
-const { createLightship } = lightship
 
 export default defineNitroPlugin(async (nitroApp: NitroApp) => {
-  const lightship: Lightship = await createLightship()
+  const lightship = await createLightship({ detectKubernetes: false })
   lightship.signalReady()
 })
