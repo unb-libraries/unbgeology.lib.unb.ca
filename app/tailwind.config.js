b5cc5d916e2f29e4a5c96a9d66f9ca66426ce792
Patrick Schilf <pschilf@unb.ca>
2023-09-29 15:44:03 -0300

    ROCKS-30 Colour pallete

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -8,7 +8,23 @@ module.exports = {
     `./node_modules/primevue/**/*.{vue,js,ts,jsx,tsx}`,
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        base: `#E3E1D9`,
+        primary: {
+          DEFAULT: `#000000`,
+          20: `#B6B4AE`,
+          40: `#888782`,
+          60: `#5B5A57`,
+          80: `#2D2D2B`,
+        },
+        accent: {
+          light: `#73D0B9`,
+          mid: `#3FA085`,
+          dark: `#00695D`,
+        },
+      },
+    },
   },
   plugins: [],
 }
