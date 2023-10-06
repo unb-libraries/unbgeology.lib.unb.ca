995569b9d354e4f4ccf5cff76b74492293c89d3f
Patrick Schilf <pschilf@unb.ca>
2023-10-06 15:36:29 -0300

    ROCKS-30 Refactor taxonomy form,pages

--- app/layers/base/components/Form/TaxonomyTerm.vue
+++ app/layers/base/components/Form/TaxonomyTerm.vue
@@ -1,10 +1,10 @@
 <template>
-  <form @submit.prevent="onSubmit">
-    <div class="flex flex-col">
+  <form @submit.prevent="submit()">
+    <div class="mt-6 flex flex-col">
       <label class="mb-2 text-lg font-bold" for="label">Label</label>
       <PvInputText :id="`${uri}:label`" v-model="label" name="Label" />
     </div>
-    <div class="mr-3 flex w-1/2 flex-col">
+    <div class="my-6 flex flex-col">
       <label class="mb-2 text-lg font-bold" for="parent">Parent</label>
       <PvInputSelect :id="`${uri}:parent`" v-model="parent" name="parent" :options="terms" option-label="label" />
     </div>
@@ -13,7 +13,7 @@
       <button type="submit" class="bg-accent-dark dark:bg-accent-mid hover:bg-accent-light mr-2 rounded-md p-3 font-bold text-white">
         Save
       </button>
-      <a href="/" class="font-base ml-2 p-3">
+      <a href="#" class="font-base ml-2 p-3" @click.prevent="emits(`cancelled`)">
         Cancel
       </a>
     </div>
@@ -31,62 +31,33 @@ const props = defineProps<{
 const emits = defineEmits<{
   created: [taxonomy: Taxonomy],
   updated: [taxonomy: Taxonomy],
+  cancelled: [],
 }>()
 
 const label = ref(props.term?.label ?? ``)
 const parent = ref(props.term?.parent ?? ``)
-const terms = ref<Taxonomy[]>([])
+const { data: terms, refresh } = await useFetch(props.uri, { transform: (data: { items: Taxonomy[] }) => data.items, default: () => [] as Taxonomy[] })
 
-watch(() => props.uri, async (newUri) => {
-  if (newUri) {
-    const { data } = await useFetch<{ items: Taxonomy[] }>(newUri)
-    terms.value = data.value?.items ?? []
-  }
-}, { immediate: true })
-
-watch(() => props.term, async (newTerm) => {
-  label.value = newTerm?.label!
-  parent.value = newTerm?.parent!
+watch(() => props.term, (term) => {
+  label.value = term?.label ?? ``
+  parent.value = term?.parent ?? ``
 })
 
-const onSubmit = async function () {
-  const onCreate = async () => {
-    const { data: term } = await useFetch<Taxonomy>(props.uri, {
-      method: `POST`,
-      body: {
-        label: label.value,
-        parent: parent.value,
-      },
-    })
+const submit = async function () {
+  const uri = !props.term ? props.uri : props.term.self
+  const { data: term } = await useFetch<Taxonomy>(uri, {
+    method: !props.term ? `POST` : `PUT`,
+    body: {
+      label: label.value,
+      parent: parent.value,
+    },
+  })
 
-    if (term.value) {
-      label.value = ``
-      parent.value = ``
-      emits(`created`, term.value)
-      terms.value.push(term.value)
-    }
-  }
-
-  const onUpdate = async (uri: string) => {
-    const { data: updatedTerm } = await useFetch<Taxonomy>(uri, {
-      method: `PUT`,
-      body: {
-        label: label.value,
-        parent: parent.value,
-      },
-    })
-
-    if (updatedTerm.value) {
-      label.value = ``
-      parent.value = ``
-      emits(`updated`, updatedTerm.value)
-    }
-  }
-
-  if (props.term) {
-    onUpdate(props.term.self)
-  } else {
-    onCreate()
+  if (term.value && !props.term) {
+    emits(`created`, term.value)
+    refresh()
+  } else if (term.value) {
+    emits(`updated`, term.value)
   }
 }
 </script>
