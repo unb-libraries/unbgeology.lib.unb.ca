f95779c2510cefe46c04c57f848b7d2800d7ff37
Patrick Schilf <pschilf@unb.ca>
2023-10-17 13:39:57 -0300

    ROCKS-30 Refactor specimen,taxonomy dashboard using entity table

--- app/layers/primevue/components/PvEntityTable.vue
+++ app/layers/primevue/components/PvEntityTable.vue
@@ -7,15 +7,15 @@
     </template>
     <PvTableColumn v-for="[column, header] in columns" :key="column" :field="column" :header="header">
       <template #body="slotProps">
-        <slot :name="column" :value="slotProps.data[column]" :entity="slotProps.data">
-          {{ column in slotProps.data ? slotProps.data[column] : '' }}
+        <slot v-if="column in slotProps.data && slotProps.data[column] !== undefined" :name="column" :value="slotProps.data[column]" :entity="slotProps.data">
+          {{ slotProps.data[column] }}
         </slot>
       </template>
     </PvTableColumn>
     <PvTableColumn field="self">
       <template #body="{ data }">
         <div class="invisible flex flex-row justify-end group-hover:visible">
-          <a :href="`/dashboard/${data.self.substring(5)}`" :title="`Edit ${label(data) ?? `item`}`" class="h-8 w-8 rounded-md p-1 hover:cursor-pointer hover:bg-yellow-400">
+          <a :href="`/dashboard/${data.self.substring(5)}`" :title="`Edit ${label(data) ?? `item`}`" class="h-8 w-8 rounded-md p-1 hover:cursor-pointer hover:bg-yellow">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -26,7 +26,7 @@
               <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
             </svg>
           </a>
-          <a :title="`Remove ${label(data) ?? `item`}`" class="h-8 w-8 rounded-md p-1 hover:cursor-pointer hover:bg-red-500" @click.prevent="onDelete(data.self)">
+          <a :title="`Remove ${label(data) ?? `item`}`" class="h-8 w-8 rounded-md p-1 hover:cursor-pointer hover:bg-red" @click.prevent="onDelete(data.self)">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
