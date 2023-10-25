8f231ab82ba1b69fbe8e750dece7a51ef416510f
Patrick Schilf <pschilf@unb.ca>
2023-10-25 14:22:04 -0300

    ROCKS-28 File API endpoints to be sortable,selectable,pageable

--- app/server/api/files/[type].get.ts
+++ app/server/api/files/[type].get.ts
@@ -2,6 +2,12 @@ import { File as FileEntity, type File } from "~/server/entityTypes/File"
 
 export default defineEventHandler(async (event) => {
   const { type } = getRouterParams(event)
+  const { select, sort, page, pageSize } = getQueryOptions(event)
+
   const files = await FileEntity.find({ type })
+    .select(getSelectedFields(select))
+    .sort(sort.join(` `))
+    .skip((page - 1) * pageSize)
+    .limit(pageSize)
   return sendEntityList<File>(event, files)
 })
--- app/server/api/files/index.get.ts
+++ app/server/api/files/index.get.ts
@@ -1,6 +1,13 @@
 import { File as FileEntity, type File } from "~/server/entityTypes/File"
 
 export default defineEventHandler(async (event) => {
+  const { select, sort, page, pageSize } = getQueryOptions(event)
+
   const files = await FileEntity.find()
+    .select(getSelectedFields(select))
+    .sort(sort.join(` `))
+    .skip((page - 1) * pageSize)
+    .limit(pageSize)
+
   return sendEntityList<File>(event, files)
 })
