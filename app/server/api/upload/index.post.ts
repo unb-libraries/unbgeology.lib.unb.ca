c1153f28c735eec8739f75e56f5c62a8a1ed1e41
Patrick Schilf <pschilf@unb.ca>
2023-10-31 12:34:17 -0300

    ROCKS-36 Optionally persist files upon upload

--- app/server/api/upload/index.post.ts
+++ app/server/api/upload/index.post.ts
@@ -1,17 +1,45 @@
+import fs from "fs/promises"
 import { readFiles } from "h3-formidable"
 import { File as FileEntity, File } from "~/server/entityTypes/File"
 
+const { APP_ROOT } = process.env
+
 export default defineEventHandler(async (event) => {
   const upload = await readFiles(event, {
+    includeFields: true,
     keepExtensions: true,
   })
 
-  const files = await Promise.all(Object.values(upload).map(files => files?.map(async (file) => {
-    const { filepath, newFilename, originalFilename, mimetype, size } = file
+  const { fields } = upload
+  const persisted = fields.persisted && Array.isArray(fields.persisted) && Boolean(fields.persisted[0])
+  const files = await Promise.all(Object.values(upload.files).map(files => files?.map(async (file) => {
+    const { originalFilename, mimetype, size } = file
+    let { filepath, newFilename } = file
+    const type = mimetype?.split(`/`)[0] ?? `file`
+
+    if (persisted) {
+      const dir = `${APP_ROOT}/public/${type !== `file` ? type : ``}`
+      try {
+        await fs.mkdir(dir)
+      } catch (err: any) {
+        if (err.code !== `EEXIST`) {
+          throw err
+        }
+      }
+
+      const newFilepath = `${dir}/${originalFilename ?? newFilename}`
+      await fs.copyFile(filepath, newFilepath)
+      await fs.rm(filepath)
+
+      filepath = newFilepath
+      newFilename = originalFilename ?? newFilename
+    }
+
     return await FileEntity.create({
       uploadName: originalFilename,
-      filename: newFilename,
+      filename: originalFilename,
       filepath,
+      persisted,
       filetype: originalFilename?.split(`.`).at(-1),
       filesize: size,
       type: mimetype?.split(`/`).at(0),
