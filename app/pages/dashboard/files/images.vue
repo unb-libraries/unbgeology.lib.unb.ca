658982f2d3ec0bbc6f742ddfe4deb37b6730d848
Patrick Schilf <pschilf@unb.ca>
2023-11-01 17:21:43 -0300

    ROCKS-36 Update dashboard/image

--- app/pages/dashboard/files/images.vue
+++ app/pages/dashboard/files/images.vue
@@ -1,46 +1,14 @@
 <template>
-  <div class="grid grid-cols-12 gap-2">
-    <div v-for="(image, index) in list?.entities" :key="image.self" class="bg-primary-80 aspect-square hover:cursor-pointer hover:grayscale">
-      <img :src="`/image/${image.filename}`" alt="Test">
-    </div>
-  </div>
-  <div class="bg-primary-80 mt-2 p-8 text-center" @drop.prevent="onDrop" @dragover.prevent="">
-    Drop files here.
-  </div>
+  <PvImageGallery :images="list?.entities ?? []" />
+  <PvFileUpload @uploaded="refresh()" />
 </template>
 
 <script setup lang="ts">
-import { type File } from '~/server/entityTypes/File'
+import { type Image } from "layers/base/types/entity"
 
 definePageMeta({
   layout: `dashboard`,
 })
 
-const onDrop = async function ({ dataTransfer }: DragEvent) {
-  console.log(`Files dropped...`)
-  if (dataTransfer?.items) {
-    for (const item of dataTransfer.items) {
-      const file = item.getAsFile()
-      if (file) {
-        formData.append(`files`, file)
-      }
-    }
-  } else if (dataTransfer?.files) {
-    for (const file of dataTransfer.files) {
-      formData.append(`files`, file)
-    }
-  }
-
-  await useFetch(`/api/upload`, {
-    method: `POST`,
-    body: formData,
-  })
-  formData.delete(`files`)
-  refresh()
-}
-
-const { list, refresh } = await fetchEntityList<File>(`/api/files`)
-const formData = new FormData()
-formData.append(`persisted`, `true`)
-
+const { list, refresh } = await fetchEntityList<Image>(`/api/files`)
 </script>
