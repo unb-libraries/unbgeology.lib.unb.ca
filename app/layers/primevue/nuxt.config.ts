088fd6e782d3ca3e382523493b35335ce93b2ab6
Patrick Schilf <pschilf@unb.ca>
2023-11-01 16:41:09 -0300

    ROCKS-36 Install @nuxt/image module

--- app/layers/primevue/nuxt.config.ts
+++ app/layers/primevue/nuxt.config.ts
@@ -5,6 +5,9 @@ export default defineNuxtConfig({
     transpile: [`primevue`],
   },
   buildDir: `./.build/.nuxt`,
+  modules: [
+    `@nuxt/image`,
+  ],
   postcss: {
     plugins: {
       tailwindcss: {},
