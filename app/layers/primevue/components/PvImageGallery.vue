21ee378737498b5115a2fb2b5d8ad6f586856ffb
Patrick Schilf <pschilf@unb.ca>
2023-11-01 17:11:33 -0300

    ROCKS-36 Use NuxtImage

--- app/layers/primevue/components/PvImageGallery.vue
+++ app/layers/primevue/components/PvImageGallery.vue
@@ -1,13 +1,22 @@
 <template>
-  <div class="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-8 lg:grid-cols-10 xl:grid-cols-12 gap-2">
+  <div class="grid grid-cols-2 gap-2 sm:grid-cols-4 md:grid-cols-8 lg:grid-cols-10 xl:grid-cols-12">
     <div v-for="image in images" :key="image.self" class="bg-primary-20 dark:bg-primary-80 aspect-square hover:cursor-pointer hover:grayscale">
-      <img :src="`/image/${image.filename}`" :alt="image.alt" :title="image.title" />
+      <nuxt-img
+        :src="`/image/${image.filename}`"
+        :alt="image.alt"
+        :title="image.title"
+        format="webp"
+        fit="cover"
+        sizes="sm:180px md:90px lg:60px xl:100px"
+        width="600"
+        height="600"
+      />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { type EntityJSON, type Image } from 'layers/base/types/entity';
+import { type EntityJSON, type Image } from 'layers/base/types/entity'
 
 defineProps<{
   images: EntityJSON<Image>[]
