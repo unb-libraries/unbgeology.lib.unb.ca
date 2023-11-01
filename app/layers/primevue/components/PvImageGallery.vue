5ccfa9c6b75eff360973f5d768f63b3e8d41174f
Patrick Schilf <pschilf@unb.ca>
2023-11-01 16:23:34 -0300

    ROCKS-36 Reusable image gallery component

---
+++ app/layers/primevue/components/PvImageGallery.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-8 lg:grid-cols-10 xl:grid-cols-12 gap-2">
+    <div v-for="image in images" :key="image.self" class="bg-primary-20 dark:bg-primary-80 aspect-square hover:cursor-pointer hover:grayscale">
+      <img :src="`/image/${image.filename}`" :alt="image.alt" :title="image.title" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { type EntityJSON, type Image } from 'layers/base/types/entity';
+
+defineProps<{
+  images: EntityJSON<Image>[]
+}>()
+</script>
