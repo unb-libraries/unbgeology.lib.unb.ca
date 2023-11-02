c121fa5a2a29f9e53f8697fbd0890b3722b4a9fe
Patrick Schilf <pschilf@unb.ca>
2023-11-02 10:03:44 -0300

    ROCKS-30 Image gallery image selection

--- app/layers/primevue/components/PvImageGallery.vue
+++ app/layers/primevue/components/PvImageGallery.vue
@@ -5,11 +5,13 @@
         :src="`/image/${image.filename}`"
         :alt="image.alt"
         :title="image.title"
+        :class="{ 'border-accent-mid border-4': selection.includes(image)}"
         format="webp"
         fit="cover"
         sizes="sm:180px md:90px lg:60px xl:100px"
         width="600"
         height="600"
+        @click.stop="onClick(image)"
       />
     </div>
   </div>
@@ -18,7 +20,35 @@
 <script setup lang="ts">
 import { type EntityJSON, type Image } from 'layers/base/types/entity'
 
-defineProps<{
+const props = defineProps<{
   images: EntityJSON<Image>[]
+  multi?: boolean
 }>()
+
+const emits = defineEmits<{
+  selected: [img: EntityJSON<Image>]
+  unselected: [img: EntityJSON<Image>]
+}>()
+
+function onClick(img: EntityJSON<Image>) {
+  const multi = props.multi ?? false
+  if (!selection.value.includes(img)) {
+    if (multi) {
+      selection.value.push(img)
+    } else {
+      selection.value = [img]
+    }
+    emits(`selected`, img)
+  } else {
+    if (multi) {
+      const index = selection.value.findIndex(i => i === img)
+      selection.value.splice(index, 1)
+    } else {
+      selection.value = []
+    }
+    emits(`unselected`, img)
+  }
+}
+
+const selection = ref([] as EntityJSON<Image>[])
 </script>
