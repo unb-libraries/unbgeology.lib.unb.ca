7f582f7cf313f1860457fa321caa6749b46c77af
Patrick Schilf <pschilf@unb.ca>
2023-11-02 10:17:46 -0300

    ROCKS-30 Display large selected image

--- app/pages/dashboard/files/images.vue
+++ app/pages/dashboard/files/images.vue
@@ -1,14 +1,31 @@
 <template>
-  <PvImageGallery :images="list?.entities ?? []" />
+  <nuxt-img
+    v-if="selectedImage"
+    :src="`/image/${selectedImage.filename}`"
+    :alt="selectedImage.alt"
+    :title="selectedImage.title"
+    class="mb-2"
+  />
+  <PvImageGallery :images="list?.entities ?? []" @selected="onSelect" @unselected="onUnselect" />
   <PvFileUpload @uploaded="refresh()" />
 </template>
 
 <script setup lang="ts">
-import { type Image } from "layers/base/types/entity"
+import { EntityJSON, type Image } from "layers/base/types/entity"
 
 definePageMeta({
   layout: `dashboard`,
 })
+
+const selectedImage = ref<EntityJSON<Image>>()
+
+function onSelect(image: EntityJSON<Image>) {
+  selectedImage.value = image
+}
+
+function onUnselect() {
+  selectedImage.value = undefined
+}
 
 const { list, refresh } = await fetchEntityList<Image>(`/api/files`)
 </script>
