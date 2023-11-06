52cbd0f618918994972aac0f9133fda7796f0a7c
Patrick Schilf <pschilf@unb.ca>
2023-11-06 14:13:55 -0400

    ROCKS-29 Populate all referenced specimen fields

--- app/server/api/specimens/[slug]/index.get.ts
+++ app/server/api/specimens/[slug]/index.get.ts
@@ -2,22 +2,17 @@ import { type Specimen } from "types/specimen"
 
 export default defineEventHandler(async (event) => {
   const { slug } = getRouterParams(event)
-  const { select } = getQueryOptions(event)
-
-  function getSelectedClassificationFields() {
-    const fields = getSelectedFields(select, `classifications`)
-    return fields.length > 0 ? fields : [`_id`]
-  }
-
-  function getSelectedImageFields() {
-    const fields = getSelectedFields(select, `images`)
-    return fields.length > 0 ? fields : [`_id`]
-  }
+  const { select, filterSelect } = getQueryOptions(event)
 
   const specimen = await Specimen.findByPK(slug)
-    .populate(`classifications`, getSelectedClassificationFields())
-    .populate(`images`, getSelectedImageFields())
     .select(getSelectedFields(select))
+    .populate(`images`, filterSelect({ root: `images`, default: [`_id`] }))
+    .populate(`classifications`, filterSelect({ root: `classifications`, default: [`_id`] }))
+    .populate(`collector`, filterSelect({ root: `collector`, default: [`_id`] }))
+    .populate(`sponsor`, filterSelect({ root: `sponsor`, default: [`_id`] }))
+    .populate(`loans.organization`, filterSelect({ root: `loans.organization` }))
+    .populate(`storage.location`, filterSelect({ root: `storage.location` }))
+    .populate(`editor`, filterSelect({ root: `editor`, default: [`_id`, `username`] }))
 
   return sendEntityOr404<Specimen>(event, specimen)
 })
