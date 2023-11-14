f4acca25e8f73478c902b71e583de691be5de5aa
Patrick Schilf <pschilf@unb.ca>
2023-11-14 14:29:22 -0400

    ROCKS-30 Modal component

---
+++ app/layers/primevue/components/PvModal.vue
@@ -0,0 +1,9 @@
+<template>
+  <Teleport to="body">
+    <div class="bg-primary-60/75 fixed left-0 top-0 z-[10000] h-screen w-screen">
+      <div class="bg-primary absolute left-1/4 top-1/4 w-1/2 rounded-lg p-8">
+        <slot />
+      </div>
+    </div>
+  </Teleport>
+</template>
