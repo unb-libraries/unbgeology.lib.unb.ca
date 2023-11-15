60eee0f3de31551623d715fcb411f719adf42092
Patrick Schilf <pschilf@unb.ca>
2023-11-15 12:26:38 -0400

    ROCKS-30 Entity list component

---
+++ app/layers/primevue/components/PvEntityList.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul>
+    <slot name="before" />
+    <li v-for="entity in entities" :key="entity.self" :class="itemClass">
+      <slot :entity="entity">
+        <span v-if="label && typeof label === 'string' && label in entity">{{ entity[label as keyof E] }}</span>
+        <span v-else-if="label && typeof label === 'function'">{{ label(entity) }}</span>
+      </slot>
+    </li>
+    <slot name="after" />
+  </ul>
+</template>
+
+<script setup lang="ts" generic="E extends Entity = Entity">
+import { type EntityJSON, type Entity } from 'layers/base/types/entity'
+
+defineProps<{
+  entities: EntityJSON<E>[]
+  itemClass?: string
+  label?:((entity: EntityJSON<E>) => string) | keyof E
+}>()
+</script>
