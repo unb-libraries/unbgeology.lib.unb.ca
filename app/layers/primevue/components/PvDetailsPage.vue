826034f3e0a67c54c312f96693e66658dc29324f
Patrick Schilf <pschilf@unb.ca>
2023-11-16 16:19:05 -0400

    ROCKS-30 Details page component

---
+++ app/layers/primevue/components/PvDetailsPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex h-full flex-col space-y-4">
+    <div class="flex-none">
+      <slot name="nav">
+        <button class="hover:border-accent-mid hover:text-accent-mid w-full rounded-md border p-1" @click="emits(`unstack`)">
+          Back
+        </button>
+      </slot>
+    </div>
+    <div class="grow">
+      <slot />
+    </div>
+    <div class="flex-none empty:hidden">
+      <slot name="actions" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const emits = defineEmits<{
+  unstack: []
+}>()
+</script>
