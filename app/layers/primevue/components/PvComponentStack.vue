218d17b6b50129ae82abc67c5390551aef194bda
Patrick Schilf <pschilf@unb.ca>
2023-11-16 15:29:19 -0400

    ROCKS-30 Component stack component for sidebar navigation

---
+++ app/layers/primevue/components/PvComponentStack.vue
@@ -0,0 +1,30 @@
+<template>
+  <component :is="stack.at(-1)![0]" v-if="stack.length > 0" @stack="onStack" @unstack="onUnstack" />
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  stack?: [any, any][]
+}>()
+
+const stack = ref(props.stack ?? [])
+provideContext()
+
+onBeforeUpdate(() => {
+  provideContext()
+})
+
+function provideContext() {
+  provide(`context`, stack.value.at(-1)![1])
+}
+
+function onStack(component: any, context: any) {
+  stack.value.push([component, context])
+}
+
+function onUnstack() {
+  if (stack.value.length > 0) {
+    stack.value.splice(stack.value.length - 1, 1)
+  }
+}
+</script>
