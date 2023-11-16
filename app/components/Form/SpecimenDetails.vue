6a3e508b985633d4731991dbcfeeda095d1a271b
Patrick Schilf <pschilf@unb.ca>
2023-11-16 16:25:00 -0400

    ROCKS-30 Specimen details

---
+++ app/components/Form/SpecimenDetails.vue
@@ -0,0 +1,23 @@
+<template>
+  <PvDetailsPage v-if="specimen">
+    <template #nav>
+      <!-- Workaround for removing navigation. -->
+      <div />
+    </template>
+    <PvEntityDetails :entity="specimen!" :fields="[`editor`, `created`, [`updated`, `Last updated`], `status`]" item-class="my-2 first:mt-0 last:mb-0" label-class="text-md text-primary-60">
+      <template #editor="{ value }">
+        {{ value.username }}
+      </template>
+    </PvEntityDetails>
+    <template #actions>
+      <button class="border-red text-red hover:bg-red w-full rounded-md border p-1 hover:text-white">
+        Delete
+      </button>
+    </template>
+  </PvDetailsPage>
+</template>
+
+<script setup lang="ts">
+import { type EntityJSON } from 'layers/base/types/entity'
+const specimen = inject(`context`)
+</script>
--- app/pages/dashboard/specimens/[slug].vue
+++ app/pages/dashboard/specimens/[slug].vue
@@ -1,19 +1,37 @@
 <template>
-  <FormSpecimen v-if="specimen" :specimen="specimen" @save="onSave" />
+  <NuxtLayout name="dashboard">
+    <template #page-title>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="mr-2 h-5 w-5"
+      >
+        <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+      </svg>
+      <span class="font-bold italic">{{ specimen?.name }}</span>
+    </template>
+    <template #sidebar>
+      <PvComponentStack :stack="[[FormSpecimenDetails, specimen]]" />
+    </template>
+    <FormSpecimen :specimen="specimen!" @save="onSave" />
+  </NuxtLayout>
 </template>
 
 <script setup lang="ts">
 import { type Specimen } from 'types/specimen'
 import { type EntityJSONBody } from 'layers/base/types/entity'
+import { FormSpecimenDetails } from "#components"
 
 definePageMeta({
-  layout: `dashboard`,
+  layout: false,
 })
 
 const { slug } = useRoute().params
 const { fetchByPK, update } = useEntityType<Specimen>(Symbol(`specimens`))
 const { entity: specimen } = await fetchByPK(slug as string)
-
 if (!specimen.value) {
   showError({ statusCode: 404 })
 }
