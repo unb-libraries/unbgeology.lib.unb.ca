de8760626fc131e5963d84c51ea1322302916d93
Patrick Schilf <pschilf@unb.ca>
2023-11-17 13:46:33 -0400

    ROCKS-30 Confirm modal component

--- app/layers/primevue/components/PvModal.vue
+++ app/layers/primevue/components/PvModal.vue
@@ -1,9 +1,16 @@
 <template>
   <Teleport to="body">
-    <div class="bg-primary-60/75 fixed left-0 top-0 z-[10000] h-screen w-screen">
-      <div class="bg-primary absolute left-1/4 top-1/4 w-1/2 rounded-lg p-8">
+    <div class="bg-primary-60/75 fixed left-0 top-0 z-[1000] h-screen w-screen" :class="overlayClass">
+      <div class="bg-primary max-h-4/5 absolute left-1/4 top-12 w-1/2 overflow-y-scroll rounded-lg p-8" :class="modalClass">
         <slot />
       </div>
     </div>
   </Teleport>
 </template>
+
+<script setup lang="ts">
+defineProps<{
+  overlayClass?: string
+  modalClass?: string
+}>()
+</script>
---
+++ app/layers/primevue/components/PvModalConfirm.vue
@@ -0,0 +1,32 @@
+<template>
+  <PvModal :modal-class="modalClass">
+    <p :class="contentClass">
+      <slot />
+    </p>
+    <div class="text-center" :class="actionsClass">
+      <button class="bg-red mr-2 w-24 rounded-md p-2" :class="confirmClass" @click="emits(`confirm`)">
+        {{ confirmLabel ?? `Yes` }}
+      </button>
+      <button class="bg-accent-mid mr-2 w-24 rounded-md p-2" :class="cancelClass" @click="emits(`cancel`)">
+        {{ cancelLabel ?? `No` }}
+      </button>
+    </div>
+  </PvModal>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  modalClass?: string
+  contentClass?: string
+  confirmLabel?: string
+  confirmClass?: string
+  cancelLabel?: string
+  cancelClass?: string
+  actionsClass?: string
+}>()
+
+const emits = defineEmits<{
+  confirm: []
+  cancel: []
+}>()
+</script>
