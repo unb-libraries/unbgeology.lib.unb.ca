244778b4bf0801105f6c082c1df7b6f8cadbc728
Patrick Schilf <pschilf@unb.ca>
2023-12-11 16:13:37 -0400

    ROCKS-30 Use default actions on /dashboard/terms page

--- app/pages/dashboard/terms/index.vue
+++ app/pages/dashboard/terms/index.vue
@@ -4,9 +4,12 @@
       <h2 class="mb-6 text-2xl">
         Geochronology
       </h2>
-      <PvEntityTable entity-type-id="Geochronology" :columns="[`label`, `division`]">
+      <PvEntityTable :entities="units" :columns="[`label`, `division`, [`actions`, ``]]">
         <template #division="{ value: division }">
           {{ division.substring(0, 1).toUpperCase() + division.substring(1).toLowerCase() }}
+        </template>
+        <template #actions="{ entity: term }">
+          <PvDefaultEntityTableActions :entity="term" :form="GeochronologyForm" :update="updateUnit" :remove="removeUnit" class="invisible group-hover:visible" />
         </template>
       </PvEntityTable>
     </section>
@@ -15,20 +18,50 @@
       <h2 class="mb-6 text-2xl">
         Classifications
       </h2>
-      <PvEntityTable entity-type-id="Classification" :columns="[`label`]" />
+      <PvEntityTable :entities="classifications" :columns="[`label`, [`actions`, ``]]">
+        <template #actions="{ entity: term }">
+          <PvDefaultEntityTableActions
+            :entity="term"
+            :form="TaxonomyTermForm"
+            :form-props="{ parents: classifications }"
+            :update="updateClassification"
+            :remove="removeClassification"
+            class="invisible group-hover:visible"
+          />
+        </template>
+      </PvEntityTable>
     </section>
 
     <section>
       <h2 class="mb-6 text-2xl">
         Storage locations
       </h2>
-      <PvEntityTable entity-type-id="StorageLocation" :columns="[`label`]" />
+      <PvEntityTable :entities="locations" :columns="[`label`, [`actions`, ``]]">
+        <template #actions="{ entity: term }">
+          <PvDefaultEntityTableActions
+            :entity="term"
+            :form="StorageLocationForm"
+            :form-props="{ parents: locations }"
+            :update="updateLocation"
+            :remove="removeLocation"
+            class="invisible group-hover:visible"
+          />
+        </template>
+      </PvEntityTable>
     </section>
   </div>
 </template>
 
 <script setup lang="ts">
+import { type Unit } from 'types/vocabularies/geochronology'
+import { type Classification, type StorageLocation } from 'types/vocabularies'
+import { GeochronologyForm, TaxonomyTermForm, StorageLocationForm } from '#components'
+
 definePageMeta({
   layout: `dashboard`,
 })
+
+const { entities: units, update: updateUnit, remove: removeUnit } = await fetchEntityList<Unit>(`Geochronology`)
+const { entities: classifications, update: updateClassification, remove: removeClassification } = await fetchEntityList<Classification>(`Classification`)
+const { entities: locations, update: updateLocation, remove: removeLocation } = await fetchEntityList<StorageLocation>(`StorageLocation`)
 </script>
