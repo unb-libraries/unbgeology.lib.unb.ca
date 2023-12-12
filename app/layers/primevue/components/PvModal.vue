96184f45f19439b21a5168c69200e570c85775da
Patrick Schilf <pschilf@unb.ca>
2023-12-12 12:36:07 -0400

    Place modal in screen center

--- app/layers/primevue/components/PvModal.vue
+++ app/layers/primevue/components/PvModal.vue
@@ -1,7 +1,7 @@
 <template>
   <Teleport v-if="isOpen" to="body">
     <div class="bg-primary-60/75 fixed left-0 top-0 z-[1000] h-screen w-screen" :class="overlayClass" @click="close()">
-      <div class="bg-primary max-h-4/5 absolute left-1/4 top-12 w-1/2 overflow-y-scroll rounded-lg p-8" :class="modalClass" @click.stop="">
+      <div class="bg-primary max-h-4/5 max-w-4/5 absolute left-1/2 top-1/2 w-fit -translate-x-1/2 -translate-y-1/2 overflow-y-scroll rounded-lg p-8" :class="modalClass" @click.stop="">
         <component :is="content!.component" v-bind="content!.props" v-on="content!.eventHandlers" />
       </div>
     </div>
