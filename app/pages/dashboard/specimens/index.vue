7721ed9af66e99defe6c3540f4132d1fd0321d31
Patrick Schilf <pschilf@unb.ca>
2023-12-13 13:00:57 -0400

    ROCKS-30 Update specimen list page

--- app/pages/dashboard/specimens/index.vue
+++ app/pages/dashboard/specimens/index.vue
@@ -1,29 +1,66 @@
 <template>
-  <PvEntityTable entity-type-id="Specimen" :columns="[`name`, `measurements`, `age`, `pieces`, `partial`, `created`, `updated`, `status`]">
-    <template #name="{ value, entity }">
-      <NuxtLink :to="`/dashboard/${entity.self.substring(5)}`" class="hover:underline">
-        {{ value }}
+  <div class="flex flex-row justify-between">
+    <h1 class="mb-6 text-2xl">
+      Specimens
+    </h1>
+    <div class="space-x-2">
+      <NuxtLink to="/dashboard/specimens/create/?type=fossil" class="form-action form-action-submit p-2 font-normal">
+        Create Fossil
+      </NuxtLink>
+      <NuxtLink to="/dashboard/specimens/create/?type=mineral" class="form-action form-action-submit p-2 font-normal">
+        Create Mineral
+      </NuxtLink>
+      <NuxtLink to="/dashboard/specimens/create/?type=rock" class="form-action form-action-submit p-2 font-normal">
+        Create Rock
+      </NuxtLink>
+    </div>
+  </div>
+  <PvEntityTable :entities="specimens" :columns="[[`objectID`, `ID`], `type`, `classification`, `measurements`, `age`, `portion`, `pieces`, `partial`]">
+    <template #objectID="{ value: objectID }">
+      <NuxtLink :to="`/dashboard/specimens/${objectID.unb.toLowerCase()}`" class="hover:underline">
+        {{ objectID.unb }}
       </NuxtLink>
     </template>
+    <template #type="{ value: type }">
+      {{ type.substring(0, 1).toUpperCase() + type.substring(1).toLowerCase() }}
+    </template>
+    <template #classification="{ value: classification }">
+      <span v-if="classification">{{ classification.label }}</span>
+    </template>
     <template #measurements="{ value: measurements }">
-      <ul v-for="dimensions in measurements" :key="dimensions">
-        <li>{{ dimensions.width }}mm x {{ dimensions.length }}mm</li>
-      </ul>
+      <template v-if="Array.isArray(measurements) && measurements.length > 0">
+        <ul v-for="dimensions in measurements" :key="dimensions">
+          <li>{{ dimensions.width }}mm x {{ dimensions.length }}mm</li>
+        </ul>
+      </template>
+      <span v-else />
     </template>
     <template #age="{ value: age }">
-      {{ age.numeric ? age.numeric : `${age.relative.boundaries.lower} - ${age.relative.boundaries.upper}` }} Ma ({{ age.relative.label }})
+      <span v-if="age && age.numeric">
+        {{ age.numeric }} Ma
+      </span>
+      <span v-if="age">
+        {{ age.relative.boundaries.lower }} - {{ age.relative.boundaries.upper }} Ma ({{ age.relative.label }})
+      </span>
     </template>
-    <template #partial="{ value }">
-      {{ value ? `Yes` : `No` }}
+    <template #portion="{ value: portion }">
+      <span v-if="portion">{{ portion.label }}</span>
     </template>
-    <template #status="{ value }">
-      <span class="rounded-md p-2 text-xs font-bold uppercase" :class="{ 'bg-yellow': value === 'draft', 'bg-blue': value === 'review', 'bg-accent-dark': value === 'published' }">{{ value }}</span>
+    <template #partial="{ value: partial }">
+      <span v-if="partial">
+        {{ partial ? `Yes` : `No` }}
+      </span>
+      <span v-else />
     </template>
   </PvEntityTable>
 </template>
 
 <script setup lang="ts">
+import { type Specimen } from 'types/specimen'
+
 definePageMeta({
   layout: `dashboard`,
 })
+
+const { entities: specimens } = await fetchEntityList<Specimen>(`Specimen`)
 </script>
