5231ca30a799e7f2bebaa0108ee62b82bd699b5b
Patrick Schilf <pschilf@unb.ca>
2024-01-02 10:29:50 -0400

    ROCKS-36 DELETE /api/files/:type/:id to remove actual file

--- app/layers/mongo/server/api/files/[type]/[id].delete.ts
+++ app/layers/mongo/server/api/files/[type]/[id].delete.ts
@@ -1,7 +1,12 @@
+import { rm as removeFile } from "fs/promises"
+
 export default defineEventHandler(async (event) => {
   const { type, id } = getRouterParams(event)
 
   const File = useFileDocumentType(type ?? `file`)
-  const file = await File.findOneAndDelete({ _id: id })
+  const file = await File.findOne({ _id: id })
+  await removeFile(file.filepath, { force: true })
+  await File.deleteOne({ _id: file._id })
+
   return sendNoContent(event)
 })
