0436824c401644fbfcde0ec89d1d1321a910f582
Patrick Schilf <pschilf@unb.ca>
2024-01-05 15:33:53 -0400

    ROCKS-38 Import items on POST /api/migrations

--- app/layers/mongo/server/api/migrations/[id]/index.put.ts
+++ app/layers/mongo/server/api/migrations/[id]/index.put.ts
@@ -1,33 +1,10 @@
-import { readFile } from "fs/promises"
-import { MigrationStatus } from "@unb-libraries/nuxt-layer-entity"
-
 export default defineEventHandler(async (event) => {
   const { id } = getRouterParams(event)
 
   const body = await readEntityBody(event, migrationBodyReader)
-
-  const original = await Migration.findByPK(id)
-  let updated = await Migration
+  const migration = await Migration
     .findByPKAndUpdate(id, body, { new: true })
     .populate(`source`)
 
-  if (original.status !== updated.status && updated.status === MigrationStatus.RUNNING) {
-    const data = JSON.parse(await readFile(updated.source.filepath, { encoding: `utf8` }))
-    if (Array.isArray(data)) {
-      await Promise.all(data.map(async (record) => {
-        const { id: sourceId, ...body } = record
-        const uri = useAppConfig().entityTypes[updated.entityType].baseURI
-        await $fetch(uri, {
-          method: `POST`,
-          body,
-        })
-      }))
-
-      updated = await Migration
-        .findByPKAndUpdate(id, { status: MigrationStatus.SUCCEDED }, { new: true })
-        .populate(`source`)
-    }
-  }
-
-  return sendEntityOr404(event, updated)
+  return sendEntityOr404(event, migration)
 })
--- app/layers/mongo/server/api/migrations/index.post.ts
+++ app/layers/mongo/server/api/migrations/index.post.ts
@@ -1,5 +1,6 @@
 export default defineEventHandler(async (event) => {
   const body = await readEntityBody(event, migrationBodyReader)
   const { _id: id } = await Migration.create(body)
+  useNitroApp().hooks.callHook(`migrate`, id)
   return await $fetch(`/api/migrations/${id}`)
 })
