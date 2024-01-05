d816657c8e0cc075a954b79b65940e63983358c0
Patrick Schilf <pschilf@unb.ca>
2024-01-05 15:29:57 -0400

    ROCKS-38 Rollback items on DELETE /api/migrations/:id

--- app/layers/mongo/server/api/migrations/[id]/index.delete.ts
+++ app/layers/mongo/server/api/migrations/[id]/index.delete.ts
@@ -1,5 +1,15 @@
 export default defineEventHandler(async (event) => {
   const { id } = getRouterParams(event)
+
+  const items = await MigrationItem.find({ migration: id })
+  await Promise.all(items.map(async (item) => {
+    if (item.destinationID) {
+      await $fetch(item.destinationID, { method: `DELETE` })
+    }
+  }))
+
+  await MigrationItem.deleteMany({ migration: id })
   await Migration.findByPKAndDelete(id)
+
   return sendNoContent(event)
 })
