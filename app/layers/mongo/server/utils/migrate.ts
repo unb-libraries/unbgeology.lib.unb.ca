a80e64f993349e3400d3c3b9e9f437387ea2dbab
Patrick Schilf <pschilf@unb.ca>
2024-01-10 11:45:33 -0400

    ROCKS-38 Migration lookup error handling

--- app/layers/mongo/server/utils/migrate.ts
+++ app/layers/mongo/server/utils/migrate.ts
@@ -1,10 +1,12 @@
 import type { Migration } from "@unb-libraries/nuxt-layer-entity"
-import type { EntityDocument } from "../../types/entity"
 
-export async function useMigrationLookup(migration: EntityDocument<Migration>, sourceID: number): Promise<string | null> {
+export async function useMigrationLookup(migration: Migration, sourceID: number): Promise<string | null> {
   const item = await MigrationItem.findOne({ migration, sourceID })
-  if (item && item.destinationID) {
-    return item.destinationID
+  if (item && item.entityURI) {
+    return item.entityURI
+  } else if (item) {
+    return null
+  } else {
+    throw new Error(`No item found for migration "${migration.id}" and sourceID "${sourceID}"`)
   }
-  return null
 }
