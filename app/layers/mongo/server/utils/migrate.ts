cccbf479f6e57e644d6111d031e8e7b23d0ecddb
Patrick Schilf <pschilf@unb.ca>
2024-01-16 16:06:27 -0400

    ROCKS-38 Reusable entity migrate handler

--- app/layers/mongo/server/utils/migrate.ts
+++ app/layers/mongo/server/utils/migrate.ts
@@ -1,4 +1,5 @@
-import type { Migration } from "@unb-libraries/nuxt-layer-entity"
+import type { Migration, Entity, EntityJSONBody } from "@unb-libraries/nuxt-layer-entity"
+import type { MigrateHandler } from "../../types"
 
 export function getMigrationDependency(migration: Migration, entityType: string) {
   return migration.dependencies.find(d => d.entityType === entityType)
@@ -11,6 +12,26 @@ export async function useMigrationLookup(migration: Migration, sourceID: number)
   } else if (item) {
     return null
   } else {
-    throw new Error(`No item found for migration "${migration.id}" and sourceID "${sourceID}"`)
+    throw new Error(`No item found for migration "${migration.name}" and sourceID "${sourceID}"`)
+  }
+}
+
+export function useMigrateHandler<T, E extends Entity = Entity>(entityType: string, handler: (data: T, lookups: Record<string, (sourceID: number) => ReturnType<typeof useMigrationLookup>>) => EntityJSONBody<E> | Promise<EntityJSONBody<E>>): MigrateHandler {
+  return async (data, migration, { ready, skip, error }) => {
+    const lookupMapper = (migration: Migration) => [migration.entityType, async (sourceID: number) => await useMigrationLookup(migration, sourceID)]
+    const lookups = Object.fromEntries([lookupMapper(migration), ...migration.dependencies.map(lookupMapper)])
+
+    if (migration.entityType === entityType) {
+      try {
+        const body = await handler(data, lookups)
+        if (body) {
+          ready(body)
+        } else {
+          skip()
+        }
+      } catch (err) {
+        error((err as Error).message)
+      }
+    }
   }
 }
--- app/layers/mongo/types/index.d.ts
+++ app/layers/mongo/types/index.d.ts
@@ -18,17 +18,19 @@ export declare module "nuxt/schema" {
   }
 }
 
+export type MigrateHandler = (data: any, migration: Migration, emits: {
+  ready: <E extends Entity = Entity>(item: EntityJSONBody<E>) => void | Promise<void>
+  require: (sourceID: number, migration: Migration) => void | Promise<void>
+  error: (errorMessage: string) => void | Promise<void>
+  skip: () => void | Promise<void>
+}) => void | Promise<void>
+
 export declare module "nitropack" {
   interface NitroRuntimeHooks {
     migrate: (migrationID: string) => void | Promise<void>
     "migrate:init": (migration: Migration, items: SourceItem[]) => void | Promise<void>
     "migrate:import": (items: MigrationItem[]) => void | Promise<void>
-    "migrate:import:item": (data: any, migration: Migration, emits: {
-      ready: <E extends Entity = Entity>(item: EntityJSONBody<E>) => void | Promise<void>
-      require: (sourceID: number, migration: Migration) => void | Promise<void>
-      error: (errorMessage: string) => void | Promise<void>
-      skip: () => void | Promise<void>
-    }) => void | Promise<void>
+    "migrate:import:item": MigrateHandler
     "migrate:import:item:imported": (item: MigrationItem, entity: EntityJSON) => void | Promise<void>
     "migrate:import:item:error": (item: MigrationItem, error: string) => void | Promise<void>
     "migrate:import:item:skipped": (item: MigrationItem) => void | Promise<void>
