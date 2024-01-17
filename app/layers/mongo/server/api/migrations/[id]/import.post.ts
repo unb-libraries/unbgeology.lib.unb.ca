61ccd633737d8737935943c5bd066c16adf6b369
Patrick Schilf <pschilf@unb.ca>
2024-01-17 10:17:17 -0400

    ROCKS-38 Resumability of migrations

--- app/layers/mongo/server/api/migrations/[id]/import.post.ts
+++ app/layers/mongo/server/api/migrations/[id]/import.post.ts
@@ -1,8 +1,10 @@
+import { MigrationStatus } from "@unb-libraries/nuxt-layer-entity"
+
 export default defineEventHandler(async (event) => {
   const { id } = getRouterParams(event)
 
   const items = await MigrationItem
-    .find({ migration: id })
+    .find({ migration: id, status: { $nin: [MigrationStatus.IMPORTED, MigrationStatus.SKIPPED, MigrationStatus.ERRORED] } })
     .populate({ path: `migration`, populate: { path: `dependencies` } })
 
   useNitroApp().hooks.callHook(`migrate:import`, items)
