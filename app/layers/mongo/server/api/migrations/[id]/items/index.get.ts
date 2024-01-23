716f0b8adaea2770d9f0737a7b9b19d1a4c30a6a
Patrick Schilf <pschilf@unb.ca>
2024-01-23 10:38:39 -0400

    ROCKS-38 Enable sorting,pagination for /api/migrations/:id/items

--- app/layers/mongo/server/api/migrations/[id]/items/index.get.ts
+++ app/layers/mongo/server/api/migrations/[id]/items/index.get.ts
@@ -1,8 +1,12 @@
 export default defineEventHandler(async (event) => {
   const { id } = getRouterParams(event)
+  const { sort, pageSize, page } = getQueryOptions(event)
+
   const items = await MigrationItem
     .find({ migration: id })
     .populate(`migration`)
+    .sort(sort.join(` `))
+    .paginate(page, pageSize)
 
   return sendEntityList(event, items)
 })
