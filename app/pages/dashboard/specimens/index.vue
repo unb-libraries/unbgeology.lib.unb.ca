40c9a23b970a4f6930f90efc4f2f519bbae7c58f
Patrick Schilf <pschilf@unb.ca>
2024-02-02 14:03:21 -0400

    ROCKS-30 Update specimen dashboard table

--- app/pages/dashboard/specimens/index.vue
+++ app/pages/dashboard/specimens/index.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="flex flex-row space-x-8">
-    <h1 class="mb-6 text-2xl">
+  <div class="mb-6 flex flex-row space-x-8">
+    <h1 class="text-2xl">
       Specimens
     </h1>
-    <div class="mb-6 flex grow flex-row space-x-2 text-right">
+    <div class="flex grow flex-row space-x-2 text-right">
       <input v-model="search" placeholder="Search" class="placeholder:text-primary dark:placeholder:text-primary-20 form-input form-input-text grow p-2 placeholder:italic">
 
       <NuxtLink to="/dashboard/specimens/create/?category=fossil" class="form-action form-action-submit p-2 font-normal">
@@ -17,44 +17,40 @@
       </NuxtLink>
     </div>
   </div>
-  <PvEntityTable :entities="specimens" :columns="[[`id`, `ID`], `category`, `classification`, `measurements`, `age`, `portion`, `pieces`, `partial`]">
-    <template #id="{ value: id }">
-      <NuxtLink :to="`/dashboard/specimens/${id.toLowerCase()}`" class="hover:underline">
-        {{ id }}
+
+  <EntityTable class="border-primary-60/75 w-full border-b" header-cell-class="group" row-class="even:dark:bg-primary-80/20 hover:bg-accent-dark/20 even:dark:hover:bg-accent-dark/20 dark:focus:bg-accent-dark/40" :entities="specimens" :columns="[[`id`, `ID`], `category`, `classification`, [`measurements`, `Dimensions`], [`pieces`, `Pieces (Partial)`]]">
+    <template #id="{ entity: specimen }">
+      <NuxtLink :to="`/dashboard/specimens/${specimen.id.toLowerCase()}`" class="hover:underline">
+        {{ specimen.id.toUpperCase() }}
       </NuxtLink>
     </template>
-    <template #category="{ value: category }">
-      {{ category.substring(0, 1).toUpperCase() + category.substring(1).toLowerCase() }}
+    <template #category="{ entity: specimen }">
+      {{ specimen.category.substring(0, 1).toUpperCase() + specimen.category.substring(1).toLowerCase() }}
     </template>
-    <template #classification="{ value: classification }">
-      <span v-if="classification">{{ classification.label }}</span>
-    </template>
-    <template #measurements="{ value: measurements }">
-      <template v-if="Array.isArray(measurements) && measurements.length > 0">
-        <ul v-for="dimensions in measurements" :key="dimensions">
-          <li>{{ dimensions.width }}mm x {{ dimensions.length }}mm</li>
-        </ul>
+    <template #classification="{ entity: specimen }">
+      <template v-if="specimen.classification">
+        {{ specimen.classification.label }}
       </template>
+    </template>
+    <template #measurements="{ entity: specimen }">
+      <ol v-if="specimen.measurements?.length">
+        <li v-for="(dimensions, index) in specimen.measurements.map(m => m.dimensions)" :key="index">
+          {{ dimensions.map(d => `${d / 10}cm`).join(` x `) }}
+        </li>
+      </ol>
       <span v-else />
     </template>
-    <template #age="{ value: age }">
-      <span v-if="age && age.numeric">
-        {{ age.numeric }} Ma
-      </span>
-      <span v-if="age">
-        {{ age.relative.boundaries.lower }} - {{ age.relative.boundaries.upper }} Ma ({{ age.relative.label }})
-      </span>
+    <template #pieces="{ entity: specimen}">
+      {{ specimen.pieces }}{{ specimen.partial ? ` (P)` : `` }}
     </template>
-    <template #portion="{ value: portion }">
-      <span v-if="portion">{{ portion.label }}</span>
-    </template>
-    <template #partial="{ value: partial }">
-      <span v-if="partial">
-        {{ partial ? `Yes` : `No` }}
-      </span>
-      <span v-else />
-    </template>
-  </PvEntityTable>
+  </EntityTable>
+
+  <div class="mt-2 flex flex-row justify-between">
+    <span v-if="list?.total" class="italic">
+      {{ (page - 1) * pageSize + 1 }} - {{ Math.min(list?.total, page * pageSize) }} of {{ pluralize(list?.total, `specimen`, `specimens`) }}
+    </span>
+    <PvPaginator v-model="page" :total="Math.ceil((list?.total ?? 0) / pageSize)" :size="5" />
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -64,6 +60,6 @@ definePageMeta({
   layout: `dashboard`,
 })
 
-const { entities: specimens, query } = await fetchEntityList<Specimen>(`Specimen`)
-const { search } = query
+const { list, entities: specimens, query } = await fetchEntityList<Specimen>(`Specimen`)
+const { search, page, pageSize } = query
 </script>
