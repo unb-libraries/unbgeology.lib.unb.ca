3a654e829025f863a36b0315969c3c13e8dafe89
Patrick Schilf <pschilf@unb.ca>
2024-02-05 15:42:44 -0400

    ROCKS-30 Update specimen page header,table

--- app/pages/dashboard/specimens/index.vue
+++ app/pages/dashboard/specimens/index.vue
@@ -1,24 +1,40 @@
 <template>
-  <div class="mb-6 flex flex-row space-x-8">
-    <h1 class="text-2xl">
-      Specimens
-    </h1>
-    <div class="flex grow flex-row space-x-2 text-right">
-      <input v-model="search" placeholder="Search" class="placeholder:text-primary dark:placeholder:text-primary-20 form-input form-input-text grow p-2 placeholder:italic">
-
-      <NuxtLink to="/dashboard/specimens/create/?category=fossil" class="form-action form-action-submit p-2 font-normal">
-        Create Fossil
-      </NuxtLink>
-      <NuxtLink to="/dashboard/specimens/create/?category=mineral" class="form-action form-action-submit p-2 font-normal">
-        Create Mineral
-      </NuxtLink>
-      <NuxtLink to="/dashboard/specimens/create/?category=rock" class="form-action form-action-submit p-2 font-normal">
-        Create Rock
-      </NuxtLink>
+  <header class="mb-6 space-y-6">
+    <div class="flex flex-row space-x-8">
+      <h1 id="title" class="text-2xl">
+        Specimens
+      </h1>
+      <div class="grow space-x-2 text-right">
+        <NuxtLink to="/dashboard/specimens/create/?category=fossil" class="form-action form-action-submit p-2 font-normal">
+          Create Fossil
+        </NuxtLink>
+        <NuxtLink to="/dashboard/specimens/create/?category=mineral" class="form-action form-action-submit p-2 font-normal">
+          Create Mineral
+        </NuxtLink>
+        <NuxtLink to="/dashboard/specimens/create/?category=rock" class="form-action form-action-submit p-2 font-normal">
+          Create Rock
+        </NuxtLink>
+      </div>
     </div>
-  </div>
 
-  <EntityTable class="border-primary-60/75 w-full border-b" header-cell-class="group" row-class="even:dark:bg-primary-80/20 hover:bg-accent-dark/20 even:dark:hover:bg-accent-dark/20 dark:focus:bg-accent-dark/40" :entities="specimens" :columns="[[`id`, `ID`], `category`, `classification`, [`measurements`, `Dimensions`], [`pieces`, `Pieces (Partial)`]]">
+    <div class="relative flex w-full flex-row space-x-2">
+      <div class="form-field w-full">
+        <label class="sr-only" for="search">Search</label>
+        <input v-model="search" placeholder="Search" name="search" class="placeholder:text-primary dark:placeholder:text-primary-20 form-input form-input-text grow p-2 placeholder:italic">
+      </div>
+    </div>
+  </header>
+
+  <EntityTable
+    v-model="selected"
+    class="border-primary-60/75 w-full border-b"
+    header-cell-class="group"
+    row-class="even:dark:bg-primary-80/20 hover:bg-accent-dark/20 even:dark:hover:bg-accent-dark/20"
+    :entities="specimens"
+    :columns="columns.filter(([id, label, selected]) => selected).map(([id, label]) => [id, label])"
+    :multi-select="true"
+    selected-row-class="dark:bg-accent-dark/40 even:dark:bg-accent-dark/40 hover:dark:bg-accent-dark/60 even:hover:dark:bg-accent-dark/60"
+  >
     <template #id="{ entity: specimen }">
       <NuxtLink :to="`/dashboard/specimens/${specimen.id.toLowerCase()}`" class="hover:underline">
         {{ specimen.id.toUpperCase() }}
@@ -54,6 +70,7 @@
 </template>
 
 <script setup lang="ts">
+import { type EntityJSON } from '@unb-libraries/nuxt-layer-entity'
 import { type Specimen } from 'types/specimen'
 
 definePageMeta({
@@ -62,4 +79,6 @@ definePageMeta({
 
 const { list, entities: specimens, query } = await fetchEntityList<Specimen>(`Specimen`)
 const { search, page, pageSize } = query
+
+const selected = ref<EntityJSON<Specimen>[]>([])
 </script>
