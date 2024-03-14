5467c6ae3ae0eb4b359a6c7e3795b3ab4be9e8b6
Patrick Schilf <pschilf@unb.ca>
2024-03-14 11:37:52 -0300

    Slugified schema mixin

--- app/layers/mongo/server/utils/mixins.ts
+++ app/layers/mongo/server/utils/mixins.ts
@@ -1,9 +1,27 @@
 import { Status } from "@unb-libraries/nuxt-layer-entity"
+import slugify from "slugify"
 import { Schema, type Document } from "mongoose"
 import type {
+  Slugified as ISlugified,
+  SlugifiedOptions,
   Stateful as IStateful,
   StatefulOptions,
 } from "../../types/mixins"
+
+export const Slugified = <T = any>(options: SlugifiedOptions<T>) => defineDocumentSchema<ISlugified, SlugifiedOptions<T>>(options => ({
+  slug: {
+    type: Schema.Types.String,
+    required: true,
+    unique: true,
+    default() {
+      return slugify(typeof options.path === `function`
+        ? options.path(this)
+        : Array.isArray(options.path)
+          ? options.path.map(path => `${this[path as keyof T]}`).join(` `)
+          : `${this[options.path as keyof T]}`, { lower: true, strict: true })
+    },
+  },
+}))(options)
 
 export const Stateful = <T extends typeof Status>(options: StatefulOptions<T>) => defineDocumentSchema<IStateful<T>, StatefulOptions<T>>(options => ({
   status: {
--- app/layers/mongo/types/mixins.ts
+++ app/layers/mongo/types/mixins.ts
@@ -1,5 +1,13 @@
 import { Status } from "@unb-libraries/nuxt-layer-entity"
 
+export interface Slugified {
+  slug: string
+}
+
+export interface SlugifiedOptions<D = any> {
+  path: Omit<keyof D, keyof Slugified> | Omit<keyof D, keyof Slugified>[] | ((doc: Omit<D, keyof Slugified>) => string)
+}
+
 export interface Stateful<T extends typeof Status> {
   status: T[keyof T]
 }
