c9e04c4cc9f5fe178c13a658bf18b1eba8915752
Patrick Schilf <pschilf@unb.ca>
2024-04-19 16:08:49 -0300

    ROCKS-29 Specimen registration middleware

--- app/server/middleware/specimens/70.specimen.register.ts
+++ app/server/middleware/specimens/70.specimen.register.ts
@@ -1,9 +1,9 @@
 export default defineEventHandler((event) => {
   const { pathname } = getRequestURL(event)
-  if (pathname.match(/^\/api\/users/)) {
-    setModel(event, User)
-    const { username } = getRouterParams(event)
-    if (pathname.split(`/`).at(-1) === username) {
+  if (/^\/api\/specimens/.test(pathname)) {
+    setModel(event, Specimen.Base)
+    const { slug } = getRouterParams(event)
+    if (pathname.split(`/`).at(-1) === slug) {
       setEntityQueryOptions(event, { filter: false, sort: false })
     }
   }
