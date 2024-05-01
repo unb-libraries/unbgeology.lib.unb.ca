e243003086fa6722054ef88ba62e1f996a1c85c0
Patrick Schilf <pschilf@unb.ca>
2024-05-01 12:40:52 -0300

    ROCKS-25 User permissions from session util

--- app/layers/base/server/utils/session.ts
+++ app/layers/base/server/utils/session.ts
@@ -1,4 +1,5 @@
-import type { H3Event, Session } from 'h3'
+import type { Permission } from '@unb-libraries/nuxt-layer-entity'
+import type { H3Event, Session, HTTPMethod } from 'h3'
 
 export function useCurrentServerSession(event: H3Event) {
   const { name } = useServerSessionConfig()
@@ -7,6 +8,28 @@ export function useCurrentServerSession(event: H3Event) {
     permissions: string[]
     validUntil: number
   }>
+}
+
+export function getCurrentUserPermissions(event: H3Event, options?: Partial<{ action: `read` | `write` | `update` | `delete` | `method` | `*` }>) {
+  const session = useCurrentServerSession(event)
+
+  function getHttpMethodAction(method: HTTPMethod) {
+    switch (method) {
+      case `POST`: return `create`
+      case `PUT`: return `update`
+      case `DELETE`: return `delete`
+      case `GET`:
+      default: return `read`
+    }
+  }
+  const action = options?.action === `method`
+    ? getHttpMethodAction(event.method)
+    : options?.action ?? `*`
+
+  return session.data.permissions
+    .map(p => createPermission(p as string))
+    .filter(p => action === `*` || p.action === action)
+    .reduceRight((acc, p) => ({ ...acc, [p.resource]: p }), {} as Record<string, Permission>)
 }
 
 export function useServerSessionConfig() {
