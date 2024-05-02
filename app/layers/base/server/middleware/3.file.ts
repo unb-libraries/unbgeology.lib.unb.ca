5e1f1750807fbfea604c6ca09eb9543a469724a0
Patrick Schilf <pschilf@unb.ca>
2024-05-02 15:48:21 -0300

    Reorder base layer middleware

--- app/layers/base/server/middleware/3.file.ts
+++ app/layers/base/server/middleware/3.file.ts
@@ -3,6 +3,11 @@ import { readFiles } from "h3-formidable"
 export default defineEventHandler(async (event) => {
   const { pathname } = getRequestURL(event)
   if (pathname === `/api/upload`) {
+    const resources = getAuthorizedResources(event, r => /^file(:\w)*$/.test(r))
+    if (!resources.length) {
+      return create403()
+    }
+
     event.context.files = await readFiles(event, {
       includeFields: true,
       keepExtensions: true,
