7b76061a6511f69e9ce350283bf47b66e69586c8
Patrick Schilf <pschilf@unb.ca>
2024-05-07 14:13:38 -0300

    ROCKS-30 Dashboard page layout

---
+++ app/layers/primevue/layouts/dashboard-page.vue
@@ -0,0 +1,19 @@
+<template>
+  <NuxtLayout name="dashboard">
+    <div class="space-y-6">
+      <header class="mb-6 space-y-6">
+        <div class="flex flex-row space-x-8">
+          <h1 id="title" class="text-2xl">
+            <slot name="page-title">
+              {{ $route.name }}
+            </slot>
+          </h1>
+          <div class="grow space-x-2 text-right">
+            <slot name="actions" />
+          </div>
+        </div>
+      </header>
+      <slot />
+    </div>
+  </NuxtLayout>
+</template>
