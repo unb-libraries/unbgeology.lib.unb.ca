567f7e1d44174d6da55e705a49d0d71aa004da91
Patrick Schilf <pschilf@unb.ca>
2024-05-27 08:43:45 -0300

    ROCKS-30 Base input component

---
+++ app/layers/primevue/components/Tw/Input/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <slot />
+    <div class="flex h-8 text-sm leading-8" v-bind="attrs">
+      <span v-if="error" class="text-red-600" :class="errorClass">
+        <slot name="error" :error="error">
+          {{ error }}
+        </slot>
+      </span>
+      <span v-else-if="help" class="text-primary-40" :class="helpClass">
+        <slot name="help" :help="help">
+          {{ help }}
+        </slot>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineOptions({
+  inheritAttrs: false,
+})
+
+const attrs = useAttrs()
+
+const props = defineProps<{
+  error?: string
+  help?: string
+  errorClass?: string
+  helpClass?: string
+}>()
+
+const setError = inject<(id: string, error: string) => void>(`setError`)
+const unsetError = inject<(id: string) => void>(`unsetError`)
+
+watch(() => props.error, (error) => {
+  if (error) {
+    setError?.(``, error)
+  } else {
+    unsetError?.(``)
+  }
+})
+</script>
