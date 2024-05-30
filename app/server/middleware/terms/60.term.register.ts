c56acfba8461b40e0f15c1efef216ed9fb24d95f
Patrick Schilf <pschilf@unb.ca>
2024-05-30 15:24:39 -0300

    Remove term model register middleware

--- app/server/middleware/terms/60.term.register.ts
+++
@@ -1,9 +0,0 @@
-export default loadDocumentModel([
-  Affiliate.Person,
-  Affiliate.Organization,
-  Classification.Fossil,
-  Classification.Mineral,
-  Classification.Rock,
-  FossilPortion,
-  StorageLocation,
-], { path: /^\/api\/terms/ })
