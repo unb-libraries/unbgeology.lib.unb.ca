d68ff2e3744e58573cc4ebe0b8bebf06a3f744ee
Patrick Schilf <pschilf@unb.ca>
2024-06-06 12:13:59 -0300

    ROCKS-36 Prune orphaned file documents on startup

---
+++ app/layers/mongo/server/plugins/files/prune.ts
@@ -0,0 +1,34 @@
+import { readFile } from "fs/promises"
+import { consola } from "consola"
+
+export default defineNitroPlugin((nitro) => {
+  nitro.hooks.hook(`mongoose:init`, async () => {
+    consola.log(`Pruning orphaned file documents...`)
+    const { documents: files } = await FileBase.find()
+
+    const fileExists = async (filepath: string) => {
+      try {
+        await readFile(filepath)
+        return true
+      } catch (err: unknown) {
+        return false
+      }
+    }
+
+    const orphans = await Promise.all(files.filter(async file => !(await fileExists(file.filepath))))
+    if (orphans.length > 0) {
+      consola.log(`${orphans.length} file documents have been orphaned.`)
+      orphans.forEach(file => consola.log(file.filepath))
+    } else {
+      consola.log(`No orphaned file documents found.`)
+    }
+
+    const { documents, total } = await FileBase.delete()
+      .where(`_id`).in(orphans.map(file => file._id))
+
+    if (total > 0) {
+      consola.log(`Pruned ${total} file document(s):`)
+      documents.forEach(file => consola.log(`${file._id}`))
+    }
+  })
+})
