54ccf46df7898602f6877fe6c990f53b993c7c3c
Patrick Schilf <pschilf@unb.ca>
2024-06-14 14:33:07 -0300

    Fix full text search index creation,update

--- app/layers/mongo/server/utils/search.ts
+++ app/layers/mongo/server/utils/search.ts
@@ -1,9 +1,10 @@
 import { type CreateIndexesOptions } from "mongodb"
+import type { IndexDirection } from "mongoose"
 
-export async function createSearchIndex(DocumentType: ReturnType<typeof defineDocumentType>, fields: Record<string, number>, options?: { filter: CreateIndexesOptions[`partialFilterExpression`]}) {
-  const collectionName = DocumentType.collection.collectionName
+export async function createSearchIndex(Model: ReturnType<typeof defineDocumentModel<any>>, fields: Record<string, number>, options?: { filter: CreateIndexesOptions[`partialFilterExpression`]}) {
+  const collectionName = Model.mongoose.model.collection.collectionName
 
-  const index = Object.keys(fields).map(field => ({ [field]: `text` })).reduce((index, field) => ({ ...index, ...field }), {})
+  const index = Object.keys(fields).map(field => ({ [field]: `text` })).reduce((index, field) => ({ ...index, ...field }), {}) as Record<string, IndexDirection>
   const weights = Object.entries(fields).map(([field, weight]) => ({ [field]: weight })).reduce((weights, field) => ({ ...weights, ...field }), {})
   const indexName = `full_text_search`
 
@@ -16,7 +17,7 @@ export async function createSearchIndex(DocumentType: ReturnType<typeof defineDo
     indexOptions.partialFilterExpression = options.filter
   }
 
-  const create = async () => await DocumentType.db
+  const create = async () => await Model.mongoose.model.db
     .collection(collectionName)
     .createIndex(index, indexOptions)
 
@@ -25,7 +26,7 @@ export async function createSearchIndex(DocumentType: ReturnType<typeof defineDo
   } catch (err: any) {
     const { codeName } = err
     if (codeName === `IndexOptionsConflict`) {
-      await DocumentType.db.collection(collectionName).dropIndex(indexName)
+      await Model.mongoose.model.db.collection(collectionName).dropIndex(indexName)
       await create()
     } else {
       throw err
