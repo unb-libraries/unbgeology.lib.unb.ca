eead49277dc55f7b65eb99e7e1b3f6c75d9b51db
Patrick Schilf <pschilf@unb.ca>
2024-06-17 17:14:21 -0300

    ROCKS-38 Rollback rollback task (accordingly to import)

--- app/layers/mongo/server/api/migrations/[id]/items/rollback.post.ts
+++ app/layers/mongo/server/api/migrations/[id]/items/rollback.post.ts
@@ -1,8 +1,7 @@
-import { MigrationItemStatus } from "@unb-libraries/nuxt-layer-entity"
+// import { MigrationItemStatus } from "@unb-libraries/nuxt-layer-entity"
 
 export default defineEventHandler(async (event) => {
   const { id } = getRouterParams(event)
-  const { pageSize, page } = getQueryOptions(event)
 
   const migrationResources = getAuthorizedResources(event, r => /^migration(:\w)*$/.test(r), { action: `update` })
   const migration = await Migration.findByID(id).select(`authTags`)
@@ -12,32 +11,9 @@ export default defineEventHandler(async (event) => {
     return create403()
   }
 
-  const sessionName = useRuntimeConfig().public.session.name
-  const Cookie = `${sessionName}=${getCookie(event, sessionName)}`
-
-  const query = MigrationItem.find()
-    .join(`migration`, Migration)
-    .where(`migration._id`).eq(parseObjectID(id))
-    .where(`status`).ne(MigrationItemStatus.INITIAL)
-    .select([`_sourceID`, `$sourceID`])
-  await useEventQuery(event, query)
-  const { documents: items, total } = await query
-    .paginate(page, pageSize)
-
   return runTask(`migrate:rollback`, {
     payload: {
-      items: await renderDocumentList(items, {
-        model: MigrationItem,
-        total,
-        canonical: {
-          // @ts-ignore
-          self: item => `/api/migrations/${id}/items/${item._sourceID}`,
-        },
-        self: () => `/api/migrations/${id}/items`,
-      }),
-      headers: {
-        Cookie,
-      },
+      migration: `${migration._id}`,
     },
   })
 })
--- app/layers/mongo/server/tasks/migrate/rollback.ts
+++ app/layers/mongo/server/tasks/migrate/rollback.ts
@@ -1,44 +1,101 @@
-import { MigrationItemStatus } from "@unb-libraries/nuxt-layer-entity"
-import type { MigrationItem, EntityJSONList } from "@unb-libraries/nuxt-layer-entity"
-import type { H3Event } from "h3"
+import { MigrationItemStatus, MigrationStatus } from "@unb-libraries/nuxt-layer-entity"
+import { consola } from "consola"
+import { type MigrationItem as IMigrationItem } from "../../documentTypes/MigrationItem"
+import { type Migration as IMigration } from "../../documentTypes/Migration"
+
+function getAuthHeaders(): { Cookie?: string } {
+  const event = useEvent()
+  if (event) {
+    const sessionName = useRuntimeConfig().public.session.name
+    return { Cookie: `${sessionName}=${getCookie(event, sessionName)}` }
+  }
+  return {}
+}
+
+async function loadMigration(id: string) {
+  return await Migration.mongoose.model.findById(id)
+}
+
+async function setItem(item: IMigrationItem, values: Partial<IMigrationItem>) {
+  return await MigrationItem.mongoose.model.findByIdAndUpdate(item._id, values, { returnDocument: `after` }).populate(`migration`)
+}
+
+async function setItemStatus(item: IMigrationItem, status: MigrationItemStatus) {
+  return await setItem(item, { status })
+}
+
+function loadProcessed(migration: IMigration) {
+  const fetchItems = async () => {
+    return await MigrationItem.mongoose.model
+      .find({ migration: migration._id, $or: [{ status: MigrationItemStatus.IMPORTED }, { status: MigrationItemStatus.ERRORED }] })
+  }
+
+  async function* doLoad() {
+    const items = await fetchItems()
+    while (items.length) {
+      if (items.length < 2) {
+        items.push(...await fetchItems())
+      }
+      yield items.pop()
+    }
+  }
+
+  return doLoad()
+}
 
 export default defineTask({
   meta: {
     name: `migrate:rollback`,
     description: `Rollback a migration`,
   },
-  run({ payload }) {
-    if (!payload.items) {
-      throw new Error(`No items provided`)
+  async run({ payload }) {
+    const { migration: migrationID } = payload as { migration: string }
+    if (!migrationID) {
+      throw new Error(`No migration provided`)
     }
 
-    const { items, headers } = payload as { items: EntityJSONList<MigrationItem>, headers: H3Event[`headers`] }
-
-    async function rollback(uri: string): Promise<number> {
-      const { entities, nav } = await $fetch<EntityJSONList<MigrationItem>>(uri, {
-        query: {
-          select: [`id`, `entityURI`, `status`],
-        },
-        headers,
-      })
-
-      entities.filter(item => item.entityURI).map(item => item.entityURI!).forEach((entityURI) => {
-        $fetch(entityURI, { method: `DELETE`, headers })
-      })
-      $fetch(uri, {
-        method: `PATCH`,
-        body: {
-          status: MigrationItemStatus.INITIAL,
-          error: null,
-          entityURI: null,
-        },
-        headers,
-      })
-
-      return nav.next ? entities.length + await rollback(nav.next) : entities.length
+    const migration = await loadMigration(migrationID)
+    if (!migration) {
+      throw new Error(`Migration ${migrationID} not found`)
     }
 
-    rollback(items.self)
-    return { result: items.total }
+    async function cycle() {
+      const queue = loadProcessed(migration!)
+      let item = await queue.next()
+      while (!item.done && item.value) {
+        await setItemStatus(item.value, MigrationItemStatus.PENDING)
+        await doRollback(item.value)
+        item = await queue.next()
+      }
+      await Migration.mongoose.model.findByIdAndUpdate(migration!._id, { status: MigrationStatus.IDLE })
+      consola.log(`Done!`)
+    }
+
+    async function doRollback(item: IMigrationItem) {
+      const { entityURI } = item
+      if (entityURI) {
+        try {
+          await $fetch(entityURI, { method: `DELETE`, headers: getAuthHeaders() })
+        } catch (err: unknown) {
+          consola.error(`Error deleting entity ${entityURI}`, (err as Error).message)
+        }
+      }
+
+      await MigrationItem.mongoose.model.findByIdAndUpdate(item._id, {
+        status: MigrationItemStatus.INITIAL,
+        $unset: { entityURI: 1, error: 1 },
+      })
+
+      await Migration.mongoose.model.findByIdAndUpdate(item._id, {
+        $inc: item.status === MigrationItemStatus.IMPORTED
+          ? { imported: -1 }
+          : { errored: -1 },
+      })
+    }
+
+    consola.info(`Rollback ${migration.name}`)
+    cycle()
+
+    return { result: true }
   },
 })
