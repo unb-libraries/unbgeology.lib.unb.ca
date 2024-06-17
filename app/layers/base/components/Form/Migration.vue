663034f5ceb46d350ed1ff3c016df28e51435367
Patrick Schilf <pschilf@unb.ca>
2024-06-17 12:35:02 -0300

    ROCKS-38 Migration create page,form

---
+++ app/layers/base/components/Form/Migration.vue
@@ -0,0 +1,99 @@
+<template>
+  <EntityForm :entity="entity" @save="onSave" @cancel="$emit(`cancel`)">
+    <TwFormField label="Name">
+      <PvInputText id="form-input-label" v-model="name" class="input input-text-lg" />
+    </TwFormField>
+    <TwFormField label="Entity type">
+      <PvInputDropdown
+        v-model="entityType"
+        class="input-select-lg"
+        :options="[[`Specimen`, `Specimen`], [`Term.Classification.Mineral`, `Classification (Mineral)`]]"
+      />
+    </TwFormField>
+    <TwFormField label="Dependencies">
+      <PvInputDropdown
+        v-model="dependencies"
+        class="input-select-lg"
+        :multi="true"
+        :options="migrations"
+        option-field="self"
+        label-field="name"
+      />
+    </TwFormField>
+    <TwFormField label="Source data">
+      <div class="space-y-2">
+        <TwInputFileDrop @drop="onDrop" />
+        <div v-if="items.length" class="bg-primary-80/40 group rounded-lg px-3 py-2">
+          <div class="flex flex-row justify-between">
+            <div>{{ items.length }} items</div>
+            <button class="button button-sm button-primary-60 invisible group-hover:visible" @click.stop.prevent="onResetItems">
+              Reset
+            </button>
+          </div>
+        </div>
+      </div>
+    </TwFormField>
+  </EntityForm>
+</template>
+
+<script setup lang="tsx">
+import { type MigrationItem, type EntityJSON, type EntityJSONProperties, type Migration } from "@unb-libraries/nuxt-layer-entity"
+import { parse } from "csv-parse/browser/esm"
+
+const props = defineProps<{
+  entity?: EntityJSONProperties<Migration> & Partial<EntityJSON<Migration>>
+}>()
+
+const emits = defineEmits<{
+  save: [migration: Pick<Migration, `name` | `entityType` | `dependencies`>, items?: Pick<MigrationItem, `id` | `data`>[]]
+  cancel: []
+}>()
+
+const { entities: migrations } = await fetchEntityList(`Migration`)
+const name = ref<string>(props.entity?.name ?? ``)
+const entityType = ref<string>(props.entity?.entityType ?? ``)
+const dependencies = ref<string[]>(props.entity?.dependencies?.map(d => d.self) ?? [])
+const items = ref<Pick<MigrationItem, `sourceID` | `data`>[]>([])
+const { createToast } = useToasts()
+
+function onSave() {
+  emits(`save`, {
+    name: name.value,
+    entityType: entityType.value,
+    dependencies: dependencies.value,
+  }, items.value)
+}
+
+async function onDrop(files: File[]) {
+  const parsed = await Promise.all(files.map(async (f) => {
+    switch (f.type) {
+      case `text/csv`:
+        return parseCsv(await f.text())
+      default: createToast(`unsupported-file-error-${f.type}`, () => `Unsupported file type: ${f.type}`, { type: `error`, duration: 4000 })
+    }
+  }))
+
+  const migrationItems = parsed.filter(f => f).map(f => f!.map<Pick<MigrationItem, `sourceID` | `data`>>((p) => {
+    const [[, sourceID], ...data] = Object.entries(p)
+    return { id: parseInt(sourceID), data: Object.fromEntries(data) }
+  })).flat()
+
+  items.value = [...items.value, ...migrationItems]
+}
+
+function parseCsv(csv: string) {
+  return new Promise<object[]>((resolve) => {
+    parse(csv, {}, (err, [header, ...rows]: string[][]) => {
+      if (!err) {
+        resolve(rows.map(row => Object.fromEntries(row.map((col, i) => [header[i], col]))))
+      } else {
+        resolve([])
+      }
+    })
+  })
+}
+
+function onResetItems() {
+  items.value = []
+}
+</script>
---
+++ app/pages/dashboard/migrations/create.vue
@@ -0,0 +1,49 @@
+<template>
+  <FormMigration @save="onSave" @cancel="navigateTo(returnUrl)" />
+</template>
+
+<script setup lang="ts">
+import { type Migration, type MigrationItem } from "@unb-libraries/nuxt-layer-entity"
+
+definePageMeta({
+  name: `Add migration`,
+  layout: `dashboard-page`,
+  auth: {
+    redirect: true,
+    permission: /^create:migration/,
+  },
+  menu: {
+    weight: 0,
+  },
+})
+
+type MigrationBody = Pick<Migration, `name` | `entityType` | `dependencies`>
+type MigrationItemBody = Pick<MigrationItem, `sourceID` | `data`>
+const { create } = useEntityType<Migration, MigrationBody>(`Migration`)
+const returnUrl = `/dashboard/migrations`
+
+async function onSave(body: MigrationBody, items?: MigrationItemBody[]) {
+  const { createToast } = useToasts()
+  const { entity: migration, error: createError } = await create(body)
+  if (!createError.value) {
+    if (items?.length) {
+      const { error: itemsCreateError } = await useFetch(`${migration.value?.self}/items`, {
+        method: `POST`,
+        body: items,
+      })
+      if (itemsCreateError.value) {
+        createToast(`migration-items-created-error`, () => `Failed to create migration items: ${itemsCreateError.value}`, { type: `error` })
+      } else {
+        createToast(`migration-items-created`, () => `Migration created`, { type: `success` })
+        navigateTo(returnUrl)
+      }
+    } else {
+      createToast(`migration-created`, () => `Migration created`, { type: `success` })
+    }
+  } else {
+    createToast(`migration-created-error`, () => `Failed to create migration: ${createError.value}`, { type: `error` })
+    navigateTo(returnUrl)
+  }
+}
+
+</script>
