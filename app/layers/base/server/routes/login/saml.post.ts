527c8d81f3bdda98ddab308442b49b3ba2bf77da
Patrick Schilf <pschilf@unb.ca>
2024-06-20 14:44:29 -0300

    Verbose SAML debug output

--- app/layers/base/server/routes/login/saml.post.ts
+++ app/layers/base/server/routes/login/saml.post.ts
@@ -1,11 +1,16 @@
+import { consola } from "consola"
 import { createError, readBody, sendRedirect, useSession } from "h3"
 import { getSamlProfile } from "../../../saml/saml"
 
 export default defineEventHandler(async (event) => {
   const { SAMLResponse, RelayState = `/` } = await readBody(event)
 
+  consola.log(`SAMLResponse:`, SAMLResponse)
+
   const sessionConfig = useServerSessionConfig()
+  consola.log(`sessionConfig:`, sessionConfig)
   const session = await useSession(event, sessionConfig)
+  consola.log(`session:`, session)
   const {
     uid: username,
     mail: email,
@@ -14,12 +19,19 @@ export default defineEventHandler(async (event) => {
     givenName: firstName,
   } = await getSamlProfile(SAMLResponse)
 
+  consola.log(`profile:`, username, email, phone, lastName, firstName)
+
+  consola.log(`User model:`, User)
+
   // REFACTOR: Avoid making direct DB call, use API instead.
   const user = await User.findOne()
     .where(`username`).eq(username)
     .select(`username`, `roles`, `active`)
 
+  consola.log(`user:`, user)
+
   if (user?.active) {
+    consola.log(`active:`, true)
     await user.update({
       profile: {
         email,
@@ -28,7 +40,9 @@ export default defineEventHandler(async (event) => {
         firstName,
       },
     })
+    consola.log(`profile updated`)
   } else {
+    consola.log(`user not updated`)
     throw createError({ status: 404, statusText: `User not found` })
   }
 
@@ -40,9 +54,11 @@ export default defineEventHandler(async (event) => {
       firstName,
       lastName,
     },
-    permissions: (await getRolePermissions(...user.roles)).map(createFieldPermissionKeys).flat(),
+    permissions: (await getRolePermissions(...(user.roles ?? []))).map(createFieldPermissionKeys).flat(),
     validUntil: new Date().valueOf() + sessionConfig.maxAge * 1000,
   })
+
+  consola.log(`session updated`)
 
   return sendRedirect(event, RelayState, 302)
 })
