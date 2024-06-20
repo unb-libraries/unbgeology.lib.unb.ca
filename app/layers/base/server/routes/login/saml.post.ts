79c6a07da306335ebb0e72d4b6e7e847d5d6f7ee
Patrick Schilf <pschilf@unb.ca>
2024-06-20 16:29:52 -0300

    Remove SAML debug output

--- app/layers/base/server/routes/login/saml.post.ts
+++ app/layers/base/server/routes/login/saml.post.ts
@@ -1,16 +1,11 @@
-import { consola } from "consola"
 import { createError, readBody, sendRedirect, useSession } from "h3"
 import { getSamlProfile } from "../../../saml/saml"
 
 export default defineEventHandler(async (event) => {
   const { SAMLResponse, RelayState = `/` } = await readBody(event)
 
-  consola.log(`SAMLResponse:`, SAMLResponse)
-
   const sessionConfig = useServerSessionConfig()
-  consola.log(`sessionConfig:`, sessionConfig)
   const session = await useSession(event, sessionConfig)
-  consola.log(`session:`, session)
   const {
     uid: username,
     mail: email,
@@ -19,19 +14,12 @@ export default defineEventHandler(async (event) => {
     givenName: firstName,
   } = await getSamlProfile(SAMLResponse)
 
-  consola.log(`profile:`, username, email, phone, lastName, firstName)
-
-  consola.log(`User model:`, User)
-
   // REFACTOR: Avoid making direct DB call, use API instead.
   const user = await User.mongoose.model
     .findOne({ username })
     .select(`username roles active`)
 
-  consola.log(`user:`, user)
-
   if (user?.active) {
-    consola.log(`active:`, true)
     await user.updateOne({ username }, {
       profile: {
         email,
@@ -40,9 +28,7 @@ export default defineEventHandler(async (event) => {
         firstName,
       },
     })
-    consola.log(`profile updated`)
   } else {
-    consola.log(`user not updated`)
     throw createError({ status: 404, statusText: `User not found` })
   }
 
@@ -57,8 +43,6 @@ export default defineEventHandler(async (event) => {
     permissions: (await getRolePermissions(...(user.roles ?? []))).map(createFieldPermissionKeys).flat(),
     validUntil: new Date().valueOf() + sessionConfig.maxAge * 1000,
   })
-
-  consola.log(`session updated`)
 
   return sendRedirect(event, RelayState, 302)
 })
--- app/layers/mongo/server/plugins/mongoose.ts
+++ app/layers/mongo/server/plugins/mongoose.ts
@@ -1,8 +1,14 @@
+import { consola } from "consola"
 import mongoose from "mongoose"
 
 export default defineNitroPlugin(async (nitro) => {
   const uri = getMongooseConnectURI()
   const connectOptions = getMongooseConnectOptions()
   const conn = await mongoose.connect(uri, connectOptions)
-  nitro.hooks.callHook(`mongoose:init`, conn)
+  if (conn) {
+    consola.info(`Connected to MongoDB`)
+    nitro.hooks.callHook(`mongoose:init`, conn)
+  } else {
+    consola.error(`Failed to connect to MongoDB`)
+  }
 })
