68fd2da8f81807dab4970531bd74911599c29112
Patrick Schilf <pschilf@unb.ca>
2024-06-20 11:15:44 -0300

    Batched file prune

--- app/layers/mongo/server/plugins/files/prune.ts
+++ app/layers/mongo/server/plugins/files/prune.ts
@@ -1,34 +1,47 @@
-import { readFile } from "fs/promises"
+import { access, constants } from "fs/promises"
 import { consola } from "consola"
+import { FileState } from "~/layers/entity/src/types"
+
+const fileExists = async (filepath: string) => {
+  try {
+    await access(filepath, constants.R_OK)
+    return true
+  } catch (err: unknown) {
+    return false
+  }
+}
+
+function loadPaginated() {
+  let skip = 0; const limit = 2
+  async function* doLoad() {
+    let files = await FileBase.mongoose.model.find().skip(skip).limit(limit)
+    while (files.length > 0) {
+      yield files
+      skip += limit
+      files = await FileBase.mongoose.model.find().skip(skip).limit(limit)
+    }
+  }
+  return doLoad()
+}
 
 export default defineNitroPlugin((nitro) => {
   nitro.hooks.hook(`mongoose:init`, async () => {
-    consola.log(`Pruning orphaned file documents...`)
-    const { documents: files } = await FileBase.find()
+    for await (const files of loadPaginated()) {
+      const orphans = (await Promise.all(files
+        .map<Promise<[typeof files[number], boolean]>>(async file => [file, await fileExists(file.filepath)])))
+        .filter(([, exists]) => !exists)
 
-    const fileExists = async (filepath: string) => {
-      try {
-        await readFile(filepath)
-        return true
-      } catch (err: unknown) {
-        return false
+      const total = orphans.length
+      if (total > 0) {
+        await Promise.all(orphans.map(([file]) => file.updateOne({ status: FileState.DELETED })))
       }
     }
 
-    const orphans = await Promise.all(files.filter(file => !fileExists(file.filepath)))
-    if (orphans.length > 0) {
-      consola.log(`${orphans.length} file documents have been orphaned.`)
-      orphans.forEach(file => consola.log(file.filepath))
+    const { deletedCount } = await FileBase.mongoose.model.deleteMany({ status: FileState.DELETED })
+    if (deletedCount > 0) {
+      consola.log(`Pruned ${deletedCount} file document(s)`)
     } else {
-      consola.log(`No orphaned file documents found.`)
-    }
-
-    const { documents, total } = await FileBase.delete()
-      .where(`_id`).in(orphans.map(file => file._id))
-
-    if (total > 0) {
-      consola.log(`Pruned ${total} file document(s):`)
-      documents.forEach(file => consola.log(`${file._id}`))
+      consola.log(`No orphaned file documents found`)
     }
   })
 })
