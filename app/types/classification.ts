c30bb86fd2751a8cbdcf10326d2b09e3d746a19f
Patrick Schilf <pschilf@unb.ca>
2024-06-20 17:36:58 -0300

    ROCKS-37 Add "domain" to classification/fossil rank

--- app/types/classification.ts
+++ app/types/classification.ts
@@ -6,12 +6,13 @@ export enum Status {
 }
 
 export enum Rank {
-  KINGDOM = 1,
-  PHYLUM = 2,
-  SUBPHYLUM = 4,
-  CLASS = 8,
-  SUBCLASS = 16,
-  ORDERS = 32,
+  DOMAIN = 1,
+  KINGDOM = 2,
+  PHYLUM = 4,
+  SUBPHYLUM = 8,
+  CLASS = 16,
+  SUBCLASS = 32,
+  ORDERS = 64,
 }
 
 export type Classification<T extends object = {}> = Term & Hierarchical<Classification<T>> & Stateful<typeof Status> & T
