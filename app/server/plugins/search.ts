bdf01b703f8e24aea345fd0dd9aba97c7c1f4eb2
Patrick Schilf <pschilf@unb.ca>
2024-06-20 14:29:10 -0300

    Disable search plugins, temporarily

--- app/server/plugins/search.ts
+++ app/server/plugins/search.ts
@@ -1,18 +1,18 @@
 export default defineNitroPlugin((nitro) => {
   nitro.hooks.hook(`mongoose:init`, (mongoose) => {
-    (async () => {
-      await createSearchIndex(Specimen.Base.mongoose.model, { description: 1 })
-      await Specimen.Base.mongoose.model.ensureIndexes()
-    })();
+    // (async () => {
+    //   await createSearchIndex(Specimen.Base, { "description": 1, "pk": 1, "objectIDs.id": 1, "objectIDs.type": 1, "origin.name": 1, "origin.description": 1 })
+    //   await Specimen?.Base?.mongoose?.model?.ensureIndexes()
+    // })();
 
-    (async () => {
-      await createSearchIndex(Term.mongoose.model, { label: 1 })
-      await Term.mongoose.model.ensureIndexes()
-    })();
+    // (async () => {
+    //   await createSearchIndex(Term, { label: 1 })
+    //   await Term?.mongoose?.model?.ensureIndexes()
+    // })();
 
-    (async () => {
-      await createSearchIndex(User.mongoose.model, { username: 1 })
-      await User.mongoose.model.ensureIndexes()
-    })()
+    // (async () => {
+    //   await createSearchIndex(User, { username: 1 })
+    //   await User?.mongoose?.model?.ensureIndexes()
+    // })()
   })
 })
