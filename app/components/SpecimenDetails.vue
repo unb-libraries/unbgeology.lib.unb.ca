f12aac5fe64f9e9ba118868780e8105d8243a9e5
Patrick Schilf <pschilf@unb.ca>
2024-07-16 12:45:33 -0300

    ROCKS-30 Specimen sidebar to display full classification context

--- app/components/SpecimenDetails.vue
+++ app/components/SpecimenDetails.vue
@@ -1,15 +1,20 @@
 <template>
-  <PvEntityDetails :entity="specimen!" :fields="[[`id`, `ID`], [`name`, `Object name`], [`type`, `Category`], `classification`, `created`, `updated`, `status`]" class="space-y-4" label-class="font-bold italic">
+  <PvEntityDetails :entity="specimen!" :fields="[[`id`, `ID`], [`name`, `Object name`], [`type`, `Category`], `classification`, `created`, `updated`, `status`]" class="space-y-4" label-class="font-bold italic" item-class="space-y-1">
     <template #id="{ value: id }">
       {{ id }}
     </template>
     <template #type="{ value: category }">
       {{ titleCased(category) }}
     </template>
-    <template #classification="{ value: classification }">
-      <template v-if="classification">
-        {{ classification.label }}
-      </template>
+    <template #classification="{ entity: { classification } }">
+      <div v-if="classification" class="flex flex-row items-center space-x-1.5">
+        <template v-for="(term, index) in [classification, ...classification.ancestors?.entities ?? []].reverse()" :key="term.self">
+          <span class="bg-primary-60/60 rounded-md px-1.5 py-0.5">
+            {{ term.label }}
+          </span>
+          <span v-if="index < (classification.ancestors?.entities ?? []).length">></span>
+        </template>
+      </div>
       <template v-else>
         Unknown
       </template>
