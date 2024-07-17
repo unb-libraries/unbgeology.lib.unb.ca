62cb8a3c2ab558be6cd19d107e38114a38feaf57
Patrick Schilf <pschilf@unb.ca>
2024-07-17 10:21:21 -0300

    ROCKS-30 Geochronology input dropdown to feature hierarchical context

--- app/components/Input/Specimen/GeoAge.vue
+++ app/components/Input/Specimen/GeoAge.vue
@@ -5,10 +5,21 @@
     option-field="self"
     label-field="label"
     class="input-select-lg"
+    item-class="even:bg-primary-80/40 hover:even:bg-accent-mid py-1 group"
+    selected-item-class="bg-accent-mid even:bg-accent-mid hover:bg-accent-light even:hover:bg-accent-light"
     placeholder="Select a geochronologic time unit"
     :add-new-option="true"
     @add="onAdd"
-  />
+  >
+    <template #item="{ options: [option, label, selected] }">
+      <div class="flex flex-col">
+        <span>{{ label }}</span>
+        <span class="group-hover:text-primary-80 text-xs italic" :class="{ 'text-primary-20': !selected, 'text-primary-80': selected }">
+          {{ units.find(op => op.self === option)?.ancestors?.entities.map(acs => acs.label).reverse().join(` &raquo; `) ?? `&nbsp;` }}
+        </span>
+      </div>
+    </template>
+  </PvInputDropdown>
 </template>
 
 <script setup lang="ts">
@@ -18,7 +29,7 @@ import { FormGeochronology } from '#components'
 import { type Unit } from '~/types/geochronology'
 
 const unit = defineModel<string>({ required: false })
-const { entities: units, add: createUnit } = await fetchEntityList(`Term`, { filter: [[`type`, FilterOperator.EQUALS, `geochronology`]], select: [`label`], sort: [`label`], pageSize: 500 })
+const { entities: units, add: createUnit } = await fetchEntityList(`Term`, { filter: [[`type`, FilterOperator.EQUALS, `geochronology`]], select: [`label`, `ancestors`], sort: [`label`], pageSize: 500 })
 function onAdd() {
   const { open: openModal } = useEntityFormModal<Unit>(FormGeochronology, {
     onSave: async (values: Unit) => {
