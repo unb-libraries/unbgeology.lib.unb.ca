2766e98f9dae90c13fec273ce740d88e6b1a86b1
Patrick Schilf <pschilf@unb.ca>
2024-07-22 13:56:42 -0300

    ROCKS-30 TwPageIndex to replace PvPaginator

--- app/layers/primevue/components/Tw/PageIndex.vue
+++ app/layers/primevue/components/Tw/PageIndex.vue
@@ -1,37 +1,32 @@
 <template>
   <div v-if="pages.length > 1">
-    <a v-if="!pages.includes(1)" class="hover:dark:bg-primary-60/20 cursor-pointer rounded-md px-3 py-1.5" @click.stop.prevent="page = 1">First</a>
+    <a v-if="!pages.includes(1)" class="hover:dark:bg-primary-60/20 cursor-pointer rounded-md px-3 py-1.5" @click.stop.prevent="$emit(`change`, 1)">First</a>
     <a
       v-for="index in pages"
       :key="index"
       class="mx-2 rounded-md px-3 py-1.5"
       :class="{ 'hover:dark:bg-primary-60/20 cursor-pointer': index !== page, 'dark:bg-primary-80/75': index === page }"
-      @click.stop.prevent="page = index"
+      @click.stop.prevent="$emit(`change`, index)"
     >{{ index }}</a>
-    <a v-if="!pages.includes(total)" class="hover:dark:bg-primary-60/20 cursor-pointer rounded-md px-3 py-1.5" @click.stop.prevent="page = total">Last</a>
+    <a v-if="!pages.includes(total)" class="hover:dark:bg-primary-60/20 cursor-pointer rounded-md px-3 py-1.5" @click.stop.prevent="$emit(`change`, total)">Last</a>
   </div>
 </template>
 
 <script setup lang="ts">
-import { consola } from "consola"
-
-onMounted(() => consola.warn(`PvPaginator is deprecated. Use TwPageIndex instead.`))
-
-// REFACTOR: Make this a prop
-const page = defineModel({ required: false, default: 1 })
 const props = defineProps<{
+  page: number
   size: number
   total: number
 }>()
 
 defineEmits<{
-  paged: [page: number]
+  change: [page: number]
 }>()
 
 const pages = computed(() => {
   const count = Math.min(props.size, props.total)
   return Array
-    .from({ length: count }, (_, i) => page.value - Math.floor(count / 2) + i)
+    .from({ length: count }, (_, i) => props.page - Math.floor(count / 2) + i)
     .map((page, _, arr) => page + arr.filter(p => p <= 0).length)
     .map((page, _, arr) => page - arr.filter(p => p > props.total).length)
 })
