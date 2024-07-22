3de6dae686105362eff7c35e9c5e2d5d5fbc095a
Patrick Schilf <pschilf@unb.ca>
2024-07-22 13:32:48 -0300

    ROCKS-30 Paginator index to exclude 0

--- app/layers/primevue/components/PvPaginator.vue
+++ app/layers/primevue/components/PvPaginator.vue
@@ -1,51 +1,34 @@
 <template>
   <div v-if="pages.length > 1">
-    <a v-if="!pages.includes(1)" class="hover:dark:bg-primary-60/20 cursor-pointer rounded-md px-3 py-1.5" @click="page = 1">First</a>
+    <a v-if="!pages.includes(1)" class="hover:dark:bg-primary-60/20 cursor-pointer rounded-md px-3 py-1.5" @click.stop.prevent="page = 1">First</a>
     <a
       v-for="index in pages"
       :key="index"
       class="mx-2 rounded-md px-3 py-1.5"
       :class="{ 'hover:dark:bg-primary-60/20 cursor-pointer': index !== page, 'dark:bg-primary-80/75': index === page }"
-      @click="page = index"
+      @click.stop.prevent="page = index"
     >{{ index }}</a>
-    <a v-if="!pages.includes(total)" class="hover:dark:bg-primary-60/20 cursor-pointer rounded-md px-3 py-1.5" @click="page = total">Last</a>
+    <a v-if="!pages.includes(total)" class="hover:dark:bg-primary-60/20 cursor-pointer rounded-md px-3 py-1.5" @click.stop.prevent="page = total">Last</a>
   </div>
 </template>
 
 <script setup lang="ts">
+// REFACTOR: Make this a prop
+const page = defineModel({ required: false, default: 1 })
 const props = defineProps<{
-  modelValue: number
   size: number
   total: number
 }>()
 
-const emits = defineEmits<{
-  // eslint-disable-next-line
-  "update:modelValue": [value: number]
+defineEmits<{
+  paged: [page: number]
 }>()
 
-const page = computed({
-  get() {
-    return props.modelValue
-  },
-  set(value) {
-    emits(`update:modelValue`, value)
-  },
-})
-
-function getStartPageIndex(current: number, count: number, last: number) {
-  current = Math.min(Math.max(current, 1), last)
-  const slice = Array
-    .from({ length: count }, (_, i) => current - Math.floor(count / 2) + i)
-    .filter(page => page >= 1 && page <= last)
-  const imbalance = slice.filter(i => i > current).length - slice.filter(i => i < current).length
-  const start = current - Math.floor(count / 2) + imbalance
-  return start
-}
-
 const pages = computed(() => {
-  const actualSize = Math.min(props.size, props.total)
-  const start = getStartPageIndex(page.value, actualSize, props.total)
-  return Array.from({ length: actualSize }).map((_, i) => start + i)
+  const count = Math.min(props.size, props.total)
+  return Array
+    .from({ length: count }, (_, i) => page.value - Math.floor(count / 2) + i)
+    .map((page, _, arr) => page + arr.filter(p => p <= 0).length)
+    .map((page, _, arr) => page - arr.filter(p => p > props.total).length)
 })
 </script>
