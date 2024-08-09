90c04b9706abad01b3f48f0f169d5cdfbd5a6320
Patrick Schilf <pschilf@unb.ca>
2024-08-09 10:01:24 -0300

    ROCKS-30 Modal minimum height

--- app/layers/primevue/components/PvModal.vue
+++ app/layers/primevue/components/PvModal.vue
@@ -1,7 +1,7 @@
 <template>
   <Teleport v-if="isOpen" to="body">
     <div class="bg-primary-60/75 fixed left-0 top-0 z-[1000] h-screen w-screen" :class="overlayClass" @click="close()">
-      <div class="bg-primary sm:max-w-4/5 sm:min-w-1/3 sm:max-h-4/5 absolute left-1/2 top-1/2 size-fit -translate-x-1/2 -translate-y-1/2 overflow-y-scroll rounded-lg p-8" :class="modalClass" @click.stop="">
+      <div class="bg-primary sm:max-w-4/5 sm:min-w-1/3 sm:min-h-4/5 sm:max-h-4/5 absolute left-1/2 top-1/2 size-fit -translate-x-1/2 -translate-y-1/2 overflow-y-scroll rounded-lg p-8" :class="modalClass" @click.stop="">
         <KeepAlive>
           <component :is="stack[0]" />
         </KeepAlive>
