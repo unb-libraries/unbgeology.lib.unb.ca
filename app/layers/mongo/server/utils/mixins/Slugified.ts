e9bde0a116f1059b5885929460bebc8d5f5e2f06
Patrick Schilf <pschilf@unb.ca>
2024-08-12 14:48:04 -0300

    Refactor slugified mixin, slug to generate in pre-save hook

--- app/layers/mongo/server/utils/mixins/Slugified.ts
+++ app/layers/mongo/server/utils/mixins/Slugified.ts
@@ -1,25 +1,43 @@
-import { Schema } from "mongoose"
 import slugify from "slugify"
+import { Schema, type Document } from "mongoose"
 import { type Slugified as ISlugified } from "@unb-libraries/nuxt-layer-entity"
 
 export type Slugified = ISlugified
+type SlugifyablePath<D = any> = Omit<keyof D, keyof ISlugified>
+type SlugifyableDocument<D = any> = Document<Omit<D, keyof ISlugified>>
 export interface SlugifiedOptions<D = any> {
-  path?: Omit<keyof D, keyof ISlugified> | Omit<keyof D, keyof ISlugified>[] | ((doc: Omit<D, keyof ISlugified>) => string)
+  immutable: boolean
+  syncOnUpdate: boolean
+  path: SlugifyablePath<D> | SlugifyablePath<D>[] | ((doc: SlugifyableDocument<D>) => string | Promise<string>)
+  unique: boolean
 }
 
-export default <T = any>(options?: Partial<SlugifiedOptions<T>>) => defineDocumentSchema<ISlugified, Partial<SlugifiedOptions<T>>>(options => ({
-  slug: {
-    type: Schema.Types.String,
-    required: options?.path !== undefined,
-    default() {
-      if (!options.path) {
-        return ``
-      }
-      return slugify(typeof options.path === `function`
-        ? options.path(this)
-        : Array.isArray(options.path)
-          ? options.path.map(path => `${this[path as keyof T]}`).join(` `)
-          : `${this[options.path as keyof T]}`, { lower: true, strict: true })
+async function createSlug<D = any>(doc: Document<D>, path: SlugifiedOptions<D>[`path`]) {
+  const slug = typeof path === `function`
+    ? await path(doc)
+    : typeof path === `string`
+      ? `${doc.get(path)}`
+      : (path as SlugifyablePath[]).map(p => doc.get(`${p}`)).join(` `)
+  return slugify(slug, { lower: true, strict: true })
+}
+
+export default <T = any>(options: Partial<SlugifiedOptions<T>>) => defineDocumentSchema<ISlugified, Partial<SlugifiedOptions<T>>>((options) => {
+  const { immutable = false, unique = false } = options
+  return {
+    slug: {
+      type: Schema.Types.String,
+      required: false,
+      unique,
+      immutable,
     },
+  }
+}, {
+  alterSchema(schema) {
+    schema.pre(`save`, async function () {
+      const { path = `_id`, syncOnUpdate = false, immutable = false } = options
+      if (this.isNew || (!immutable && syncOnUpdate)) {
+        this.slug = await createSlug(this as unknown as Document<T>, path)
+      }
+    })
   },
-}))(options ?? {})
+})(options)
