49edc51edae1725dd3e89c9b995b3d2670773f05
Patrick Schilf <pschilf@unb.ca>
2024-08-15 10:19:18 -0300

    ROCKS-38 Item import endpoints to use Mongoose documents

--- app/layers/mongo/server/api/migrations/[id]/items/[sourceID]/import/index.get.ts
+++ app/layers/mongo/server/api/migrations/[id]/items/[sourceID]/import/index.get.ts
@@ -1,21 +1,28 @@
-import { MigrationItemStatus, type EntityJSON, type MigrationItem } from "@unb-libraries/nuxt-layer-entity"
+import { MigrationItemStatus } from "@unb-libraries/nuxt-layer-entity"
 
 export default defineEventHandler(async (event) => {
-  const self = getRequestURL(event).pathname
-  const itemURI = self.split(`/`).slice(0, -1).join(`/`)
-  const sessionName = useRuntimeConfig().public.session.name
-  const cookie = `${sessionName}=${getCookie(event, sessionName)}`
+  const { id, sourceID } = getRouterParams(event)
 
-  try {
-    const item = await $fetch<EntityJSON<MigrationItem>>(itemURI, { headers: { Cookie: cookie } })
-    return {
-      self,
-      status: useEnum(MigrationItemStatus)
-        .labelOf(item.status)
-        .toLowerCase(),
-      // TODO: Add creation and completion date
-    }
-  } catch (err: unknown) {
-    return err
+  const migrationResources = getAuthorizedResources(event, r => /^migration(:\w)*$/.test(r))
+  const migration = await Migration.findByID(id).select(`authTags`)
+  if (!migration) {
+    return create404()
+  } else if (!migrationResources.length || !migration.authTags.some(t => migrationResources.includes(t))) {
+    return create403()
   }
+
+  const resources = getAuthorizedResources(event, r => /^migrationitem(:\w)*$/.test(r))
+  if (!resources.length) {
+    return create403()
+  }
+
+  const item = await MigrationItem.mongoose.model.findOne()
+    .where(`migration`).equals(parseObjectID(id))
+    .where(`sourceID`).equals(sourceID)
+
+  return (item && {
+    self: getRequestURL(event).pathname,
+    status: useEnum(MigrationItemStatus).labelOf(item.status).toLowerCase(),
+    // TODO: Add creation and completion date
+  }) || create404()
 })
