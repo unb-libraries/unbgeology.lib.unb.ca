4bf7710800aa41ffa3a53817265ce056655fc9da
Patrick Schilf <pschilf@unb.ca>
2024-08-21 08:33:47 -0300

    ROCKS-38 MigrationItem status to be integer field

--- app/layers/mongo/server/documentTypes/MigrationItem.ts
+++ app/layers/mongo/server/documentTypes/MigrationItem.ts
@@ -4,8 +4,9 @@ import type { DocumentBase as IDocumentBase } from "../../types/schema"
 import { type Authorize as IAuthorize } from "../utils/mixins/Authorize"
 import type { Migration as IMigration } from "./Migration"
 
-export interface MigrationItem extends Omit<MigrationItemEntity, keyof Entity | `migration`>, IAuthorize, IDocumentBase {
+export interface MigrationItem extends Omit<MigrationItemEntity, keyof Entity | `migration` | `status`>, IAuthorize, IDocumentBase {
   migration: IMigration
+  status: MigrationItemStatus
 }
 
 export default defineDocumentModel<MigrationItem>(`MigrationItem`, defineDocumentSchema<MigrationItem>({
@@ -36,20 +37,34 @@ export default defineDocumentModel<MigrationItem>(`MigrationItem`, defineDocumen
   },
   status: {
     type: EntityFieldTypes.Number,
-    enum: MigrationItemStatus,
+    required: true,
+    // TODO: Use enum
+    min: 1,
+    max: 31,
     default: MigrationItemStatus.INITIAL,
   },
 }, {
   alterSchema(schema) {
-    schema.pre(`save`, function (next) {
+    schema.pre(`save`, async function () {
       this.$locals.isNew = this.isNew
-      next()
+
+      // Validate status does not transition from IMPORTED to ERRORED or vice versa
+      if (this.isNew) {
+        const before = await MigrationItem.mongoose.model.findById(this._id)
+        const { IMPORTED, ERRORED } = MigrationItemStatus
+        if (before && ((before.status & IMPORTED && this.status & ERRORED) || (before.status & ERRORED && this.status & IMPORTED))) {
+          const [beforeLabel, afterLabel] = [before, this].map(item => useEnum(MigrationItemStatus).labelOf(item.status))
+          throw new Error(`Cannot transition from ${beforeLabel} to ${afterLabel}`)
+        }
+      }
     })
+
     schema.post(`save`, async function () {
       if (this.$locals.isNew) {
         await Migration.mongoose.model.updateOne({ _id: this.migration }, { $inc: { total: 1 } })
       }
     })
+
     schema.post(`deleteOne`, { document: true, query: false }, async function () {
       await Migration.mongoose.model.updateOne({ _id: this.migration }, { $inc: { total: -1 } })
     })
@@ -63,7 +78,4 @@ export default defineDocumentModel<MigrationItem>(`MigrationItem`, defineDocumen
     ]
   },
 }))
-  .mixin(Stateful({
-    values: MigrationItemStatus,
-    default: MigrationItemStatus.INITIAL,
-  })).mixin(DocumentBase())())
+  .mixin(DocumentBase())())
