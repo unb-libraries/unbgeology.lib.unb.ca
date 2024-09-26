4f3cef67e79f6e9c4b8286feeff0a25f28ec3001
Patrick Schilf <pschilf@unb.ca>
2024-09-26 11:09:32 -0300

    Prevent parent population in non-hierarchical term slugification

--- app/layers/mongo/server/documentTypes/Term.ts
+++ app/layers/mongo/server/documentTypes/Term.ts
@@ -1,8 +1,8 @@
-import { type Term as TermEntity, type Entity } from "@unb-libraries/nuxt-layer-entity"
-import { type DocumentBase } from "../../types/schema"
-import type { Authorize } from "../utils/mixins/Authorize"
+import type { Term as TermEntity, Entity } from '@unb-libraries/nuxt-layer-entity'
+import type { DocumentBase as IDocumentBase } from '../../types/schema'
+import type { Authorize } from '../utils/mixins/Authorize'
 
-export interface Term extends Omit<TermEntity, keyof Entity>, Authorize, DocumentBase {
+export interface Term extends Omit<TermEntity, keyof Entity>, Authorize, IDocumentBase {
 }
 
 export default defineDocumentModel<Term>(`Term`, defineDocumentSchema<Term>({
@@ -18,9 +18,9 @@ export default defineDocumentModel<Term>(`Term`, defineDocumentSchema<Term>({
   async path(term) {
     // REFACTOR: Terms are not hierarchical, move to Taxonomy type
     const label = term.get(`label`)
-    if (`parent` in term && term.parent) {
+    if (term.get(`parent`)) {
       await term.populate(`parent`)
-      const parent = term.parent as Term
+      const parent = term.get(`parent`)
       return parent.slug ? `${parent.slug} ${label}` : label
     }
     return label
