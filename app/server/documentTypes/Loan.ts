b49b4a5672200bcf47c289f1663b5f669ba96911
Patrick Schilf <pschilf@unb.ca>
2024-11-25 13:51:48 -0400

    Define Loan_Specimen relationship model

--- app/server/documentTypes/Loan.ts
+++ app/server/documentTypes/Loan.ts
@@ -1,12 +1,41 @@
 import type { Entity } from "@unb-libraries/nuxt-layer-entity"
+import type { Loan as LoanEntity } from "types/loan"
+import type { Specimen } from "./Specimen"
 import { EntityFieldTypes } from "~/layers/mongo/types/entity"
 import type { DocumentBase as IDocumentBase } from "~/layers/mongo/types/schema"
-import type { Loan as LoanEntity } from "types/loan"
 
-export interface Loan extends Omit<LoanEntity, keyof Entity | `start` | `end`>, IDocumentBase {
+export interface Loan extends Omit<LoanEntity, keyof Entity | `start` | `end` | `specimens`>, IDocumentBase {
   start: number
   end: number
 }
+
+export interface Specimen_Loan extends IDocumentBase {
+  loan: Loan
+  specimen: Specimen
+  id?: string
+  url?: string
+}
+
+export defineDocumentModel(`Specimen_Loan`, defineDocumentSchema<Specimen_Loan>({
+  loan: {
+    type: EntityFieldTypes.ObjectId,
+    ref: `Loan`,
+    required: true,
+  },
+  specimen: {
+    type: EntityFieldTypes.ObjectId,
+    ref: `Specimen`,
+    required: true,
+  },
+  id: {
+    type: EntityFieldTypes.String,
+    required: false,
+  },
+  url: {
+    type: EntityFieldTypes.String,
+    required: false,
+  },
+}).mixin(DocumentBase()))
 
 export default defineDocumentModel(`Loan`, defineDocumentSchema<Loan>({
   description: {
@@ -48,11 +77,6 @@ export default defineDocumentModel(`Loan`, defineDocumentSchema<Loan>({
     },
     required: true,
   },
-  specimens: {
-    type: [EntityFieldTypes.ObjectId],
-    ref: `Specimen`,
-    required: true,
-  }
   contract: {
     type: EntityFieldTypes.ObjectId,
     ref: `File`,
