a9fa416241726ca61ce55698cc743d9795e27a37
Patrick Schilf <pschilf@unb.ca>
2024-12-02 16:17:20 -0400

    Loan index page

---
+++ app/pages/dashboard/loans/index.vue
@@ -0,0 +1,154 @@
+<template>
+  <NuxtLayout name="dashboard-page">
+    <template #actions>
+      <NuxtLink
+        v-if="hasPermission(/^create:loan/)"
+        to="/dashboard/loans/create"
+        class="button button-lg button-accent-mid hover:button-accent-light"
+      >
+        Add loan
+      </NuxtLink>
+    </template>
+
+    <EntityTable
+      v-model="selection"
+      :entities="loans"
+      :columns="['id', 'start', 'end', ['specimens', '#Specimens']]"
+      class="border-primary-60/75 w-full border-b"
+      header-cell-class="group"
+      row-class="table-row"
+      selected-row-class="active"
+    >
+      <template #id="{ entity: loan }">
+        <NuxtLink
+          :to="`/dashboard/loans/${loan.id}`"
+          class="hover:underline"
+        >
+          {{ loan.id }}
+        </NuxtLink>
+      </template>
+      <template #start="{ entity: { start } }">
+        {{ new Date(start).toLocaleDateString() }}
+      </template>
+      <template #end="{ entity: { end } }">
+        {{ new Date(end).toLocaleDateString() }}
+      </template>
+      <template #specimens="{ entity: { specimens } }">
+        {{ specimens.length }}
+      </template>
+    </EntityTable>
+    <div class="flex w-full flex-row justify-between px-4">
+      <span
+        v-if="list?.total"
+        class="italic"
+      >
+        {{ (page - 1) * pageSize + 1 }} - {{ Math.min(list?.total ?? 0, page * pageSize) }} of {{ pluralize(list?.total ?? 0, `loan`, `loans`) }}
+      </span>
+      <TwPageIndex
+        :page="page"
+        :total="Math.ceil((list?.total ?? 0) / pageSize)"
+        :size="5"
+        @change="(index) => { page = index }"
+      />
+    </div>
+
+    <template #sidebar>
+      <EntityAdminSidebar
+        v-if="selection"
+        :entities="[selection]"
+      >
+        <PvEntityDetails
+          :entity="selection"
+          :fields="columns"
+          class="space-y-4"
+          label-class="font-bold italic"
+        >
+          <template #start="{ entity: { start } }">
+            {{ new Date(start).toLocaleDateString() }}
+          </template>
+          <template #end="{ entity: { end } }">
+            {{ new Date(end).toLocaleDateString() }}
+          </template>
+          <template #specimens="{ entity: { specimens } }">
+            <ul>
+              <li
+                v-for="specimen in specimens"
+                :key="specimen.id"
+              >
+                <NuxtLink :to="`/dashboard/specimens/${specimen.id}`">
+                  {{ specimen.id.toUpperCase() }}
+                </NuxtLink>
+              </li>
+            </ul>
+          </template>
+          <template #contact="{ entity: { contact } }">
+            <div class="flex flex-col">
+              <span>{{ contact.name }}</span>
+              <span>{{ contact.affiliation }}</span>
+              <span>{{ contact.email }}</span>
+              <span>{{ contact.phone }}</span>
+            </div>
+          </template>
+          <template #contract="{ entity: { contract } }">
+            <span v-if="contract">{{ contract.id }}</span>
+            <span v-else>Nothing attached</span>
+          </template>
+        </PvEntityDetails>
+        <template #actions>
+          <div class="space-y-2">
+            <button
+              v-if="hasPermission(/^delete:loan/)"
+              class="button button-lg button-outline-red-dark hover:button-red-dark w-full"
+              @click.stop.prevent="onClickRemove"
+            >
+              Delete
+            </button>
+          </div>
+        </template>
+      </EntityAdminSidebar>
+    </template>
+  </NuxtLayout>
+</template>
+
+<script setup lang="tsx">
+import type { EntityJSON } from "@unb-libraries/nuxt-layer-entity"
+import { PvEntityDeleteConfirm } from "#components"
+import type { Loan } from "~/types/loan"
+
+definePageMeta({
+  layout: false,
+  name: `Loans`,
+  auth: {
+    redirect: true,
+    permission: /^(update|delete):loan/,
+  },
+  menu: {
+    weight: 110,
+  },
+})
+
+const { hasPermission } = useCurrentUser()
+const { setContent, close: closeModal } = useModal()
+const { createToast } = useToasts()
+
+const { entities: loans, list, remove, error, query: { page, pageSize } } = await fetchEntityList<Loan>(`Loan`)
+const columns: [keyof Loan, string][] = [[`id`, `ID`], [`start`, `Start`], [`end`, `End`], [`specimens`, `#Specimens`], [`contact`, `Contact`], [`contract`, `Contract`]]
+const selection = ref<EntityJSON<Loan>>()
+
+function onClickRemove() {
+  const label = `the loan "${selection.value!.id}"`
+  setContent(() => <PvEntityDeleteConfirm label={label} onConfirm={onRemove} onCancel={closeModal} />)
+}
+
+async function onRemove() {
+  const id = selection.value!.id
+  await remove(selection.value!)
+  if (error.value) {
+    createToast(`loan-delete-error`, () => error.value, { type: `error` })
+  } else {
+    createToast(`loan-delete-success`, () => `Deleted loan "${id}".`, { type: `success`, duration: 4000 })
+  }
+  selection.value = undefined
+  closeModal()
+}
+</script>
