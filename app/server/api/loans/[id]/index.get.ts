6c162e310fe45e89ca96775b0c4696acdb477b5f
Patrick Schilf <pschilf@unb.ca>
2024-12-02 13:59:48 -0400

    GET /api/loans/:id endpoint

---
+++ app/server/api/loans/[id]/index.get.ts
@@ -0,0 +1,25 @@
+import type { Loan as ILoan } from "~/types/loan"
+
+export default defineEventHandler(async (event) => {
+  const { id } = getRouterParams(event)
+  const { select } = getLoanQueryParams(event)
+
+  const query = Loan.mongoose.model.findById(id)
+  if (select.includes(`specimens`)) {
+    query.populate(`specimens`)
+  }
+  if (select.includes(`contract`)) {
+    query.populate(`contract`)
+  }
+
+  const loan = await query
+  if (!loan) {
+    throw createError({ statusCode: 404, statusMessage: `Loan entity not found.` })
+  }
+
+  return {
+    self: `/api/loans/${id}`,
+    id: `${id}`,
+    ...Object.fromEntries(Object.entries(loan.toJSON<ILoan>()).filter(([key]) => select.includes(key as keyof ILoan))),
+  }
+})
