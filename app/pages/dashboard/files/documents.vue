4267a7cc1476dcbd7e40ce92c6a42746063e2449
Patrick Schilf <pschilf@unb.ca>
2024-12-05 12:57:42 -0400

    Document list page

---
+++ app/pages/dashboard/files/documents.vue
@@ -0,0 +1,16 @@
+<template>
+  <TwDocumentBrowser
+    v-model="selection"
+  />
+</template>
+
+<script setup lang="tsx">
+import type { Document, EntityJSON } from "@unb-libraries/nuxt-layer-entity"
+
+definePageMeta({
+  layout: `dashboard-page`,
+  name: `Documents`,
+})
+
+const selection = ref<EntityJSON<Document>>()
+</script>
