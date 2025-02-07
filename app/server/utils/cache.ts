4f0181538af8a81c6eca73c0d56c44505672208e
Patrick Schilf <pschilf@unb.ca>
2025-02-07 09:55:31 -0400

    Include "filter" in caching key

--- app/server/utils/cache.ts
+++ app/server/utils/cache.ts
@@ -2,7 +2,7 @@ import { objectHash, sha256base64 } from "ohash"
 import type { H3Event } from "h3"
 
 export function getSpecimenRequestCacheId(event: H3Event) {
-  const { page, pageSize, select, search, sort, where } = getSpecimenQueryParams(event)
+  const { page, pageSize, select, search, sort, where, filter } = getSpecimenQueryParams(event)
     
   const permissions = getCurrentUserPermissions(event)
   const resources = getAuthorizedResources(event, r => /^specimen(:\w)*$/.test(r))
@@ -13,8 +13,17 @@ export function getSpecimenRequestCacheId(event: H3Event) {
     pageSize,
     search,
     permissions,
-    select: select.sort().filter(field => !authFields.length || authFields.includes(field)),
-    sort: sort.filter(([field]) => !authFields.length || authFields.includes(field)),
-    where: Object.fromEntries(Object.entries(where).sort(([a], [b]) => a < b ? -1 : a > b ? 1 : 0).filter(([field]) => !authFields.length || authFields.includes(field))),
+    select: select
+      .sort()
+      .filter(field => !authFields.length || authFields.includes(field)),
+    sort: sort
+      .filter(([field]) => !authFields.length || authFields.includes(field)),
+    where: Object.fromEntries(Object.entries(where)
+      .filter(([field]) => !authFields.length || authFields.includes(field))
+      .sort(([f1, d1], [f2, d2]) => f1 < f2 ? -1 : f1 > f2 ? 1 : d1 < d2 ? -1 : d1 > d2 ? 1 : 0)),
+    filter: filter
+      .filter(([field]) => !authFields.length || authFields.includes(field))
+      .map(([f, op, v]) => [f, `${op}:${v}`])
+      .sort(([f1, o1], [f2, o2]) => f1 < f2 ? -1 : f1 > f2 ? 1 : o1 < o2 ? -1 : o1 > o2 ? 1 : 0),
   }))
 }
