d2b6aa1726d504b11f9e3d8c1c94ea6622f3b7f8
Patrick Schilf <pschilf@unb.ca>
2025-02-11 15:03:17 -0400

    Classifications filter component

---
+++ app/components/Filter/Classification.vue
@@ -0,0 +1,46 @@
+<template>
+  <Filter title="Classification" v-model:collapsed="collapsed">
+    <div class="group relative" tabindex="0">
+      <input v-model="search" placeholder="Search" name="search-classifications" :class="['input w-full input-text-md placeholder:text-primary dark:placeholder:text-primary-20 placeholder:italic', { 'group-focus-within:rounded-b-none': options.length }]">
+      <ul v-if="options.length" class="invisible group-focus-within:visible absolute bg-primary w-full border-x border-b rounded-b-md border-primary-80 max-h-48 overflow-y-scroll">
+        <li v-for="{ self, label } in options" :key="self" tabindex="0" class="px-3 py-1 hover:bg-accent-mid hover:text-white cursor-pointer" @keypress.enter.space="onSelect([self, label])" @click="onSelect([self, label])">
+          {{ label }}
+        </li>
+      </ul>
+    </div>
+    <ul v-if="selection.length" class="flex flex-col w-full p-2 divide-y divide-primary-40 divide-dashed">
+      <li v-for="[self, label] in selection" class="inline-flex items-center py-2 first:pt-0 last:pb-0">
+        <span class="grow">{{ label }}</span>
+        <button class="cursor-pointer rounded-md flex-none p-1 hover:bg-primary-40" @click="selection = selection.filter(([s]) => s !== self)"><IconCancel class="size-4 fill-none stroke-2 stroke-current" /></button>
+      </li>
+    </ul>
+  </Filter>
+</template>
+
+<script lang="ts" setup>
+import { type EntityJSONList, type EntityJSON } from '@unb-libraries/nuxt-layer-entity'
+import { type Classification } from '~/types/classification'
+
+const selection = defineModel<[string, string][]>({ default: [] })
+defineProps<{}>()
+
+const collapsed = ref(false)
+const results = ref<EntityJSON<Classification>[]>([])
+const options = computed(() => results.value.filter(({ self }) => !selection.value.some(([s]) => s === self)))
+const search = ref('')
+
+function onSelect(item: [string, string]) {
+  selection.value = [...selection.value, item]
+}
+
+watch(search, async (search) => {
+  if (search) {
+    const { data: classifications } = await useFetch<EntityJSONList<Classification>>(`/api/terms/classifications`, {
+      query: { filter: ['label:match:' + search] }
+    })
+    results.value = classifications.value?.entities ?? []
+  } else {
+    results.value = []
+  }
+})
+</script>
