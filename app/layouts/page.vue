4a9b93df56e5b0b7ee9749966c73a2924ba0ab30
Patrick Schilf <pschilf@unb.ca>
2025-02-14 12:23:18 -0400

    More compact page layout

--- app/layouts/page.vue
+++ app/layouts/page.vue
@@ -1,13 +1,13 @@
 <template>
   <NuxtLayout name="default">
     <header class="container mx-auto flex-none">
-      <h1 class="my-8 text-4xl">
+      <h1 class="my-4 text-4xl">
         <slot name="title">
           {{ name }}
         </slot>
       </h1>
     </header>
-    <div id="content" class="container grow overflow-y-hidden mx-auto mb-16 mt-4">
+    <div id="content" class="container grow overflow-y-hidden mx-auto mb-8 mt-4">
       <slot />
     </div>
   </NuxtLayout>
--- app/pages/search.vue
+++ app/pages/search.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="space-y-2 flex-col flex h-full">
     <span v-if="list?.total" class="flex-none">Displaying {{ specimens.length }} of {{ list?.total }} specimens</span>
-    <div class="flex-none w-full flex">
+    <div class="flex-none space-x-1 w-full flex">
       <div class="form-field grow">
         <label class="sr-only" for="search">Search</label>
         <input v-model="search" placeholder="Search" name="search" class="placeholder:text-primary dark:placeholder:text-primary-20 rounded-md form-input form-input-text grow p-2 placeholder:italic">
       </div>
-      <button class="justify-center hover:border-accent-mid items-center flex border rounded-md border-primary-60 aspect-square bg-primary flex-none cursor-pointer" @click.prevent.stop="onSwitchViewMode('list')">
+      <button v-show="viewMode === 'map'" class="justify-center hover:border-accent-light items-center flex border rounded-md border-primary-60 aspect-square bg-primary flex-none cursor-pointer" @click.prevent.stop="onSwitchViewMode('list')">
         <IconList class="fill-none stroke-current size-6 stroke-1.5 flex" />
       </button>
-      <button class="justify-center items-center flex hover:border-accent-mid border rounded-md border-primary-60 aspect-square bg-primary flex-none cursor-pointer" @click.prevent.stop="onSwitchViewMode('map')">
+      <button v-show="viewMode === 'list'" class="justify-center items-center flex hover:border-accent-light border rounded-md border-primary-60 aspect-square bg-primary flex-none cursor-pointer" @click.prevent.stop="onSwitchViewMode('map')">
         <IconMap class="fill-none stroke-current size-6 stroke-1.5 flex" />
       </button>
     </div>
@@ -63,7 +63,7 @@
           <ul v-if="viewMode === 'list' && list?.total" class="space-y-2">
             <li v-for="specimen in specimens" :key="specimen.self" class="bg-primary-60">
               <div class="flex flex-row">
-                <div class="h-24 aspect-square bg-primary-20 flex justify-center items-center">
+                <div class="h-20 aspect-square bg-primary-20 flex justify-center items-center">
                   <img v-if="specimen.images?.total > 0" :src="`${specimen.images?.entities[0].uri}?w=40&h=40`" class="aspect-square object-cover">
                   <IconFossil v-else-if="specimen.type === 'fossil'" class="size-16 stroke-primary-40 fill-none" />
                   <IconRock v-else-if="specimen.type === 'rock'" class="size-16 stroke-primary-40 fill-none" />
