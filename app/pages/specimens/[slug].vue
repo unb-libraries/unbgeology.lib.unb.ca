ed07ea229c35a5536cc275d0e5044e974813bf6c
Patrick Schilf <pschilf@unb.ca>
2025-02-20 11:52:03 -0400

    Responsive detail view

--- app/pages/specimens/[slug].vue
+++ app/pages/specimens/[slug].vue
@@ -6,83 +6,112 @@
       </h1>
       <span class="-mt-1 block text-sm italic">#{{ specimen!.id.toUpperCase() }}</span>
     </header>
-    <div class="flex flex-row space-x-8">
-      <div class="flex w-2/3 flex-col space-y-12">
-        <section>
-          <div v-if="(specimen?.images?.total ?? 0) > 0" class="space-y-1">
-            <img :src="`${activeImage?.[1]}?w=1024&h=731`" />
-            <div class="w-full h-24 inline-flex gap-x-1 overflow-x-scroll">
-              <img v-for="{ self, uri } in specimen?.images?.entities" :key="self" :src="`${uri}?w=100&h=100`" :class="['cursor-pointer hover:opacity-50 size-24', { 'opacity-50': self === activeImage?.[0] }]" @click="activeImage = [self, uri]" />
-            </div>
+    <div class="flex w-full flex-col space-y-8 items-end">
+      <section class="w-full flex flex-col lg:flex-row gap-8">
+        <div v-if="(specimen?.images?.total ?? 0) > 0" class="space-y-1 w-full lg:w-3/4">
+          <div class="aspect-7/5 w-full">
+            <img :src="`${activeImage?.[1]}?w=1280&h=915`" />
           </div>
-          <div v-else class="bg-primary-20 dark:bg-primary-60 aspect-7/5 w-full text-base dark:text-primary-40 justify-center items-center flex">
-            <IconFossil v-if="specimen!.type === 'fossil'" class="size-48 fill-none stroke-current stroke-1.5" />
-            <IconMineral v-else-if="specimen!.type === 'mineral'" class="size-48 fill-none stroke-current stroke-1.5" />
-            <IconRock v-else class="size-48 fill-none stroke-current stroke-1.5" />
+          <div class="w-full h-24 inline-flex gap-x-1 overflow-x-scroll">
+            <img v-for="{ self, uri } in specimen?.images?.entities" :key="self" :src="`${uri}?w=100&h=100`" :class="['cursor-pointer hover:opacity-50 size-24', { 'opacity-50': self === activeImage?.[0] }]" @click="activeImage = [self, uri]" />
           </div>
-        </section>
-        <section v-if="specimen?.description" class="ml-64 text-justify">
+        </div>
+        <div v-else class="bg-primary-20 dark:bg-primary-60 aspect-7/5 text-base dark:text-primary-40 justify-center items-center flex w-3/4 h-full">
+          <IconFossil v-if="specimen!.type === 'fossil'" class="size-48 fill-none stroke-current stroke-1.5" />
+          <IconMineral v-else-if="specimen!.type === 'mineral'" class="size-48 fill-none stroke-current stroke-1.5" />
+          <IconRock v-else class="size-48 fill-none stroke-current stroke-1.5" />
+        </div>
+        <dl class="flex flex-col w-full mx-auto lg:w-1/4 lg:gap-y-4">
+          <!-- Category -->
+          <div class="grid grid-cols-2 lg:grid-cols-1 gap-x-4">
+            <dt class="text-end lg:text-start text-primary-40 uppercase font-bold">Category</dt>
+            <dd>{{ specimen!.type[0].toUpperCase() + specimen!.type.slice(1).toLowerCase() }}</dd>
+          </div>
+          <!-- Classification -->
+          <div class="grid grid-cols-2 lg:grid-cols-1 gap-x-4">
+            <dt class="text-end lg:text-start text-primary-40 uppercase font-bold">Classification</dt>
+            <dd>{{ specimen!.classification?.label ?? "Unknown" }}</dd>
+          </div>
+          <!-- Date -->
+          <div class="grid grid-cols-2 lg:grid-cols-1 gap-x-4">
+            <dt class="text-end lg:text-start text-primary-40 uppercase font-bold">Date</dt>
+            <dd>{{ specimen!.date ?? "Unknown" }}</dd>
+          </div>
+          <!-- Composition -->
+          <div class="grid grid-cols-2 lg:grid-cols-1 gap-x-4">
+            <dt class="text-end lg:text-start text-primary-40 uppercase font-bold">Composition</dt>
+            <dd>
+              <template v-if="specimen!.type !== 'mineral'">{{(specimen! as Fossil | Rock).composition?.entities.map(({ label }) => label).join(`, `)}}</template>
+              <!-- FIX: Composition is not included in Mineral classification -->
+              <template v-else-if="(specimen!.classification as Mineral)?.composition">{{ (specimen!.classification as Mineral).composition }}</template>
+              <template v-else>Unknown</template>
+            </dd>
+          </div>
+          <!-- Age -->
+          <div class="grid grid-cols-2 lg:grid-cols-1 gap-x-4">
+            <dt class="text-end lg:text-start text-primary-40 uppercase font-bold">Age</dt>
+            <dd>
+              <template v-if="specimen?.age?.relative">
+                {{specimen?.age?.relative?.map(({ label, start }, i) => `${label} (${Number(specimen!.age.numeric?.[i] ?? start) / 1000000} Mya)`).join(' - ')}}
+              </template>
+              <template v-else>Unknown</template>
+            </dd>
+          </div>
+          <!-- Pieces -->
+          <div class="grid grid-cols-2 lg:grid-cols-1 gap-x-4">
+            <dt class="text-end lg:text-start text-primary-40 uppercase font-bold">Pieces</dt>
+            <dd>{{ specimen?.pieces }}{{ specimen?.partial ? ` (Partial)` : `` }}</dd>
+          </div>
+          <!-- Measurements -->
+          <div class="grid grid-cols-2 lg:grid-cols-1 gap-x-4">
+            <dt class="text-end lg:text-start text-primary-40 uppercase font-bold">Measurements</dt>
+            <dd>
+              <ul v-for="dimensions in specimen!.measurements?.dimensions" :key="dimensions.join('x')">
+                <li>{{dimensions.map(d => `${d}mm`).join(' x ')}}</li>
+              </ul>
+            </dd>
+          </div>
+          <!-- Storage -->
+          <div class="grid grid-cols-2 lg:grid-cols-1 gap-x-4">
+            <dt class="text-end lg:text-start text-primary-40 uppercase font-bold">Storage</dt>
+            <dd>{{ specimen?.storage?.entities.at(-1)?.location.public ? `On Display` : `In Archive` }}</dd>
+          </div>
+        </dl>
+      </section>
+      <section v-if="specimen?.description" class="w-3/4 mx-auto lg:mx-0">
+        <h2 class="sr-only text-primary-40 mb-3 text-lg font-bold uppercase">
+          Description
+        </h2>
+        <div>
           {{ specimen?.description }}
-        </section>
-        <section v-if="specimen?.origin" class="ml-64 w-full">
-          <h2 class="text-primary-40 mb-3 text-lg font-bold uppercase">
-            Place of Origin
-          </h2>
-          <LeafletMap class="min-h-128 w-full" :zoom="7" :center="[specimen?.origin?.latitude ?? 0, specimen?.origin?.longitude ?? 0]">
-            <LeafletMarker
-              v-if="specimen.origin"
-              :name="specimen.name"
-              :center="[specimen?.origin.latitude, specimen?.origin.longitude]"
-            />
-          </LeafletMap>
-        </section>
-        <section v-if="(specimen?.publications ?? []).length > 0" class="ml-64">
-          <h2 class="text-primary-40 mb-3 text-lg font-bold uppercase">
-            Publications
-          </h2>
-          <PvEntityList class="list-inside list-decimal" :entities="publications" item-class="my-3 first:mt-0 last:mb-0" v-slot="{ entity: publication }">
-            <a :href="publication.doi" class="hover:underline">{{ publication.citation }}</a>
-          </PvEntityList>
-        </section>
-      </div>
-      <div class="w-1/3">
-        <PvEntityDetails :entity="specimen!" :fields="[['type', 'Category'], 'classification', `date`, `age`, `composition`, `pieces`, `measurements`, `storage`]" item-class="my-6 first:mt-0 last:mb-0" label-class="text-primary-40 uppercase">
-          <template #type>{{ specimen!.type[0].toUpperCase() + specimen!.type.slice(1).toLowerCase() }}</template>
-          <template #classification>
-            {{ classificationLabels.join(' > ') }}
-          </template>
-          <template #date>{{ specimen!.date ?? 'Unknown' }}</template>
-          <template #composition>
-            <template v-if="specimen!.type !== 'mineral'">{{(specimen! as Fossil | Rock).composition?.entities.map(({ label }) => label).join(`, `)}}</template>
-            <!-- FIX: Composition is not included in Mineral classification -->
-            <template v-else-if="(specimen!.classification as Mineral)?.composition">{{ (specimen!.classification as Mineral) }}</template>
-            <template v-else>Unknown</template>
-          </template>
-          <template #age>
-            <template v-if="specimen?.age?.relative">
-              {{specimen?.age?.relative?.map(({ label, start }, i) => `${label} (${Number(specimen!.age.numeric?.[i] ?? start) / 1000000} Mya)`).join(' - ')}}
-            </template>
-            <template v-else>Unknown</template>
-          </template>
-          <template #pieces>
-            {{ specimen?.pieces }}{{ specimen?.partial ? ` (Partial)` : `` }}
-          </template>
-          <template #measurements>
-            <ul v-for="dimensions in specimen!.measurements?.dimensions" :key="dimensions.join('x')">
-              <li>{{dimensions.map(d => `${d}mm`).join(' x ')}}</li>
-            </ul>
-          </template>
-          <template #storage>
-            {{ specimen?.storage.at(-1)?.location.public ? `On Display` : `In Archive` }}
-          </template>
-        </PvEntityDetails>
-      </div>
+        </div>
+      </section>
+      <section v-if="specimen?.origin" class="w-full lg:w-3/4">
+        <h2 class="text-primary-40 mb-3 text-lg font-bold text-center lg:text-start uppercase">
+          Place of Origin
+        </h2>
+        <LeafletMap class="min-h-128 w-full" :zoom="7" :center="[specimen?.origin?.latitude ?? 0, specimen?.origin?.longitude ?? 0]">
+          <LeafletMarker
+            v-if="specimen.origin"
+            :name="specimen.name"
+            :center="[specimen?.origin.latitude, specimen?.origin.longitude]"
+          />
+        </LeafletMap>
+      </section>
+      <section v-if="publications.length" class="w-full lg:w-3/4">
+        <h2 class="text-center lg:text-start mx-auto lg:mx-0 text-primary-40 mb-3 text-lg font-bold uppercase">
+          Publications
+        </h2>
+        <PvEntityList class="list-inside list-decimal" :entities="publications" item-class="my-3 first:mt-0 last:mb-0" v-slot="{ entity: publication }">
+          <a :href="publication.doi" class="hover:underline">{{ publication.citation }}</a>
+        </PvEntityList>
+      </section>
     </div>
   </article>
 </template>
 
 <script setup lang="ts">
-import { type Fossil, type Publication, type Rock, type Specimen } from 'types/specimen'
+import { type Fossil, type Rock, type Specimen } from 'types/specimen'
 import type { Mineral } from 'types/classification'
 
 definePageMeta({
@@ -103,5 +132,5 @@ const classificationLabels = computed(() => [
 ].reverse())
 
 const activeImage = ref(specimen?.value?.images?.entities?.length ? [specimen!.value?.images?.entities[0].self, specimen!.value?.images?.entities[0].uri] : undefined)
-const publications = computed(() => specimen.value?.publications ?? [])
+const publications = computed(() => specimen.value?.publications?.entities ?? [])
 </script>
