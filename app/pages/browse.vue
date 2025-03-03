0cfccdb3c592a268ea1283511960d4353355cfae
Patrick Schilf <pschilf@unb.ca>
2025-03-03 16:35:44 -0400

    Render classification images on browse page

--- app/pages/browse.vue
+++ app/pages/browse.vue
@@ -6,7 +6,11 @@
       <li :class="['cursor-pointer rounded-md px-4 py-2', { 'bg-primary-40 text-base dark:bg-primary-40 dark:text-primary': selection === 'rock', 'bg-transparent dark:hover:bg-primary-40 dark:hover:text-primary dark:text-base': selection !== 'rock' }]" @click="onSelect('rock')">Rocks</li>
     </ul>
     <ul class="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 2xl:grid-cols-10 gap-2 overflow-y-scroll">
-      <li v-for="classification in list?.entities" class="cursor-pointer hover:border-accent-dark hover:text-accent-dark dark:hover:border-accent-mid dark:hover:text-accent-mid aspect-square flex justify-center items-center bg-primary-20 dark:bg-primary-80 border border-primary-40 dark:border-primary-60"><a href="/specimens">{{ classification.label }}</a>
+      <li v-for="classification in list?.entities" class="p-1 cursor-pointer hover:border-accent-dark hover:text-accent-dark dark:hover:border-accent-mid dark:hover:text-accent-mid aspect-square flex justify-center items-center bg-primary-20 dark:bg-primary-80 border border-primary-40 dark:border-primary-60">
+        <a href="/specimens" class="flex flex-col gap-y-2 size-full items-center justify-center">
+          <img v-if="classification.image" :src="`${classification.image.uri}?w=320&h=320`" :title="classification.label" class="size-full object-cover" />
+          {{ classification.label }}
+        </a>
       </li>
     </ul>
   </div>
