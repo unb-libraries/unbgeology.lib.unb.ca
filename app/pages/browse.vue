9a57209cf263821a650b212ef5230c26280b18e1
Patrick Schilf <pschilf@unb.ca>
2025-03-04 11:47:03 -0400

    Browse to properly link to pre-filtered specimen page

--- app/pages/browse.vue
+++ app/pages/browse.vue
@@ -7,7 +7,7 @@
     </ul>
     <ul class="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 2xl:grid-cols-10 gap-2 overflow-y-scroll">
       <li v-for="classification in list?.entities" class="p-1 cursor-pointer hover:border-accent-dark hover:text-accent-dark dark:hover:border-accent-mid dark:hover:text-accent-mid aspect-square flex justify-center items-center bg-primary-20 dark:bg-primary-80 border border-primary-40 dark:border-primary-60">
-        <a href="/specimens" class="flex flex-col gap-y-2 size-full items-center justify-center">
+        <a :href="`/specimens?filter=classification:1:${classification.self}`" class="flex flex-col gap-y-2 size-full items-center justify-center">
           <img v-if="classification.image" :src="`${classification.image.uri}?w=320&h=320`" :title="classification.label" class="size-full object-cover" />
           {{ classification.label }}
         </a>
