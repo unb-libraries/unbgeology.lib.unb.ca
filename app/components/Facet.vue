13e25ef170cb21075324574f5fa02d9d2e4f1901
Patrick Schilf <pschilf@unb.ca>
2025-04-16 11:28:44 -0300

    Refactor facet component

--- app/components/Facet.vue
+++ app/components/Facet.vue
@@ -1,9 +1,9 @@
 <template>
   <Filter :title="title" v-model:collapsed="collapsed" toggler-class="hidden xl:block">
-    <div v-for="{ value, count } in options" class="inline-flex items-center space-x-1">
+    <div v-for="{ value, label, count } in options" class="inline-flex items-center space-x-1">
       <input type="checkbox" :id="`filter-${title.toLowerCase()}[${value}]`" :name="`${title.toLowerCase()}[${value}]`" class="size-5 rounded-md input input-checkbox" :value="value" :checked="selection.includes(value)" @change="selection = selection.includes(value) ? selection.filter(v => v !== value) : [...selection, value]">
       <label :for="`filter-${title.toLowerCase()}[${value}]`" class="text-lg mr-4 cursor-pointer">
-        {{ value }} ({{ count }})
+        {{ label }} ({{ count }})
       </label>
     </div>
   </Filter>
@@ -13,11 +13,31 @@
 const selection = defineModel<unknown[]>({ default: [] })
 const collapsed = defineModel<boolean>('collapsed', { default: false })
 
-defineProps<{
+const props = defineProps<{
   title: string
   options: {
     value: unknown
     count: number
   }[]
+  valueField?: string
+  labelField?: string
 }>()
+
+const options = computed(() => props.options.map(({ value, count }) => ({
+  value: getValue(value),
+  label: getLabel(value),
+  count,
+})))
+
+function getValue(value: unknown) {
+  return props.valueField && typeof value === `object` && value
+    ? value[props.valueField as keyof typeof value] ?? value
+    : value
+}
+
+function getLabel(value: unknown) {
+  return props.labelField && typeof value === `object` && value
+    ? value[props.labelField as keyof typeof value] ?? value
+    : value
+}
 </script>
