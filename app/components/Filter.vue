863afce304900145f13c166bcd6db85e0afc37ad
Patrick Schilf <pschilf@unb.ca>
2025-04-16 12:06:31 -0300

    Inner facet scrolling

--- app/components/Filter.vue
+++ app/components/Filter.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="bg-primary-20 dark:bg-primary-60 py-2 px-4 space-y-2 divide-y divide-primary-40">
-    <div class="inline-flex items-center justify-between w-full">
+  <div class="bg-primary-20 dark:bg-primary-60 py-2 px-4 space-y-2 divide-y divide-primary-40 overflow-y-hidden flex flex-col">
+    <div class="inline-flex items-center justify-between w-full flex-none">
       <button @click.prevent="collapsed = !collapsed" class="inline-flex items-center justify-between w-full cursor-pointer">
         <slot name="title" :title="title">
           <span class="block text-xl">{{ title }}</span>
@@ -9,8 +9,10 @@
         <IconAngleUp v-else class="size-8 fill-none stroke-white" :class="togglerClass" />
       </button>
     </div>
-    <div v-show="!collapsed" class="flex flex-col py-2">
-      <slot />
+    <div v-show="!collapsed" class="py-2 grow overflow-y-hidden">
+      <div class="flex flex-col h-full overflow-y-scroll">
+        <slot />
+      </div>
     </div>
   </div>
 </template>
