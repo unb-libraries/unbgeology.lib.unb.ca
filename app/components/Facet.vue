a834745adc93c460d4e92cef7f06a27f30e35937
Patrick Schilf <pschilf@unb.ca>
2025-04-22 12:22:35 -0300

    Always display facet toggle buttons

--- app/components/Facet.vue
+++ app/components/Facet.vue
@@ -1,5 +1,5 @@
 <template>
-  <Filter :title="title" v-model:collapsed="collapsed" toggler-class="hidden xl:block">
+  <Filter :title="title" v-model:collapsed="collapsed">
     <div v-for="{ value, label, count } in options" class="inline-flex items-center space-x-1">
       <input type="checkbox" :id="`filter-${title.toLowerCase()}[${value}]`" :name="`${title.toLowerCase()}[${value}]`" class="size-5 rounded-md input input-checkbox" :value="value" :checked="selection.includes(value)" @change="selection = selection.includes(value) ? selection.filter(v => v !== value) : [...selection, value]">
       <label :for="`filter-${title.toLowerCase()}[${value}]`" class="text-lg mr-4 cursor-pointer">
--- app/components/Filter.vue
+++ app/components/Filter.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="bg-primary-20 dark:bg-primary-60 py-2 px-4 space-y-2 divide-y divide-primary-40 overflow-y-hidden flex flex-col">
+  <div :class="['bg-primary-20 dark:bg-primary-60 py-2 px-4 space-y-2 divide-y divide-primary-40 overflow-y-hidden flex flex-col', { 'flex-none': collapsed }]">
     <div class="inline-flex items-center justify-between w-full flex-none">
-      <button @click.prevent="collapsed = !collapsed" class="inline-flex items-center justify-between w-full cursor-pointer">
+      <button @click.prevent.stop="collapsed = !collapsed" class="inline-flex items-center justify-between w-full cursor-pointer">
         <slot name="title" :title="title">
           <span class="block text-xl">{{ title }}</span>
         </slot>
