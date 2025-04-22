bf46c14bd877a0d27bf9cf2d8a2d0a4bb4ffce79
Patrick Schilf <pschilf@unb.ca>
2025-04-22 16:12:39 -0300

    Shrink/grow sidebar

--- app/pages/specimens/index.vue
+++ app/pages/specimens/index.vue
@@ -20,8 +20,8 @@
       <div :class="['xl:w-1/5 xl:h-full xl:relative', { 'fixed top-0 left-0 size-full bg-primary-80/80': !sidebarCollapsed }]" @click.stop.self="sidebarCollapsed = true">
         <div :class="['absolute gap-2 xl:relative xl:flex xl:flex-col bottom-0 max-h-4/5 xl:max-h-full xl:h-full left-0 w-full overflow-y-scroll', { hidden: sidebarCollapsed }]">
           <Facet v-if="facets.category" v-model="categories" :options="facets.category" title="Categories" class="flex-none" />
-          <Facet v-if="facets.classification" v-model="classifications" :options="facets.classification" value-field="self" label-field="label" title="Classification" class="grow" />
-          <Facet v-if="facets.age" v-model="units" :options="facets.age" value-field="self" label-field="label" title="Age" class="grow" />
+          <Facet v-if="facets.classification" v-model="classifications" :options="facets.classification" value-field="self" label-field="label" title="Classification" class="shrink" />
+          <Facet v-if="facets.age" v-model="units" :options="facets.age" value-field="self" label-field="label" title="Age" class="shrink" />
           <Facet v-if="facets.onDisplay" v-model="onDisplay" :options="facets.onDisplay.map(({ count }) => ({ value: 'Yes', count }))" value-field="self" label-field="label" title="On display" class="flex-none" />
         </div>
       </div>
