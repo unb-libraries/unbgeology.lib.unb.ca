0bb8c46d72afe71754e628b5b5a42024cc36568c
Patrick Schilf <pschilf@unb.ca>
2025-07-15 11:26:42 -0300

    Sticky site,page header

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="flex h-full flex-col">
-    <page-header />
-    <main class="flex flex-col grow overflow-y-scroll">
+  <div class="flex flex-col">
+    <page-header class="sticky top-0 bg-base dark:bg-primary-80 z-10" />
+    <main class="flex flex-col">
       <slot />
     </main>
     <page-footer />
--- app/layouts/page.vue
+++ app/layouts/page.vue
@@ -1,13 +1,13 @@
 <template>
   <NuxtLayout name="default">
-    <header class="container mx-auto flex-none">
+    <header class="container mx-auto flex-none sticky top-[5.5rem] z-20 bg-base dark:bg-primary-80">
       <h1 class="my-4 text-4xl">
         <slot name="title">
           {{ name }}
         </slot>
       </h1>
     </header>
-    <div id="content" class="container grow overflow-y-hidden mx-auto mb-8 mt-4">
+    <div id="content" class="container mx-auto mb-8">
       <slot />
     </div>
   </NuxtLayout>
