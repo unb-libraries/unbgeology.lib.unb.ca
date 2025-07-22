da8cd75dc72fe1b93fe89314cf55cc09537e0a71
Patrick Schilf <pschilf@unb.ca>
2025-07-22 15:53:22 -0300

    Fix numeric age facet reactivity

--- app/pages/specimens/index.vue
+++ app/pages/specimens/index.vue
@@ -29,13 +29,13 @@
       </div>
     </div>
     <div class="flex flex-col xl:flex-row grow gap-x-2">
-      <div v-show="Object.keys(list?.facets ?? {}).length" :class="['xl:w-1/5 xl:relative', { 'fixed top-0 left-0 size-full bg-primary-80/80': !sidebarCollapsed }]" @click.stop.self="sidebarCollapsed = true">
+      <div v-show="Object.keys(list?.facets ?? {}).length" :class="['xl:w-1/5 xl:relative z-40 xl:z-auto', { 'fixed top-0 left-0 size-full bg-primary-80/80': !sidebarCollapsed }]" @click.stop.self="sidebarCollapsed = true">
         <div :class="['absolute gap-2 xl:flex xl:flex-col bottom-0 xl:sticky xl:top-[calc(15.5rem+2px)] max-h-4/5 xl:max-h-[calc(100dvh-15.5rem-2px)] overflow-y-scroll left-0 w-full', { hidden: sidebarCollapsed }]">
           <Facet v-if="(facets.category ?? []).length" v-model="categories" :options="facets.category" value-field="id" label-field="label" title="Categories" class="flex-none" />
           <Facet v-if="(facets.classification ?? []).length" v-model="classifications" :options="facets.classification" value-field="self" label-field="label" title="Classification" :collapsible="10" class="shrink" />
           <Facet v-if="(facets.age ?? []).length" v-model="units" :options="facets.age" value-field="self" label-field="label" title="Age" :collapsible="10" class="shrink" />
           <Facet v-if="(facets.onDisplay ?? []).length" v-model="onDisplay" :options="facets.onDisplay.map(({ value, count }) => ({ value: { value, label: value === true ? 'Yes' : 'No' }, count }))" value-field="value" label-field="label" title="On display" class="flex-none" />
-          <Facet v-if="(facets.numericAge ?? []).length" v-model="numericAge" :options="facets.numericAge.map(({ value, count }) => ({ value, label: value.map((b: number) => Math.floor(b / 1000000)), count })).map(({ value, label, count }) => ({ value: { value, label: label[0] === 0 ? `< ${label[1]} Mya` : label.length < 2 ? `> ${label[0]} Mya` : `${label[0]} - ${label[1]} Mya` }, count }))" value-field="value" label-field="label" title="Numeric Age" class="flex-none" />
+          <Facet v-if="(facets.numericAge ?? []).length" v-model="numericAge" :options="facets.numericAge.map(({ value, count }) => ({ value: value[0], label: value.map((b: number) => Math.floor(b / 1000000)), count })).map(({ value, label, count }) => ({ value: { value, label: label[0] === 0 ? `< ${label[1]} Mya` : label.length < 2 ? `> ${label[0]} Mya` : `${label[0]} - ${label[1]} Mya` }, count }))" value-field="value" label-field="label" title="Numeric Age" class="flex-none" />
         </div>
       </div>
       <div class="grow h-full relative">
@@ -149,7 +149,7 @@ const mapCenter = ref<Coordinate>([46.65848709787655, -66.35685870803573]) // In
 const { entities: specimens, list, query: { page, pageSize, search, select, filter } } = await fetchEntityList<Specimen>("Specimen", {
   search: (Array.isArray(q.search) ? q.search.at(-1) : q.search) ?? '',
   select: ['id', 'name', 'images', 'type', 'classification', 'origin', 'status'],
-  page: (Array.isArray(q.page) ? Number(q.page.at(-1)) : q.page) ?? 1,
+  page: (Array.isArray(q.page) ? Number(q.page.at(-1)) : Number(q.page ?? 1)),
   pageSize: q.mode === 'map' ? 500 : 25,
   filter: (Array.isArray(q.filter) ? q.filter : [q.filter].filter(Boolean)).map(filter => filter?.split(':')),
 })
@@ -175,16 +175,21 @@ watch(filter, updateQuery)
 const categories = ref<string[]>(filter.value?.filter(([field]) => field === 'type').map(([, , value]) => value as string) ?? [])
 const classifications = ref<string[]>(filter.value?.filter(([field]) => field === 'classification').map(([, , value]) => value as string) ?? [])
 const units = ref<string[]>(filter.value?.filter(([field]) => field === 'age.relative').map(([, , value]) => value as string) ?? [])
-const numericAge = ref<[number, number][]>([(filter.value?.filter(([field, op]) => field === 'age.numeric' && [FilterOperator.GREATER, FilterOperator.LESS, FilterOperator.EQUALS].includes(op)).map(([, , value]) => Number(value)) ?? [])].filter(value => value.length === 2) ?? [])
+const numericAge = ref<number[]>([(filter.value?.filter(([field, op]) => field === 'age.numeric' && [FilterOperator.GREATER, FilterOperator.LESS, FilterOperator.EQUALS].includes(op)).map(([, , value]) => Number(value)) ?? [])].filter(value => value.length === 2) ?? [])
 const onDisplay = ref<boolean[]>(filter.value?.filter(([field]) => field === 'storage.location.public').map(([, , value]) => true) ?? [])
 
 function updateFilter() {
   filter.value = [
-    ...filter.value.filter(([field]) => !['type', 'classification', 'age.relative', 'storage.location.public'].includes(field)),
+    ...filter.value.filter(([field]) => !['type', 'classification', 'age.relative', 'age.numeric', 'storage.location.public'].includes(field)),
     ...categories.value.map(category => ['type', FilterOperator.EQUALS, category] as Filter),
     ...classifications.value.map(classification => ['classification', FilterOperator.EQUALS, classification] as Filter),
     ...units.value.map(unit => ['age.relative', FilterOperator.EQUALS, unit] as Filter),
-    ...numericAge.value.map((b) => b.map((b, i) => ['age.numeric', ((!i && FilterOperator.GREATER) || (FilterOperator.LESS | FilterOperator.EQUALS)), String(b)] as Filter)).flat(),
+    ...numericAge.value
+      .map((b) => ((facets.value.numericAge
+        .map(({ value }) => value as [number, number])
+        .find(([l]) => l === b)) ?? [])
+        .map((b, i) => ['age.numeric', ((!i && FilterOperator.GREATER) || (FilterOperator.LESS | FilterOperator.EQUALS)), String(b)] as Filter))
+      .flat(),
     ...onDisplay.value.map(() => ['storage.location.public', FilterOperator.EQUALS] as Filter),
   ]
 }
