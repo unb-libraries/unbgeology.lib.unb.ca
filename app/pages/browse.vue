124f8f2614c135b86078f5add162442ce447b7d8
Patrick Schilf <pschilf@unb.ca>
2025-07-24 12:18:17 -0300

    New "browse" layout

--- app/pages/browse.vue
+++ app/pages/browse.vue
@@ -1,18 +1,55 @@
 <template>
-  <div class="flex flex-col space-y-8 h-full">
-    <ul class="flex space-x-4 flex-none w-full justify-center">
-      <li :class="['cursor-pointer rounded-md px-4 py-2', { 'bg-primary-40 text-base dark:bg-primary-40 dark:text-primary': selection === 'fossil', 'bg-transparent dark:hover:bg-primary-40 dark:hover:text-primary dark:text-base': selection !== 'fossil' }]" @click="onSelect('fossil')">Fossils</li>
-      <li :class="['cursor-pointer rounded-md px-4 py-2', { 'bg-primary-40 text-base dark:bg-primary-40 dark:text-primary': selection === 'mineral', 'bg-transparent dark:hover:bg-primary-40 dark:hover:text-primary dark:text-base': selection !== 'mineral' }]" @click="onSelect('mineral')">Minerals</li>
-      <li :class="['cursor-pointer rounded-md px-4 py-2', { 'bg-primary-40 text-base dark:bg-primary-40 dark:text-primary': selection === 'rock', 'bg-transparent dark:hover:bg-primary-40 dark:hover:text-primary dark:text-base': selection !== 'rock' }]" @click="onSelect('rock')">Rocks</li>
-    </ul>
-    <ul class="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 2xl:grid-cols-10 gap-2 overflow-y-scroll">
-      <li v-for="classification in list?.entities" class="p-1 cursor-pointer hover:border-accent-dark hover:text-accent-dark dark:hover:border-accent-mid dark:hover:text-accent-mid aspect-square flex justify-center items-center bg-primary-20 dark:bg-primary-80 border border-primary-40 dark:border-primary-60">
-        <a :href="`/specimens?filter=classification:1:${classification.self}`" class="flex flex-col gap-y-2 size-full items-center justify-center">
-          <img v-if="classification.image" :src="`${classification.image.uri}?w=320&h=320`" :title="classification.label" class="size-full object-cover" />
-          {{ classification.label }}
-        </a>
-      </li>
-    </ul>
+  <div class="flex flex-col gap-y-12 w-full">
+    <div class="flex flex-col gap-y-2 w-full items-center overflow-x-hidden">
+      <div class="flex gap-x-2">
+        <button v-for="cat in ['fossil', 'mineral', 'rock']"
+          :key="cat"
+          :data-state="category === cat ? 'active' : 'inactive'"
+          class="bg-primary-40 px-4 py-2 border-none rounded-md data-[state=active]:bg-accent-mid hover:bg-accent-dark hover:data-[state=active]:bg-accent-dark"
+          @click="selectCategory(cat)" 
+        >
+          {{ cat.charAt(0).toUpperCase() + cat.slice(1) }}s
+        </button>
+      </div>
+      <div class="flex gap-x-2 max-w-full">
+        <div ref="selectorsDiv" class="grow flex gap-x-2 overflow-x-scroll snap-x">
+          <button v-for="cls in classifications!.entities"
+            :key="cls.self"
+            type="button"
+            ref="selectors"
+            :data-state="classification.find(({ self }) => cls.self === self) ? 'active' : 'inactive'"
+            class="bg-primary-60 px-4 py-2 border-none rounded-md snap-center flex-nowrap text-nowrap data-[state=active]:bg-accent-mid hover:bg-accent-dark hover:data-[state=active]:bg-accent-dark"
+            @click="setClassification(cls)"
+          >
+            {{ cls.label }}
+          </button>
+        </div>
+        <button type="button" v-show="next" class="flex-none bg-accent-mid border-none rounded-md p-2" @click.stop="selectorsDiv?.scrollBy({ left: 100, behavior: 'smooth' })">
+          Next
+        </button>
+      </div>
+    </div>
+    <div v-if="classification.length" class="flex gap-x-12 w-full">
+      <div class="bg-primary-20 dark:bg-primary-60 aspect-7/5 text-base dark:text-primary-40 justify-center items-center w-1/2 flex">
+        <IconFossil v-if="category === 'fossil'" class="size-48 fill-none stroke-current stroke-1.5" />
+        <IconMineral v-else-if="category === 'mineral'" class="size-48 fill-none stroke-current stroke-1.5" />
+        <IconRock v-else class="size-48 fill-none stroke-current stroke-1.5" />
+      </div>
+      <div class="flex flex-col gap-y-4 w-1/2">
+        <h2 class="text-4xl mb-4">{{ classification.at(-1)?.label }}</h2>
+        <div class="grow">
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus, aliquid minus. Quia vero consequatur deserunt eius sed dolore quisquam repellendus magni. Aperiam cumque deleniti perferendis adipisci commodi, aut ipsum repellendus?</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, eveniet neque! Doloremque, aliquam. Illum eligendi ea ipsam, nihil aut quis explicabo sequi ducimus eius. Minima est autem sunt! Consequuntur, autem.</p>
+        </div>
+        <div class="grid grid-cols-5 gap-x-2">
+          <div v-for="i in 5" class="bg-primary-20 dark:bg-primary-60 aspect-square text-base dark:text-primary-40 justify-center items-center flex">
+            <IconFossil v-if="category === 'fossil'" class="size-24 fill-none stroke-current stroke-1.5" />
+            <IconMineral v-else-if="category === 'mineral'" class="size-24 fill-none stroke-current stroke-1.5" />
+            <IconRock v-else class="size-24 fill-none stroke-current stroke-1.5" />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -25,12 +62,31 @@ definePageMeta({
   name: 'Browse',
 })
 
-const selection = ref('fossil')
-const filter = computed(() => ['depth:equals:0', `type:equals:classification/${selection.value}`])
-const { data: list, refresh } = await useFetch<EntityJSONList<Classification>>('/api/terms/classifications', { query: { filter } })
+const category = ref('fossil')
+const classification = ref<Pick<Classification, 'self' | 'label'>[]>([])
 
-async function onSelect(type: string) {
-  selection.value = type
+const selectorsDiv = ref<HTMLDivElement>()
+const selectors = ref<HTMLButtonElement[]>([])
+const next = computed(() => selectorsDiv.value && selectors.value
+  .sort((a, b) => a.getBoundingClientRect().right - b.getBoundingClientRect().right)
+  .find(s => s.getBoundingClientRect().right > selectorsDiv.value!.getBoundingClientRect().right))
+
+const filter = computed(() => ['depth:equals:0', `type:equals:classification/${category.value}`])
+const { data: classifications, refresh } = await useFetch<EntityJSONList<Classification>>('/api/terms/classifications', { query: { filter } })
+
+async function selectCategory(type: string) {
+  category.value = type
+  classification.value = []
   await refresh()
+  nextTick(() => {
+    const newClassification = classifications.value?.entities[0]
+    if (newClassification) {
+      setClassification({ self: newClassification.self, label: newClassification.label })
+    }
+  })
+}
+
+function setClassification(self: Pick<Classification, 'self' | 'label'>) {
+  classification.value = [...classification.value.slice(0, -1), self]
 }
 </script>
