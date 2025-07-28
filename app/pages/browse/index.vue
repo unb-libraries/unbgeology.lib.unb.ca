6c8005c8974db6b0721ab234e5fa278169a9b2e6
Patrick Schilf <pschilf@unb.ca>
2025-07-28 14:15:51 -0300

    Browse landing page

--- app/pages/browse/index.vue
+++ app/pages/browse/index.vue
@@ -1,55 +1,13 @@
 <template>
-  <div class="flex flex-col gap-y-12 w-full">
-    <div class="flex flex-col gap-y-2 w-full items-center overflow-x-hidden">
-      <div class="flex gap-x-2">
-        <button v-for="cat in ['fossil', 'mineral', 'rock']"
-          :key="cat"
-          :data-state="category === cat ? 'active' : 'inactive'"
-          class="bg-primary-40 px-4 py-2 border-none rounded-md data-[state=active]:bg-accent-mid hover:bg-accent-dark hover:data-[state=active]:bg-accent-dark"
-          @click="selectCategory(cat)" 
-        >
-          {{ cat.charAt(0).toUpperCase() + cat.slice(1) }}s
-        </button>
+  <div class="flex gap-x-6 w-full">
+    <article v-for="category in ['fossil', 'mineral', 'rock']" class="flex flex-col gap-y-6 w-1/3">
+      <SpecimenImage :category="(category as 'fossil' | 'mineral' | 'rock')" width="525" height="375" class="w-full aspect-7/5" />
+      <h2 class="text-2xl">{{ category.charAt(0).toUpperCase() + category.slice(1) }}s</h2>
+      <div>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod, sequi. Placeat facere enim culpa, corrupti accusantium dicta quae debitis ab, saepe qui a nihil quod dolores ratione. Doloremque, reiciendis rem?</p>
+        <a :href="`/browse/${category}`" class="text-accent-mid text-lg font-semibold hover:underline border-none rounded-md">Browse {{ category.charAt(0).toUpperCase() + category.slice(1) }}s</a>
       </div>
-      <div class="flex gap-x-2 max-w-full">
-        <div ref="selectorsDiv" class="grow flex gap-x-2 overflow-x-scroll snap-x">
-          <button v-for="cls in classifications!.entities"
-            :key="cls.self"
-            type="button"
-            ref="selectors"
-            :data-state="classification.find(({ self }) => cls.self === self) ? 'active' : 'inactive'"
-            class="bg-primary-60 px-4 py-2 border-none rounded-md snap-center flex-nowrap text-nowrap data-[state=active]:bg-accent-mid hover:bg-accent-dark hover:data-[state=active]:bg-accent-dark"
-            @click="setClassification(cls)"
-          >
-            {{ cls.label }}
-          </button>
-        </div>
-        <button type="button" v-show="next" class="flex-none bg-accent-mid border-none rounded-md p-2" @click.stop="selectorsDiv?.scrollBy({ left: 100, behavior: 'smooth' })">
-          Next
-        </button>
-      </div>
-    </div>
-    <div v-if="classification.length" class="flex gap-x-12 w-full">
-      <div class="bg-primary-20 dark:bg-primary-60 aspect-7/5 text-base dark:text-primary-40 justify-center items-center w-1/2 flex">
-        <IconFossil v-if="category === 'fossil'" class="size-48 fill-none stroke-current stroke-1.5" />
-        <IconMineral v-else-if="category === 'mineral'" class="size-48 fill-none stroke-current stroke-1.5" />
-        <IconRock v-else class="size-48 fill-none stroke-current stroke-1.5" />
-      </div>
-      <div class="flex flex-col gap-y-4 w-1/2">
-        <h2 class="text-4xl mb-4">{{ classification.at(-1)?.label }}</h2>
-        <div class="grow">
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus, aliquid minus. Quia vero consequatur deserunt eius sed dolore quisquam repellendus magni. Aperiam cumque deleniti perferendis adipisci commodi, aut ipsum repellendus?</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, eveniet neque! Doloremque, aliquam. Illum eligendi ea ipsam, nihil aut quis explicabo sequi ducimus eius. Minima est autem sunt! Consequuntur, autem.</p>
-        </div>
-        <div class="grid grid-cols-5 gap-x-2">
-          <div v-for="i in 5" class="bg-primary-20 dark:bg-primary-60 aspect-square text-base dark:text-primary-40 justify-center items-center flex">
-            <IconFossil v-if="category === 'fossil'" class="size-24 fill-none stroke-current stroke-1.5" />
-            <IconMineral v-else-if="category === 'mineral'" class="size-24 fill-none stroke-current stroke-1.5" />
-            <IconRock v-else class="size-24 fill-none stroke-current stroke-1.5" />
-          </div>
-        </div>
-      </div>
-    </div>
+    </article>
   </div>
 </template>
 
