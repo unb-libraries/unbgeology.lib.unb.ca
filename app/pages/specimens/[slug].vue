09ba58ca28024a5ca4a6c6f5f9cc2cbb47723a0d
Patrick Schilf <pschilf@unb.ca>
2025-08-08 16:11:11 -0300

    Refactored specimen detail view

--- app/pages/specimens/[slug].vue
+++ app/pages/specimens/[slug].vue
@@ -1,84 +1,118 @@
 <template>
   <article class="container mx-auto flex flex-col space-y-6 pb-24 pt-12">
-    <header class="ml-32">
-      <div class="inline-flex items-center space-x-4">
-        <h1 class="text-2xl font-bold">
+    <header class="flex flex-col gap-y-1 ml-32">
+      <div class="inline-flex items-center text-2xl gap-x-4">
+        <h1 class="font-bold">
           {{ specimen?.name ?? 'Unknown' }}
         </h1>
-        <span v-if="status !== Status.PUBLISHED" :class="['rounded-md text-xs px-2 py-1', {
-          'bg-yellow text-primary': status === Status.MIGRATED,
-          'bg-red-light': status === Status.DRAFT,
-          'bg-blue': status === Status.REVIEW,
-        }]">{{ useEnum(Status).labelOf(specimen!.status).toUpperCase() }}</span>
+        <div class="inline-flex gap-x-2 items-center">
+          <span v-if="specimen?.storage?.entities.at(-1)?.location.public"
+            class="px-2 py-1.5 text-sm text-base-97 rounded-md font-semibold uppercase leading-none bg-accent-26"
+          >
+            On Display
+          </span>
+          <IconLock v-if="useEnum(Status).valueOf(specimen.status) !== Status.PUBLISHED" class="text-red stroke-2 size-current" />
+          <SpecimenEditLink :specimen="specimen" />
+        </div>
       </div>
-      <span class="-mt-1 block text-sm italic">#{{ specimen!.id.toUpperCase() }}</span>
+      <div class="inline-flex text-sm leading-none text-base-27 font-semibold uppercase">
+        <span class="pr-4 border-r border-base-27">{{ specimen!.id.toUpperCase() }}</span>
+        <span class="pl-4">{{ classificationLabels.join(' &raquo; ') }}</span>
+      </div>
     </header>
-    <div class="flex w-full flex-col space-y-8 items-end">
-      <section class="w-full flex flex-col lg:flex-row gap-8">
-        <div v-if="(specimen?.images?.total ?? 0) > 0" class="space-y-1 w-full lg:w-3/4">
-          <div class="aspect-7/5 w-full">
-            <img :src="`${activeImage?.[1]}?w=1280&h=915`" />
-          </div>
-          <div class="w-full h-24 inline-flex gap-x-1 overflow-x-scroll">
-            <img v-for="{ self, uri } in specimen?.images?.entities" :key="self" :src="`${uri}?w=100&h=100`" :class="['cursor-pointer hover:opacity-50 size-24', { 'opacity-50': self === activeImage?.[0] }]" @click="activeImage = [self, uri]" />
+    <div class="flex w-full flex-col space-y-12 items-end">
+      <section class="flex w-full justify-start">
+        <div class="flex w-full lg:w-3/4 gap-x-2 aspect-7/5">
+          <SpecimenImage :specimen :url="activeImage?.uri" width="1280" height="915" class="grow aspect-7/5" />
+          <div class="flex flex-col flex-none gap-y-2 w-24 h-full overflow-y-hidden">
+            <button v-for="(image, i) in specimen.images?.entities ?? []"
+              :key="image.self"
+              type="button"
+              :data-index="i"
+              :data-status="image.self === activeImage?.self ? 'active' : 'inactive'" 
+              class="group w-full aspect-square data-[status=inactive]:cursor-pointer"
+              @click="activeImage = image"
+            >
+              <img 
+                :src="`${image.uri}?w=400&h=400`"
+                class="group-data-[status=active]:opacity-50 group-data-[status=inactive]:hover:opacity-50" />
+            </button>
           </div>
         </div>
-        <div v-else class="bg-primary-20 dark:bg-primary-60 aspect-7/5 text-base dark:text-primary-40 justify-center items-center flex w-3/4 h-full">
-          <IconFossil v-if="specimen!.type === 'fossil'" class="size-48 fill-none stroke-current stroke-1.5" />
-          <IconMineral v-else-if="specimen!.type === 'mineral'" class="size-48 fill-none stroke-current stroke-1.5" />
-          <IconRock v-else class="size-48 fill-none stroke-current stroke-1.5" />
+        
+      </section>
+      <section v-if="specimen?.description" class="w-1/2 mx-auto">
+        <h2 class="text-base-27 mb-3 text-lg font-bold text-start uppercase">
+          Description
+        </h2>
+        <div>
+          {{ specimen?.description }}
         </div>
-        <dl class="flex flex-col w-full mx-auto lg:w-1/4 lg:gap-y-4">
-          <!-- Category -->
-          <div class="grid grid-cols-2 lg:grid-cols-1 gap-x-4">
-            <dt class="text-end lg:text-start text-primary-40 uppercase font-bold">Category</dt>
-            <dd>{{ specimen!.type[0].toUpperCase() + specimen!.type.slice(1).toLowerCase() }}</dd>
-          </div>
-          <!-- Classification -->
-          <div class="grid grid-cols-2 lg:grid-cols-1 gap-x-4">
-            <dt class="text-end lg:text-start text-primary-40 uppercase font-bold">Classification</dt>
-            <dd v-if="classificationLabels.length">
-              <ul v-for="label in classificationLabels" :key="label">
-                <li>{{ label }}</li>
+      </section>
+
+      <section class="w-1/2 mx-auto">
+        <h2 class="text-base-27 mb-3 text-lg font-bold text-start uppercase">Specifications</h2>
+        <table class="w-full">
+          <tr class="border-b border-base-77 last:border-b-0">
+            <td class="py-3 pt-0 text-sm text-base-27 font-semibold uppercase">ID</td>
+            <td class="py-3 pt-0">{{ specimen?.id.toUpperCase() }}</td>
+          </tr>
+          <tr class="border-b border-base-77 last:border-b-0">
+            <td class="py-3 text-sm text-base-27 font-semibold uppercase">Object name</td>
+            <td class="py-3">{{ specimen?.name }}</td>
+          </tr>
+          <tr class="border-b border-base-77 last:border-b-0">
+            <td class="py-3 text-sm text-base-27 font-semibold uppercase">Legal status</td>
+            <td class="py-3">{{ useEnum(Legal).valueOf(specimen?.legal) === Legal.PERMANENT ? 'Permanent collection' : 'On loan' }}</td>
+          </tr>
+          <tr class="border-b border-base-77 last:border-b-0">
+            <td class="py-3 text-sm text-base-27 font-semibold uppercase">Category</td>
+            <td class="py-3">{{ specimen?.type[0].toUpperCase() + specimen?.type.slice(1) }}</td></tr>
+          <tr class="border-b border-base-77 last:border-b-0">
+            <td class="py-3 text-sm text-base-27 font-semibold uppercase">Classification</td>
+            <td class="py-3">
+              {{[...specimen?.classification?.ancestors?.entities?.map(({ label }) => label) ?? [], specimen?.classification?.label].filter(Boolean).join(' &raquo; ')}}
+            </td>
+          </tr>
+          <tr class="border-b border-base-77 last:border-b-0">
+            <td class="py-3 text-sm text-base-27 font-semibold uppercase">Pieces</td>
+            <td class="py-3">{{ specimen?.pieces }}</td></tr>
+          <tr class="border-b border-base-77 last:border-b-0">
+            <td class="py-3 text-sm text-base-27 font-semibold uppercase">Partial</td>
+            <td class="py-3">{{ specimen?.partial ? 'Yes' : 'No' }}</td></tr>
+          <tr v-if="specimen.type === 'fossil'">
+            <td>Portion</td>
+            <td>{{ specimen?.portion?.label }}</td>
+          </tr>
+          <tr v-if="compositionLabels.length" class="border-b border-base-77 last:border-b-0">
+            <td class="py-3 text-sm text-base-27 font-semibold uppercase">Composition</td>
+            <td class="py-3">
+              <ul v-if="specimen?.type === 'mineral'">
+                <li v-for="label in compositionLabels" :key="label">{{ label }}</li>
               </ul>
-            </dd>
-            <dd v-else>Not specified</dd>
-          </div>
-          <!-- Date -->
-          <div class="grid grid-cols-2 lg:grid-cols-1 gap-x-4">
-            <dt class="text-end lg:text-start text-primary-40 uppercase font-bold">Date</dt>
-            <dd>{{ specimen!.date ?? "Unknown" }}</dd>
-          </div>
-          <!-- Composition -->
-          <div class="grid grid-cols-2 lg:grid-cols-1 gap-x-4">
-            <dt class="text-end lg:text-start text-primary-40 uppercase font-bold">Composition</dt>
-            <dd v-if="compositionLabels.length">
-              <ul v-for="label in compositionLabels" :key="label">
-                <li>{{ label }}</li>
-              </ul>
-            </dd>
-            <dd v-else>Not specified</dd>
-          </div>
-          <!-- Age -->
-          <div class="grid grid-cols-2 lg:grid-cols-1 gap-x-4">
-            <dt class="text-end lg:text-start text-primary-40 uppercase font-bold">Age</dt>
-            <dd>
-              <template v-if="specimen?.age?.relative">
-                {{specimen?.age?.relative?.map(({ label, start }, i) => `${label} (${Number(specimen!.age.numeric?.[i] ?? start) / 1000000} Mya)`).join(' - ')}}
-              </template>
-              <template v-else>Unknown</template>
-            </dd>
-          </div>
-          <!-- Pieces -->
-          <div class="grid grid-cols-2 lg:grid-cols-1 gap-x-4">
-            <dt class="text-end lg:text-start text-primary-40 uppercase font-bold">Pieces</dt>
-            <dd v-if="specimen?.pieces">{{ specimen?.pieces }}{{ specimen?.partial ? ` (Partial)` : `` }}</dd>
-            <dd v-else>Not specified</dd>
-          </div>
-          <!-- Measurements -->
-          <div class="grid grid-cols-2 lg:grid-cols-1 gap-x-4">
-            <dt class="text-end lg:text-start text-primary-40 uppercase font-bold">Measurements</dt>
-            <dd>
+              <template v-else>{{ compositionLabels.join(', ') }}</template>
+            </td>
+          </tr>
+          <tr v-if="specimen?.age?.relative" class="border-b border-base-77 last:border-b-0">
+            <td class="py-3 text-sm text-base-27 font-semibold uppercase">Age</td>
+            <td class="py-3">
+              {{specimen?.age?.relative?.map(({ label }) => label).join(' to ')}}
+            </td>
+          </tr>
+          <tr class="border-b border-base-77 last:border-b-0">
+            <td class="py-3 text-sm text-base-27 font-semibold uppercase">Numeric age</td>
+            <td class="py-3">
+              <template v-if="specimen?.age?.numeric">{{specimen?.age?.numeric?.map(mya => Number(mya) / Math.pow(10, 6)).join(' - ')}}</template>
+              <template v-else>{{specimen?.age?.relative?.map(({ start }) => Number(start) / Math.pow(10, 6)).join(' - ')}} Mya</template>
+            </td>
+          </tr>
+          <tr class="border-b border-base-77 last:border-b-0">
+            <td class="py-3 text-sm text-base-27 font-semibold uppercase">Origin</td>
+            <td class="py-3">{{ specimen?.origin?.name }}</td>
+          </tr>
+          <tr class="border-b border-base-77 last:border-b-0">
+            <td class="py-3 pb-0 text-sm text-base-27 font-semibold uppercase">Measurements</td>
+            <td class="py-3 pb-0">
               <ul v-if="specimen!.measurements?.count && useEnum(MeasurementCount).valueOf(specimen!.measurements.count) === MeasurementCount.INDIVIDUAL">
                 <li v-for="(dimensions) in specimen!.measurements!.dimensions" :key="dimensions.join('x')">
                   {{dimensions.map(d => `${d}mm`).join(' x ')}}
@@ -103,51 +137,44 @@
                 Too small to measure
               </template>
               <template v-else>Not specified</template>
-            </dd>
-          </div>
-          <!-- Storage -->
-          <div class="grid grid-cols-2 lg:grid-cols-1 gap-x-4">
-            <dt class="text-end lg:text-start text-primary-40 uppercase font-bold">Storage</dt>
-            <dd v-if="specimen?.storage?.entities.at(-1)">{{ specimen?.storage?.entities.at(-1)?.location.public ? `On Display` : `In Archive` }}</dd>
-            <dd v-else>Not specified</dd>
-          </div>
-        </dl>
+            </td>
+          </tr>
+        </table>
       </section>
-      <section v-if="specimen?.description" class="w-3/4 mx-auto lg:mx-0">
-        <h2 class="sr-only text-primary-40 mb-3 text-lg font-bold uppercase">
-          Description
-        </h2>
-        <div>
-          {{ specimen?.description }}
-        </div>
-      </section>
-      <section v-if="specimen?.origin" class="w-full lg:w-3/4">
-        <h2 class="text-primary-40 mb-3 text-lg font-bold text-center lg:text-start uppercase">
-          Place of Origin
+      <section v-if="specimen?.origin?.latitude && specimen?.origin?.longitude" class="w-full lg:w-3/4">
+        <h2 class="text-base-27 mb-3 text-lg font-bold text-start uppercase">
+          Map of Origin
         </h2>
         <LeafletMap v-if="specimen?.origin?.latitude && specimen?.origin?.longitude" class="min-h-128 w-full" :zoom="7" :center="[specimen.origin.latitude, specimen.origin.longitude]">
           <LeafletMarker
             v-if="specimen.origin"
-            :name="specimen.name"
+            :name="specimen?.origin?.name ?? 'Unknown'"
             :center="[specimen?.origin.latitude, specimen?.origin.longitude]"
-          />
+          >
+            <div class="inline-flex gap-2 w-full p-4 text-nowrap text-base-27 font-semibold uppercase">
+              {{ specimen.origin.name ?? 'Unknown' }}
+            </div>
+          </LeafletMarker>
         </LeafletMap>
-        <div v-else-if="specimen?.origin?.name">{{ specimen.origin.name }}</div>
+        <div v-else-if="specimen?.origin?.name">{{ specimen?.origin?.name ?? 'Unknown' }}</div>
       </section>
       <section v-if="publications.length" class="w-full lg:w-3/4">
-        <h2 class="text-center lg:text-start mx-auto lg:mx-0 text-primary-40 mb-3 text-lg font-bold uppercase">
+        <h2 class="text-base-27 mb-3 text-lg font-bold text-start uppercase">
           Publications
         </h2>
-        <PvEntityList class="list-inside list-decimal" :entities="publications" item-class="my-3 first:mt-0 last:mb-0" v-slot="{ entity: publication }">
-          <a :href="publication.doi" class="hover:underline">{{ publication.citation }}</a>
-        </PvEntityList>
+        <ul class="list-inside list-decimal">
+          <li v-for="(publication, i) in publications" :key="publication.self" class="py-3 first:pt-0 last:pb-0 border-b border-base-77 last:border-b-0">
+            <a v-if="publication.doi" :href="publication.doi" class="hover:underline">{{ publication.citation }}</a>
+            <template v-else>{{ publication.citation }}</template>
+          </li>
+        </ul>
       </section>
     </div>
   </article>
 </template>
 
 <script setup lang="ts">
-import { Immeasurabibility, MeasurementCount, Status, type Fossil, type Rock, type Specimen } from 'types/specimen'
+import { Immeasurabibility, Legal, MeasurementCount, Status, type Fossil, type Rock, type Specimen } from 'types/specimen'
 
 definePageMeta({
   layout: `default`,
@@ -162,8 +189,9 @@ if (!specimen.value) {
 }
 
 const classificationLabels = computed(() => [
-  specimen.value?.classification?.label,
-  ...(specimen.value?.classification?.ancestors?.entities.map(({ label }) => label) ?? []),
+  specimen.value!.classification?.label,
+  ...(specimen.value!.classification?.ancestors?.entities.map(({ label }) => label) ?? []),
+  specimen.value!.type[0].toUpperCase() + specimen.value!.type.slice(1).toLowerCase() + 's',
 ].filter(Boolean).reverse())
 
 const compositionLabels = computed(() => {
@@ -177,7 +205,24 @@ const compositionLabels = computed(() => {
   return []
 })
 
-const activeImage = ref(specimen?.value?.images?.entities?.length ? [specimen!.value?.images?.entities[0].self, specimen!.value?.images?.entities[0].uri] : undefined)
+const activeImage = ref(specimen?.value?.images?.entities?.[0])
 const publications = computed(() => specimen.value?.publications?.entities ?? [])
-const status = computed(() => useEnum(Status).valueOf(specimen.value!.status))
 </script>
+
+<style>
+.leaflet-popup-content {
+  @apply bg-base-87 text-base-17 m-0 !w-fit;
+}
+
+.leaflet-popup-content-wrapper {
+  @apply bg-base-87 rounded-none;
+}
+
+.leaflet-popup-tip {
+  @apply bg-base-87;
+}
+
+.leaflet-popup-close-button {
+  @apply hidden;
+}
+</style>
