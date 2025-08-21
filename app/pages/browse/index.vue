46e04bc47294f7d53bb19a8c329d0c20fd903c1b
Patrick Schilf <pschilf@unb.ca>
2025-08-21 12:48:33 -0300

    Copy category cards to browse index

--- app/pages/browse/index.vue
+++ app/pages/browse/index.vue
@@ -1,15 +1,11 @@
 <template>
   <NuxtLayout name="page">
     <template #breadcrumbs></template>
-    <div class="flex gap-x-6 w-full">
-      <article v-for="category in ['fossil', 'mineral', 'rock']" class="flex flex-col gap-y-6 w-1/3">
-        <SpecimenImage :category="(category as 'fossil' | 'mineral' | 'rock')" width="525" height="375" class="w-full aspect-7/5" />
-        <h2 class="text-2xl">{{ category.charAt(0).toUpperCase() + category.slice(1) }}s</h2>
-        <div>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod, sequi. Placeat facere enim culpa, corrupti accusantium dicta quae debitis ab, saepe qui a nihil quod dolores ratione. Doloremque, reiciendis rem?</p>
-          <a :href="`/browse/${category}`" class="text-accent-mid text-lg font-semibold hover:underline border-none rounded-md">Browse {{ category.charAt(0).toUpperCase() + category.slice(1) }}s</a>
-        </div>
-      </article>
+    <div class="grid grid-cols-3 gap-x-12">
+      <CategoryCard v-for="category in ['fossil', 'mineral', 'rock']"
+        :key="category"
+        :category="(category as 'fossil' | 'mineral' | 'rock')"
+      />
     </div>
   </NuxtLayout>
 </template>
