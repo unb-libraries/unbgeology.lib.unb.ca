0538c195bd2cdb38647bc2c74223cee4d3027f79
Patrick Schilf <pschilf@unb.ca>
2025-08-21 11:35:19 -0300

    Front page category placeholder (?) images,text

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -20,13 +20,30 @@
     <!-- Browse by Category -->
     <section class="flex flex-col container mx-auto">
       <h2 class="text-4xl mb-12">Browse by Category</h2>
-      <div class="flex gap-x-12">
-        <article v-for="category in ['fossil', 'mineral', 'rock']" class="flex flex-col gap-y-6 w-1/3">
-          <SpecimenImage :category="(category as 'fossil' | 'mineral' | 'rock')" width="525" height="375" class="w-full aspect-7/5" />
-          <h2 class="text-2xl">{{ category.charAt(0).toUpperCase() + category.slice(1) }}s</h2>
-          <div class="flex flex-col space-y-4">
-            <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod, sequi. Placeat facere enim culpa, corrupti accusantium dicta quae debitis ab, saepe qui a nihil quod dolores ratione. Doloremque, reiciendis rem?</div>
-            <a :href="`/browse/${category}`" class="text-accent-26 dark:text-accent-36 text-lg font-semibold hover:underline border-none rounded-md">Browse {{ category.charAt(0).toUpperCase() + category.slice(1) }}s</a>
+      <div class="grid grid-cols-3 gap-x-12">
+        <article v-for="category in ['fossil', 'mineral', 'rock']" class="flex flex-col gap-y-6 h-full">
+          <SpecimenImage
+            :category="(category as 'fossil' | 'mineral' | 'rock')"
+            :url="`/top-cat-images-${category}s.jpg`"
+            width="525"
+            height="375"
+            class="flex-none w-full aspect-7/5"
+          />
+          <h2 class="flex-none text-2xl">{{ category.charAt(0).toUpperCase() + category.slice(1) }}s</h2>
+          <div class="flex flex-col grow">
+            <div v-if="category === 'fossil'" class="grow">
+              <p>Fossils are the preserved remains or traces of ancient organisms that provide valuable insights into Earth's past life forms and environments. They are the key to understanding the history of life on our planet.</p>
+              <p>Studying fossils is a multidisciplinary endeavor that combines geology, paleontology, biology, and other scientific disciplines. It helps us unravel the story of life on Earth and provides a fascinating glimpse into the ancient past.</p>
+            </div>
+            <div v-if="category === 'mineral'" class="grow">
+              <p>Minerals are naturally occurring, inorganic solid substances with a specific chemical composition and a characteristic crystal structure. They are the building blocks of rocks and are found in various geological environments.</p>
+              <p>Studying minerals provides insights into Earth's geological processes, as well as their importance in industries and everyday life.</p>
+            </div>
+            <div v-if="category === 'rock'" class="grow">
+              <p>Rocks are solid, naturally occurring substances that make up the Earth's crust. They are composed of minerals, which are the building blocks of rocks. Geologists study rocks to understand Earth's history, the processes that shape the planet's surface, and the resources they may contain.</p>
+              <p>It's important to note that rocks vary greatly in their properties, appearance, and composition depending on their specific type and the geological processes that formed them.</p>
+            </div>
+            <a :href="`/browse/${category}`" class="flex-none text-accent-26 hover:text-accent-46 dark:text-accent-36 text-lg font-semibold underline border-none rounded-md">Browse {{ category.charAt(0).toUpperCase() + category.slice(1) }}s</a>
           </div>
         </article>
       </div>
