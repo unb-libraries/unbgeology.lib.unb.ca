ed57db9efbadcdb9290aa70a41d18ab3f1131e38
Patrick Schilf <pschilf@unb.ca>
2025-08-22 14:51:08 -0300

    Remove origin description from search index

--- app/server/api/specimens/index.get.ts
+++ app/server/api/specimens/index.get.ts
@@ -78,8 +78,6 @@ export default defineCachedEventHandler(async (event) => {
           { autocomplete: { query: queryFilter.search, path: 'name' } },
           { text: { query: queryFilter.search, path: 'origin.name' } },
           { autocomplete: { query: queryFilter.search, path: 'origin.name' } },
-          { text: { query: queryFilter.search, path: 'origin.description' } },
-          { autocomplete: { query: queryFilter.search, path: 'origin.description' } },
           { phrase: { query: queryFilter.search, path: 'description' } },
         ],
       },
