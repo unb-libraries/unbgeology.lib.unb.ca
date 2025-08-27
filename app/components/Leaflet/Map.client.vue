cab719d97414d500e5b449261215071aa44fc1be
Patrick Schilf <pschilf@unb.ca>
2025-08-27 12:32:14 -0300

    Insert leaflet overrides into main css

--- app/components/Leaflet/Map.client.vue
+++ app/components/Leaflet/Map.client.vue
@@ -15,7 +15,6 @@ const leafletCSS = new URL('leaflet/dist/leaflet.css', import.meta.url).href
 const markerClusterCSS = new URL('leaflet.markercluster/dist/MarkerCluster.css', import.meta.url).href
 const markerClusterDefaultCSS = new URL('leaflet.markercluster/dist/MarkerCluster.Default.css', import.meta.url).href
 const gestureHandlingCSS = new URL('leaflet-gesture-handling/dist/leaflet-gesture-handling.css', import.meta.url).href
-const leafletPopupCSS = new URL('~/assets/css/leaflet-popup.css', import.meta.url).href
 
 useHead({
   link: [
@@ -23,7 +22,6 @@ useHead({
     { rel: 'stylesheet', href: markerClusterCSS },
     { rel: 'stylesheet', href: markerClusterDefaultCSS },
     { rel: 'stylesheet', href: gestureHandlingCSS },
-    { rel: 'stylesheet', href: leafletPopupCSS },
   ],
 })
 
