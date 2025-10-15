e437110a46ccf0f80b7acbcc81abdc625980a52d
Patrick Schilf <pschilf@unb.ca>
2025-10-15 14:28:31 -0300

    Site meta

--- app/config/prod/nuxt.config.ts
+++ app/config/prod/nuxt.config.ts
@@ -1,6 +1,29 @@
 import { existsSync, mkdirSync } from 'fs'
 
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [
+        { rel: `icon`, type: `image/svg+xml`, href: `/favicon.svg` },
+        { rel: `icon`, type: `image/vnd.microsoft.icon`, href: `/favicon.ico` },
+        { rel: `icon`, type: `image/png`, sizes: `16x16`, href: `/favicon-16x16.png` },
+        { rel: `icon`, type: `image/png`, sizes: `32x32`, href: `/favicon-32x32.png` },
+        { rel: `apple-touch-icon`, sizes: `180x180`, href: `/apple-touch-icon.png` },
+        { rel: `mask-icon`, href: `/safari-pinned-tab.svg`, color: `#900000` },
+        { rel: `manifest`, href: `/site.webmanifest` },
+      ],
+      meta: [
+        { name: `application-name`, content: `UNBGeology | Earth Science Collections | UNB Libraries` },
+        { name: `geo.region`, content: `CA-NB` },
+        { name: `geo.position`, content: `45.946153; -66.643089` },
+        { name: `geo.placename`, content: `Fredericton` },
+        { name: `msapplication-config`, content: `/browserconfig.xml` },
+        { name: `msapplication-TileColor`, content: `#2b5797` },
+        { name: `theme-color`, content: `#ffffff` },
+        { 'http-equiv': `content-language`, 'content': `en` },
+      ],
+    },
+  },
   buildDir: `/app/html/.build/.nuxt`,
   css: [
     `~/assets/css/main.css`,
