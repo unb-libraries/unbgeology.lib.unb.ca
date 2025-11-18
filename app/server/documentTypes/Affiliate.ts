5495f5df1d0f5e4365c3518f3b991f9b9d64e9c9
Patrick Schilf <pschilf@unb.ca>
2025-11-18 10:31:54 -0400

    Affiliate/Person pronouns optional

--- app/server/documentTypes/Affiliate.ts
+++ app/server/documentTypes/Affiliate.ts
@@ -89,7 +89,7 @@ export default {
     pronouns: {
       type: EntityFieldTypes.Mixed,
       enum: Pronouns,
-      required: true,
+      required: false,
     },
     title: {
       type: EntityFieldTypes.Mixed,
