c8ef9a8ed2da065db566484bfc2762af9c7322e5
Patrick Schilf <pschilf@unb.ca>
2026-05-11 15:13:24 -0300

    Implement specimen sitemap URLs handler

--- app/server/api/_sitemap/urls/specimens.get.ts
+++ app/server/api/_sitemap/urls/specimens.get.ts
@@ -1,5 +1,28 @@
 import type { SitemapUrlInput } from "@nuxtjs/sitemap"
+import { type EntityJSONList } from "~/layers/entity/src/types"
+import { type Specimen } from "~/types/specimen"
 
 export default defineSitemapEventHandler(async () => {
-  return [] satisfies SitemapUrlInput[]
+  let done = false
+  const urls: SitemapUrlInput[] = []
+  let page = 1
+
+  while (!done) {
+    const { entities, nav: { next } } = await $fetch<EntityJSONList<Specimen>>('/api/specimens', {
+      query: {
+        select: ['id', 'updated'],
+        pageSize: 500,
+        page: page++,
+      }})
+    urls.push(...(entities.map(({ id, updated }) => ({
+      loc: `/specimens/${id}`,
+      lastmod: updated,
+    })) ?? []))
+
+    if (!next) {
+      done = true
+    }
+  }
+
+  return urls
 })
