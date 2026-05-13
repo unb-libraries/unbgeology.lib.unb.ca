d94d8097c7e9d6501327ebde60b542020a328c20
Patrick Schilf <pschilf@unb.ca>
2026-05-13 15:14:38 -0300

    Fix classification input form imports

--- app/components/Input/Specimen/Classification.vue
+++ app/components/Input/Specimen/Classification.vue
@@ -24,6 +24,7 @@
 <script setup lang="ts">
 import { FilterOperator } from '@unb-libraries/nuxt-layer-entity'
 import type { Classification } from '~/types/classification'
+import { FormClassificationFossil, FormClassificationMineral, FormClassificationRock } from '#components'
 
 const classification = defineModel<string>({ required: false })
 const props = defineProps<{
