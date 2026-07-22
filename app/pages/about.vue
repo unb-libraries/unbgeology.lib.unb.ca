1b943d2bac4e2c023f7c59217bfe68e984db9c43
Patrick Schilf <pschilf@unb.ca>
2026-07-22 14:40:32 -0300

    About page (2)

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,20 +1,22 @@
 <template>
-  <p class="my-4">
-    The UNB Geology project is a collaboration between the University of New Brunswick's Department of Earth Sciences and UNB Libraries. The goal of the project is to provide digital access to UNB's Earth Science Collections and to improve accuracy, traceability, searchability, and long-term preservation of the collections, as well as enhance global visibility of UNB's geological heritage.
-  </p>
+  <section class="my-8">
+    <p class="my-4">
+      The UNB Geology project is a collaboration between the University of New Brunswick's Department of Earth Sciences and UNB Libraries. The goal of the project is to provide digital access to UNB's Earth Science Collections and to improve accuracy, traceability, searchability, and long-term preservation of the collections, as well as enhance global visibility of UNB's geological heritage.
+    </p>
+  </section>
   
   <section class="my-8">
     <h2 class="text-2xl">The Earth Science Collections</h2>
     <p class="my-4">
-      The University of New Brunswick's history of documenting natural history goes back as far as 1837 when <strong>James Robb</strong>, Professor for Chemistry and Natural Sciences (1837-1861), first created a space to house geological, mineral, and biological specimens at the University of New Brunswick. <strong>Lort Bailey</strong> continued and expanded the new museum, advocated to maintain its collections and to integrate them into research and teaching. Geology and Mineralogy emerges as a field of study in the 1930s, paving the way for the establishment of the Department of Geology and Mineralogy in 1941. <strong>Arnold McAllister</strong>, in his role as Professor and Department Head (1962-1971), started cataloguing geological specimens in 1957. During his time the curriculum grew to include to include Mineralogy, Palaeontology, Stratigraphy/Sedimentology, Petrology, Geochemistry, Tectonophysics. UNB launched its first Geological Engineering program in the 1980s.
+      The University of New Brunswick's history of curating geological heritage goes back as far as 1837 when <strong>James Robb</strong>, Professor for Chemistry and Natural Sciences (1837-1861), first created a space to house geological, mineral, and biological specimens at the University of New Brunswick. <strong>Lort Bailey</strong> continued and expanded the new museum, advocated to maintain its collections and to integrate them into research and teaching. Geology and Mineralogy emerges as a field of study in the 1930s, paving the way for the establishment of the Department of Geology and Mineralogy in 1941. <strong>Arnold McAllister</strong>, in his role as Professor and Department Head (1962-1971), started cataloguing geological specimens in the 1950s. During his time the curriculum grew to include to include Mineralogy, Palaeontology, Stratigraphy/Sedimentology, Petrology, Geochemistry, Tectonophysics. UNB launched its first Geological Engineering program in the 1980s.
     </p>
     <p class="my-4">
       In 2011 the Earth Science Centre officially opened, made possible by a $1,000,000 donation from <strong>Dr. Robert Quartermain</strong>. The Centre is located in the Forestry and Geology building on the University of New Brunswick's Fredericton campus. It is home to the Quartermain Museum and the Earth Science Collections. In 2014 <strong>Dr. Ann Timmermans</strong> became the museum's first curator.
     </p>
-    <p class="my-4">
+    <section class="my-4">
       <h3 class="text-lg font-semibold mb-2">Notable Contributors</h3>
       Many individuals have contributed to grow the collections to include thousands of specimens, most notably:
-      <ul class="list-disc pl-6">
+      <ul class="list-disc my-4 pl-6">
         <li>Dr. David Lentz</li>
         <li>Dr. Ronald Pickerill</li>
         <li>Dr. Joseph White</li>
@@ -22,34 +24,35 @@
         <li>Dr. Wouter van de Poll</li>
         <li>Dr. James Noble</li>
       </ul>
-    </p>
+    </section>
   </section>
 
   <section class="my-8">
     <h2 class="text-2xl">Curatorial Staff</h2>
     <p class="my-4">
-      <strong>Dr. Ann C. Timmermans</strong> is a geologist, Associate Teaching Professor, and curator of the Quartermain Museum. Originally from Ontario, she holds an BSc from the University of Waterloo as well as an MSc and PhD from Carleton University. As a geologist she gained extensive field experience in geophysical exploration across North America and Africa. Since joining UNB in 2014, she has focused on geoscience education, museum curation, and outreach guided by the UN Sustainability Goals. Committed to preserving geological heritage, she works with curators and scientists worldwide to advance responsible stewardship of natural history collections.
+      <strong>Dr. Ann C. Timmermans</strong> is a geologist, Associate Teaching Professor, and curator of the Quartermain Museum. Originally from Ontario, she holds a BSc from the University of Waterloo as well as an MSc and PhD from Carleton University. As a geologist she gained extensive field experience in geophysical exploration across North America and Africa. Since joining UNB in 2014, she has focused on geoscience education, museum curation, and outreach guided by the UN Sustainability Goals. Committed to preserving geological heritage, she works with curators and scientists worldwide to advance responsible stewardship of natural history collections.
     </p>
   </section>
 
   <section class="my-8">
     <h2 class="text-2xl">Technical Administration & Development</h2>
-    <p class="my-4">
-      <strong>Jeff Carter</strong> is Manager of Library Systems, UNB Libraries. He is a reformed educator with a passion for purposeful and considered implementation of technology. Jeff has been working in the field since 1993 and in web development at UNB Libraries since 2000.
-    </p>
-    <p class="my-4">
-      <strong>Patrick Schilf</strong> is a software developer specializing in web technologies with 10+ years experience. Born and raised in Germany, he joined UNB Libraries's Systems group in 2019. His work is driven by the prospect of building the tools that contribute to scholars' and students' pursuits of teaching, learning, and research, as well as digital publication and preservation.
-    </p>
+    <ul class="list-disc my-4 pl-6">
+      <li>Patrick Schilf, Web developer</li>
+      <li>Jeremy McDermott, Web developer</li>
+      <li>Jonny Wood, Graphic Designer</li>
+    </ul>
   </section>
 
   <section class="my-8">
-    <h2 class="text-2xl mb-4">Contact</h2>
-    <span class="inline-flex items-center font-bold gap-x-2">
-      <IconMail
-        class="size-4 fill-none stroke-2 stroke-base-2 dark:text-base-77"
-      />
-      <a href="mailto:quartermain@unb.ca" class="underline">Contact the curator</a>
-    </span>
+    <h2 class="text-2xl">Contact</h2>
+    <p class="my-4">
+      <span class="inline-flex items-center font-bold gap-x-2">
+        <IconMail
+          class="size-4 fill-none stroke-2 stroke-base-2 dark:stroke-base-77"
+        />
+        <a href="mailto:quartermain@unb.ca" class="underline">Contact the curator</a>
+      </span>
+    </p>
   </section>
 
 </template>
