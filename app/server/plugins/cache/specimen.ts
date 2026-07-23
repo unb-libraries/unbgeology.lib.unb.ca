12a3eb1ea04b8fdc5e23bece80cda8b0b9dbaf8d
Patrick Schilf <pschilf@unb.ca>
2026-07-23 14:42:08 -0300

    Invalidate cache upon deleting record

--- app/server/plugins/cache/specimen.ts
+++ app/server/plugins/cache/specimen.ts
@@ -1,6 +1,6 @@
 export default defineNitroPlugin((nitro) => {
   nitro.hooks.hook(`afterResponse`, async (event, response) => {
-    if (event.path.match(/^\/api\/(specimens|terms|users)/) && [`PATCH`, `POST`, `PUT`].includes(event.method)) {
+    if (event.path.match(/^\/api\/(specimens|terms|users)/) && [`PATCH`, `POST`, `PUT`, `DELETE`].includes(event.method)) {
       const cache = useStorage(`cache`)
       await cache.clear()
     }
