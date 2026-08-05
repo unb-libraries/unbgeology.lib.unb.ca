437ed54c070572c13500d69b44002fc1159b7657
Patrick Schilf <pschilf@unb.ca>
2026-08-05 12:17:28 -0300

    Fix EditLink class inheritance

--- app/components/SpecimenEditLink.vue
+++ app/components/SpecimenEditLink.vue
@@ -1,6 +1,6 @@
 <template>
   <WithPermission :permission="/^update:specimen:/">
-    <a :href="`/dashboard/specimens/${specimen.id}`" :title="`Edit ${specimen.id.toUpperCase()}`">
+    <a :href="`/dashboard/specimens/${specimen.id}`" :title="`Edit ${specimen.id.toUpperCase()}`" v-bind="$attrs">
       <IconPen class="fill-none stroke-current hover:stroke-accent-26 dark:hover:stroke-accent-36 stroke-2 size-current" />
     </a>
   </WithPermission>
@@ -9,6 +9,7 @@
 <script lang="ts" setup>
 import type { Specimen } from '~~/types/specimen'
 
+defineOptions({ inheritAttrs: false })
 const props = defineProps<{
   specimen: Specimen
 }>()
