6effc2a1e3c59d1e0a3cd57e72155d41b91a540c
Patrick Schilf <pschilf@unb.ca>
2026-08-27 14:00:43 -0300

    fix: carto api required, change to esri

--- app/components/Leaflet/Map.client.vue
+++ app/components/Leaflet/Map.client.vue
@@ -100,8 +100,8 @@ function createMap(container: HTMLDivElement) {
     ])
   })
   
-  const tiles = tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
-    attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`,
+  const tiles = tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community',
   })
   map.addLayer(tiles)
 
